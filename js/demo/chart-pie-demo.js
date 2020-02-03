573f6157381b1d7cd8d6736aeabc9e905c3aa297
nugurejeil <nugurejeil1@gmail.com>
2020-02-03 22:11:46 +0900

    파이차트 트랜스파일링

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -1,11 +1,14 @@
+"use strict";
+
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
+Chart.defaults.global.defaultFontColor = '#858796'; // Pie Chart Example
 
-// Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var chartText = document.querySelectorAll("#chartText .mr-2");
-marker.sort((a, b) => (a.확진자수 < b.확진자수) ? 1 : -1);
+marker.sort(function (a, b) {
+  return a.확진자수 < b.확진자수 ? 1 : -1;
+});
 chartText[0].innerText = marker[0].Name;
 chartText[1].innerText = marker[1].Name;
 chartText[2].innerText = marker[2].Name;
@@ -17,8 +20,8 @@ var myPieChart = new Chart(ctx, {
       data: [marker[0].확진자수, marker[1].확진자수, marker[2].확진자수],
       backgroundColor: ['#ff3333', '#ffc155', '#fcff55'],
       hoverBackgroundColor: ['#ff0000', '#ff0000', '#ff0000'],
-      hoverBorderColor: "rgba(222, 0, 0, 1)",
-    }],
+      hoverBorderColor: "rgba(222, 0, 0, 1)"
+    }]
   },
   options: {
     maintainAspectRatio: false,
@@ -30,11 +33,11 @@ var myPieChart = new Chart(ctx, {
       xPadding: 15,
       yPadding: 15,
       displayColors: false,
-      caretPadding: 10,
+      caretPadding: 10
     },
     legend: {
       display: false
     },
-    cutoutPercentage: 80,
-  },
+    cutoutPercentage: 80
+  }
 });
