74235ce21e0935d90d5a002e3ee37b1268d23801
Taeuk Kang <mail@taeuk.me>
2020-02-04 19:29:57 +0900

    Update index.js to import all files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "jquery";
+import $ from 'jquery';
+require('jquery.easing')(jQuery);
+
+import "./marker";
+import "./worldmap";
+
+import "./bootstrap.bundle.min";
+import "./sb-admin-2";
+import "./setText";
+import "./Chart.min";
+import "./chart-area-demo";
+import "./chart-pie-demo";
+
+import "./jquery.dataTables.min";
+import "./dataTables.bootstrap4.min";
+
+import "./datatables-demo";
