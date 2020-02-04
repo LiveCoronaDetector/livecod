82c457bd4233d0ab8dbdf795c9ad5970ca5c83d5
Taeuk Kang <mail@taeuk.me>
2020-02-04 19:15:35 +0900

    Edit webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
-
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
@@ -10,20 +10,20 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new CopyPlugin([
-      { from: 'public/assets', to: 'assets' },
-    ]),
     new HtmlWebpackPlugin({
       title: "코로나 맵",
       template: "public/index.html"
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
     })
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    contentBase: path.join(__dirname, "public")
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: '/public'
+    path: path.resolve(__dirname, "dist")
   }
 };
