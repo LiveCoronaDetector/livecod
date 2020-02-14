32e63a98d36473324cde5db9eafd7cb0ca80b261
nugurejeil <nugurejeil1@gmail.com>
2020-02-14 16:37:42 +0900

    세계지도 자동화

--- js/worldmap.js
+++ js/worldmap.js
@@ -12,231 +12,78 @@ window.onload = function () {
   var worldMapWrapper = document.querySelector(".world-map");
   var worldMap = document.querySelector("#objWorldmap");
   var svgDocument = worldMap.getSVGDocument();
-  var infected = svgDocument.querySelectorAll(".click");
+  var land = svgDocument.querySelectorAll(".land");
   var textBox = document.createElement("div");
   var markerData = _marker;
   var countryData; // svg 국가명 매칭 함수
 
-  function matchingCountry(identity) {
-    switch (identity) {
-      case 'CN':
-        markerData.forEach(function (item) {
-          if (item.Name === "중국") {
-            countryData = item;
-          }
-        });
-        break;
+  // 확진자 수에 따른 svg 클래스 삽입 함수
+  function countryMarker(data, land){
+    for(var i=0; i<data.length; i++){
+      for(var j=0; j<land.length; j++){
+        //marker.js의 국가 영어이름과 svg엘레먼트의 title속성값이 같다면 확진자가 존재하는 국가
+        if(data[i].Name_en === land[j].getAttribute("title")){
 
-      case 'TW':
-        markerData.forEach(function (item) {
-          if (item.Name === "대만") {
-            countryData = item;
+          // 완치자 수가 있으면 카운터는 확진자수 - 완치자수
+          if(data[i].완치자수){
+            var counter = data[i].확진자수 - data[i].완치자수;
+          }else{
+            var counter = data[i].확진자수;
           }
-        });
-        break;
+          land[j].setAttribute("class", land[j].getAttribute("class") + " infected");
 
-      case 'TH':
-        markerData.forEach(function (item) {
-          if (item.Name === "태국") {
-            countryData = item;
+          // 카운터 수에 따른 클래스 삽입
+          if(counter < 10){  
+            land[j].setAttribute("class", land[j].getAttribute("class") + " low");
+          }else if(counter >= 10 && counter < 20){
+            land[j].setAttribute("class", land[j].getAttribute("class") + " middle");
+          }else if(counter >= 20 && counter < 30){
+            land[j].setAttribute("class", land[j].getAttribute("class") + " high");
+          }else if(counter >= 30 && counter < 40){
+            land[j].setAttribute("class", land[j].getAttribute("class") + " veryhigh");
+          }else if(counter >= 40){
+            land[j].setAttribute("class", land[j].getAttribute("class") + " highest");
           }
-        });
-        break;
-
-      case 'JP':
-        markerData.forEach(function (item) {
-          if (item.Name === "일본") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'VN':
-        markerData.forEach(function (item) {
-          if (item.Name === "베트남") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'NP':
-        markerData.forEach(function (item) {
-          if (item.Name === "네팔") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'MY':
-        markerData.forEach(function (item) {
-          if (item.Name === "말레이시아") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'KH':
-        markerData.forEach(function (item) {
-          if (item.Name === "캄보디아") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'LK':
-        markerData.forEach(function (item) {
-          if (item.Name === "스리랑카") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'AE':
-        markerData.forEach(function (item) {
-          if (item.Name === "아랍에미리트") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'IN':
-        markerData.forEach(function (item) {
-          if (item.Name === "인도") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'PH':
-        markerData.forEach(function (item) {
-          if (item.Name === "필리핀") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'US':
-        markerData.forEach(function (item) {
-          if (item.Name === "미국") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'CA':
-        markerData.forEach(function (item) {
-          if (item.Name === "캐나다") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'FR':
-        markerData.forEach(function (item) {
-          if (item.Name === "프랑스") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'DE':
-        markerData.forEach(function (item) {
-          if (item.Name === "독일") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'FI':
-        markerData.forEach(function (item) {
-          if (item.Name === "핀란드") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'IT':
-        markerData.forEach(function (item) {
-          if (item.Name === "이탈리아") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'GB':
-        markerData.forEach(function (item) {
-          if (item.Name === "영국") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'RU':
-        markerData.forEach(function (item) {
-          if (item.Name === "러시아") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'SE':
-        markerData.forEach(function (item) {
-          if (item.Name === "스웨덴") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'ES':
-        markerData.forEach(function (item) {
-          if (item.Name === "스페인") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'AU':
-        markerData.forEach(function (item) {
-          if (item.Name === "호주") {
-            countryData = item;
-          }
-        });
-        break;
-
-      case 'KR':
-        markerData.forEach(function (item) {
-          if (item.Name === "한국") {
-            countryData = item;
-          }
-        });
-        break;
+        }
+      }
     }
-    if(countryData.완치자수){
-      textBox.innerHTML = 
-      '<dl class="list-countryInfo"><div><dt>국명 :</dt><dd>' + 
-      countryData.Name + '(' + countryData.Name_en + 
-      ')</dd></div><div><dt>확진자 :</dt><dd>' 
-      + countryData.확진자수 + 
-      '</dd></div><div><dt>사망자 :</dt><dd>' 
-      + countryData.사망자수 + 
-      '</dd></div><div><dt>완치자 :</dt><dd>' 
-      + countryData.완치자수 + 
-      '</dd></div></dl>';
-    }else{
-      textBox.innerHTML = 
-      '<dl class="list-countryInfo"><div><dt>국명 :</dt><dd>' + 
-      countryData.Name + '(' + countryData.Name_en + 
-      ')</dd></div><div><dt>확진자 :</dt><dd>' 
-      + countryData.확진자수 + 
-      '</dd></div><div><dt>사망자 :</dt><dd>' 
-      + countryData.사망자수 + 
-      '</dd></div><div><dt>완치자 :</dt><dd> 0</dd></div></dl>';
+  }
+  countryMarker(markerData, land);
+
+  var infected = svgDocument.querySelectorAll(".infected");
+
+  function matchingCountry(identity) {
+    
+    for(var i=0; i<markerData.length; i++){
+      if(markerData[i].Name_en == identity){
+        if(markerData[i].완치자수){
+          textBox.innerHTML = 
+          '<dl class="list-countryInfo"><div><dt>국명 :</dt><dd>' + 
+          markerData[i].Name + '(' + markerData[i].Name_en + 
+          ')</dd></div><div><dt>확진자 :</dt><dd>' 
+          + markerData[i].확진자수 + 
+          '</dd></div><div><dt>사망자 :</dt><dd>' 
+          + markerData[i].사망자수 + 
+          '</dd></div><div><dt>완치자 :</dt><dd>' 
+          + markerData[i].완치자수 + 
+          '</dd></div></dl>';
+        }else{
+          textBox.innerHTML = 
+          '<dl class="list-countryInfo"><div><dt>국명 :</dt><dd>' + 
+          markerData[i].Name + '(' + markerData[i].Name_en + 
+          ')</dd></div><div><dt>확진자 :</dt><dd>' 
+          + markerData[i].확진자수 + 
+          '</dd></div><div><dt>사망자 :</dt><dd>' 
+          + markerData[i].사망자수 + 
+          '</dd></div><div><dt>완치자 :</dt><dd> 0</dd></div></dl>';
+        }
+      }
     }
   }
 
   var _loop = function _loop(i) {
     infected[i].addEventListener("click", function (e) {
-      var identity = infected[i].id;
+      var identity = infected[i].getAttribute("title");
       matchingCountry(identity);
       textBox.className = identity + " mapTextbox";
       textBox.style.left = e.pageX - 90 + "px";
