b0506278daa699c482abc1c6e2898052168d583e
paullabkorea <paullabkorea@gmail.com>
2020-02-24 20:45:00 +0900

    test

--- js/koreaMap.js
+++ js/koreaMap.js
@@ -1,6 +1,6 @@
-var koreamapOptions = {
-    center: new naver.maps.LatLng(36.158971, 126.7969388),
-    zoom: 7
-};
-
-var koreamap = new naver.maps.Map('koreaMap', koreamapOptions);
+// var koreamapOptions = {
+//     center: new naver.maps.LatLng(36.158971, 126.7969388),
+//     zoom: 7
+// };
+//
+// var koreamap = new naver.maps.Map('koreaMap', koreamapOptions);
