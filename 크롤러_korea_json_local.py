4f2477d2db9e3215e429a8040d1a1f02128ac48f
paullabkorea <paullabkorea@gmail.com>
2020-03-08 15:55:06 +0900

    크롤러 업데이트

---
+++ 크롤러_korea_json_local.py
@@ -0,0 +1,41 @@
+#이 파일은 코로나맵을 만드는 분들 중 크롤링을 하시려는 분들의 학습용으로 만들어져 있습니다.
+
+import requests
+import re
+from bs4 import BeautifulSoup
+import json
+
+html = requests.get("http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun=").text
+# print(html)
+
+soup = BeautifulSoup(html, 'html.parser')
+업데이트날짜 = soup.select('.timetable > .info > span')[0].text
+# print(업데이트날짜)
+
+# datas = soup.select('#maplayout > button')
+datas = soup.select('.maplist > ul > li')
+# print(datas)
+
+시도별확진자 = []
+
+for d in datas:
+    지역이름 = d.find_all('strong')[0].text
+    확진자수 = int(d.find_all('span', class_='sub_num')[0].text[:-1])
+    사망자수 = int(d.find_all('span', class_='sub_num')[1].text[:-1])
+
+    # print(f'지역이름 : {지역이름}')
+    # print(f'확진자수 : {확진자수}')
+    # print(f'사망자수 : {사망자수}')
+
+    시도별확진자.append({
+        '지역이름' : 지역이름,
+        '확진자수' : 확진자수,
+        '사망자수' : 사망자수,
+    })
+
+#삭제된 데이터 확인
+print(f'삭제된 데이터 : {시도별확진자[0]}')
+시도별확진자.append({'업데이트날짜': 업데이트날짜})
+
+with open("크롤러_지역별현황.json", "w", encoding='UTF-8-sig') as json_file:
+    json.dump(시도별확진자[1:], json_file, ensure_ascii=False, indent=4)
---
+++ 크롤러_world_json.py
@@ -0,0 +1,33 @@
+#이 파일은 코로나맵을 만드는 분들 중 크롤링을 하시려는 분들의 학습용으로 만들어져 있습니다.
+
+import requests
+import re
+from bs4 import BeautifulSoup
+import json
+
+html = requests.get("https://www.worldometers.info/coronavirus/").text
+# print(html) 작동확인 1
+soup = BeautifulSoup(html, 'html.parser')
+datas = soup.select('#main_table_countries > tbody > tr')
+# print(datas) 작동확인 2
+세계확진자 = []
+
+for d in datas:
+    국가이름 = d.find_all('td')[0].text
+    확진자 = d.find_all('td')[1].text
+    사망자 = d.find_all('td')[3].text
+    완치자 = d.find_all('td')[5].text
+    # print(f'국가이름 : {국가이름}')
+    # print(f'확진자 : {확진자}')
+    # print(f'사망자 : {사망자}')
+    # print(f'완치자 : {완치자}')
+    세계확진자.append({
+        '국가이름' : 국가이름.strip(),
+        '확진자' : int(0 if 확진자.strip().replace(',', '') == "" else 확진자.strip().replace(',', '')),
+        '사망자' : int(0 if 사망자.strip().replace(',', '') == "" else 사망자.strip().replace(',', '')),
+        '완치자' : int(0 if 완치자.strip().replace(',', '') == "" else 완치자.strip().replace(',', '')),
+    })
+
+with open("세계확진자.json", "w", encoding='UTF-8-sig') as json_file:
+    json.dump(세계확진자, json_file, ensure_ascii=False, indent=4)
+    # file.write(json.dumps(dict, ensure_ascii=False))
