93298ab6c39d9ba09d0e36329b684a99728093bf
hyeonukdev <lego7407@gmail.com>
2020-03-15 18:45:55 +0900

    top15_tooltip

--- js/chart/chart-pie.js
+++ js/chart/chart-pie.js
@@ -8,6 +8,9 @@ var 모든확진자수 = marker.reduce(function (a, b) {
   // console.log(a, b.확진자수)
   return a + (b.확진자수 || 0);
 }, 0);
+var top확진자 = document.getElementsByClassName("top확진자");
+var top완치자 = document.getElementsByClassName("top완치자");
+var top사망자 = document.getElementsByClassName("top사망자");
 var topText = document.querySelectorAll("#Top5>h4");
 var top확진자 = document.querySelectorAll("#Top5>div>.bg-warning");
 var top완치자 = document.querySelectorAll("#Top5>div>.bg-success");
@@ -21,16 +24,19 @@ for (var i = 0; i < 15; i++) {
   var percentage = marker[i].확진자수 / 모든확진자수 * 100;
   var TOP5_치명율 = (marker[i].사망자수 / marker[i].확진자수 * 100).toFixed(2);
   var TOP5_완치율 = (marker[i].완치자수 / marker[i].확진자수 * 100).toFixed(2);
-  topText[i].innerHTML = String(i+1) + '. ' +
-  marker[i].Name +
-  ' : ' +
-  Number(marker[i].확진자수).toLocaleString() +
-  "<span class='float-right'>" +
-  Math.round(percentage * 100) / 100 + "%" +
-  "(치명률 : " + TOP5_치명율 +
-  "%, 완치율 : "  + TOP5_완치율 + "%)" +
-  "</span>";
+  topText[i].innerHTML = String(i + 1) + '. ' +
+    marker[i].Name +
+    ' : ' +
+    Number(marker[i].확진자수).toLocaleString() +
+    "<span class='float-right'>" +
+    Math.round(percentage * 100) / 100 + "%" +
+    "(치명률 : " + TOP5_치명율 +
+    "%, 완치율 : " + TOP5_완치율 + "%)" +
+    "</span>";
   top확진자[i].style.width = percentage / 100 * (100 - TOP5_완치율 - TOP5_치명율) + "%";
   top완치자[i].style.width = percentage / 100 * TOP5_완치율 + "%";
   top사망자[i].style.width = percentage / 100 * TOP5_치명율 + "%";
+  top확진자[i].title = marker[i].확진자수.toLocaleString() + "명";
+  top완치자[i].title = marker[i].완치자수.toLocaleString() + "명";
+  top사망자[i].title = marker[i].사망자수.toLocaleString() + "명";
 }
