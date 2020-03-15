3d8ee71937aecf579d2383687e574c9a785432d7
hyeonukdev <lego7407@gmail.com>
2020-03-15 19:03:31 +0900

    top15_tooltip

--- js/chart/chart-pie.js
+++ js/chart/chart-pie.js
@@ -36,7 +36,7 @@ for (var i = 0; i < 15; i++) {
   top확진자[i].style.width = percentage / 100 * (100 - TOP5_완치율 - TOP5_치명율) + "%";
   top완치자[i].style.width = percentage / 100 * TOP5_완치율 + "%";
   top사망자[i].style.width = percentage / 100 * TOP5_치명율 + "%";
-  top확진자[i].title = "확진자" + marker[i].확진자수.toLocaleString() + "명";
-  top완치자[i].title = "완치자" + marker[i].완치자수.toLocaleString() + "명";
-  top사망자[i].title = "사망자" + marker[i].사망자수.toLocaleString() + "명";
+  top확진자[i].title = "확진자 " + marker[i].확진자수.toLocaleString() + "명";
+  top완치자[i].title = "완치자 " + marker[i].완치자수.toLocaleString() + "명";
+  top사망자[i].title = "사망자 " + marker[i].사망자수.toLocaleString() + "명";
 }
