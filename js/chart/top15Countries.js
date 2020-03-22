5577d3692e56c36a8f4d569e81ab7d7803fc772d
Taeuk Kang <mail@taeuk.me>
2020-03-22 11:28:15 +0900

    Fix stacked bar chart values
    Previously, the stacked bar chart showed stats of each country's infected, dead, and recovered. However, since the total amount of the chart reflects the infected, what should actually be shown is the "in quarantine" value. This value is now reflected in the chart in place of the infected value.

--- js/chart/top15Countries.js
+++ js/chart/top15Countries.js
@@ -1,9 +1,3 @@
-"use strict";
-
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796'; // Pie Chart Example
-
 var 모든확진자수 = marker.reduce(function (a, b) {
   // console.log(a, b.확진자수)
   return a + (b.확진자수 || 0);
@@ -25,9 +19,11 @@ for (var i = 0; i < 15; i++) {
 
   var percentage = cases_count / 모든확진자수 * 100,                                      //나라별 비율
       TOP5_fatality_rate = (dead_toll_count / cases_count * 100).toFixed(2),          // 치명율
-      TOP5_full_recovery_rate = (full_recovery_count / cases_count * 100).toFixed(2), // 완치율
-      rate_total = dead_toll_count + cases_count + full_recovery_count,               //확진자수 + 사망자수 + 완치자수
-      rate_cases = cases_count / rate_total * 100,                                    //확진자수 비율
+      TOP5_full_recovery_rate = (full_recovery_count / cases_count * 100).toFixed(2), // 완치율,
+      quarantine_count = cases_count - full_recovery_count - dead_toll_count,            // 격리중 (치료중) = 확진자 - 완치자 - 사망자
+      rate_total = quarantine_count + dead_toll_count + full_recovery_count,               //격리자수 + 사망자수 + 완치자수
+      rate_cases = cases_count / rate_total * 100,                                    //확진자수 비율,
+      rate_quarantine = quarantine_count / rate_total * 100,                           //격리자수 비율
       rate_death_toll = dead_toll_count / rate_total * 100,                           //사망자수 비율
       rate_full_recovery = full_recovery_count / rate_total * 100;                    //완치자수 비율
       
@@ -39,11 +35,11 @@ for (var i = 0; i < 15; i++) {
     +   "완치율 : <span class='bold'>" + TOP5_full_recovery_rate + "</span>%" 
     + "</div>"; //치명률, 완치율
 
-  topCases[i].style.width = rate_cases + "%";               //확진자비율 - 그래프값
-  topFullRecover[i].style.width = rate_full_recovery + "%"; //완치자비율 - 그래프값
-  topDeathToll[i].style.width = rate_death_toll + "%";      //사망자비율 - 그래프값
+  topCases[i].style.width = rate_quarantine + "%";               //격리자비율 - 그래프값
+  topFullRecover[i].style.width = rate_full_recovery + "%";      //완치자비율 - 그래프값
+  topDeathToll[i].style.width = rate_death_toll + "%";           //사망자비율 - 그래프값
 
-  topCases[i].title = "확진자 " + cases_count.toLocaleString() + "명";                //TOP 확진자
+  topCases[i].title = "격리자 " + quarantine_count.toLocaleString() + "명";                //TOP 격리자
   topFullRecover[i].title = "완치자 " + full_recovery_count.toLocaleString() + "명";  //TOP 완치자
   topDeathToll[i].title = "사망자 " + dead_toll_count.toLocaleString() + "명";        //TOP 사망자
 }
