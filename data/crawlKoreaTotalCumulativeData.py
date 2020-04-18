4c4d4e2c64d9e73385f6ec688e47143304ff093e
Jake Tae <jaesungtae@gmail.com>
2020-04-18 17:35:39 +0900

    feature: make JSON reading more efficient

--- data/crawlKoreaTotalCumulativeData.py
+++ data/crawlKoreaTotalCumulativeData.py
@@ -8,17 +8,14 @@ from utils import write_data
 
 
 def get_past_data():
-    data = ""
-    with open('./data/koreaTotalCumulativeData.js', 'r', encoding='UTF-8-sig') as f:
-        _ = f.readline()
-        line = f.readline()[34:]
-        while line:
-            data += line
-            line = f.readline()
-
-    semi_colon = data.index(';')
-
-    return json.loads(data[:semi_colon].replace('\n', '').replace('\t', ''))
+    with open("./data/koreaTotalCumulativeData.js", "r", encoding="UTF-8-sig") as f:
+        data = f.read()
+        obj = (
+            data[data.find("[") : data.rfind("]") + 1]
+            .replace("\n", "")
+            .replace("\t", "")
+        )
+    return json.loads(obj)
 
 
 def get_today_data(url, data):
@@ -30,16 +27,16 @@ def get_today_data(url, data):
         source = html.read()
         html.close()
 
-        soup = BeautifulSoup(source, 'html.parser')
+        soup = BeautifulSoup(source, "html.parser")
         tables = soup.find("div", class_="data_table mgt16").find_all("td")
 
         num = [element.get_text() for element in tables]
 
         before_tot = data[-1][1]
-        today_tot = int(num[0].replace(',', ''))
+        today_tot = int(num[0].replace(",", ""))
         diff = today_tot - before_tot
         death = int(num[3])
-        release = int(num[1].replace(',', ''))
+        release = int(num[1].replace(",", ""))
 
         data.append([day, today_tot, diff, death, release])
 
@@ -52,9 +49,9 @@ def run():
     past_data = get_past_data()
     data = get_today_data(url, past_data)
 
-    save_dir = './data/koreaTotalCumulativeData.js'
-    crawler_name = 'crawlKoreaTotalCumulativeData.py'
-    var_name = 'koreaRegionalCumulativeData'
+    save_dir = "./data/koreaTotalCumulativeData.js"
+    crawler_name = "crawlKoreaTotalCumulativeData.py"
+    var_name = "koreaRegionalCumulativeData"
 
     write_data(data, save_dir, crawler_name, var_name)
 
