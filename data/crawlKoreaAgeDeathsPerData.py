2ed55120bc855bdd4674cbde65d3217329677860
Jake Tae <jaesungtae@gmail.com>
2020-04-19 23:05:48 +0900

    fix: resolve crawler error

--- data/crawlKoreaAgeDeathsPerData.py
+++ data/crawlKoreaAgeDeathsPerData.py
@@ -8,9 +8,9 @@ def get_data(url):
     ageDeathsPer = []
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
-    data = soup.select('.num > tbody > tr')
+    data = soup.select('.num > tbody > tr')[-9:]
 
-    for datum in data[4:]:
+    for datum in data:
         temp = [i.text.replace('\xa0', '') for i in datum]
         ageDeathsPer.append({
             '구분': temp[0],
@@ -31,6 +31,7 @@ def run():
     var_name = 'ageDeathsPer'
 
     write_data(ageDeathsPer, save_dir, crawler_name, var_name)
+
 
 print("#####################################")
 print("############ 한국 나이별 치명율 데이터 #############")
