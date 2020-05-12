767188a4ca7884e901a0018c649d5d6966743a22
Jake Tae <jaesungtae@gmail.com>
2020-05-13 07:58:03 +0900

    feature: optimize control flow

--- data/crawlKoreaRegionalCumulativeData.py
+++ data/crawlKoreaRegionalCumulativeData.py
@@ -1,29 +1,31 @@
 from utils import get_raw_data, write_data
 
 
-def extract_data(total_data):
-    index = total_data[0].index('2/1/20')  # 14
-    korea_date = total_data[0][index:]
-    korea_date = [i[:-3] for i in korea_date]
-
+def extract_data(total_data, return_dates=False):
     for i in total_data:
         if i[1] == 'Korea, South':
-            korea_data = i[index:]
+            korea_data = list(map(int, i[index:]))
             break
-
-    return korea_data, korea_date
+    if return_dates:
+        index = total_data[0].index('2/1/20')
+        korea_date = total_data[0][index:]
+        korea_date = [i[:-3] for i in korea_date]
+        return korea_data, korea_date
+    return korea_data
 
 
 def build_result(korea_data):
-    result = [[korea_data[0][0], int(korea_data[0][1]), 1, int(
-        korea_data[0][2]), int(korea_data[0][3])]]
-    for i, _ in enumerate(korea_data[1:]):
+    result = []
+    for i, _ in enumerate(korea_data):
+        if i == 1:
+            diff = 1
+        else:
+            diff = korea_data[i][0] - korea_data[i - 1][0]
         result.append([
             korea_data[i][0],
-            int(korea_data[i][1]),
-            int(korea_data[i][1]) - int(korea_data[i - 1][1]),
-            int(korea_data[i][2]),
-            int(korea_data[i][3])
+            diff,
+            korea_data[i][1],
+            korea_data[i][2]
         ])
     return result
 
@@ -37,13 +39,12 @@ def run():
     deaths_total_data = get_raw_data(deaths_CSV_URL)
     recovered_total_data = get_raw_data(recovered_CSV_URL)
 
-    korea_deaths, _ = extract_data(confirmed_total_data)
-    korea_recovered, _ = extract_data(recovered_total_data)
-    korea_confirmed, korea_deaths_date = extract_data(deaths_total_data)
-    korea_data = list(zip(korea_deaths_date, korea_confirmed,
-                          korea_deaths, korea_recovered))
+    korea_deaths = extract_data(confirmed_total_data)
+    korea_recovered = extract_data(recovered_total_data)
+    korea_confirmed, korea_deaths_date = extract_data(deaths_total_data, return_dates=True)
+    korea_data = list(zip(korea_confirmed, korea_deaths, korea_recovered))
 
-    result = build_result(korea_data)
+    result = list(zip(korea_deaths_date, build_result(korea_data)))
 
     save_dir = './data/koreaRegionalCumulativeData.js'
     crawler_name = 'crawlKoreaRegionalCumulativeData.py'
