25c6892796d1ee43412d13a4fe7c44a87c0dc2c2
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-07 03:01:37 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,12 +5,20 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200607015752530?did=1195m"
     },
     {
-        "title": "‘코로나19’ 신규 확진자 57명 추가…수도권만 52명",
-        "link": "https://www.donga.com/news/article/all/20200607/101394732/1"
+        "title": "코로나 신규 확진 57명… 이틀째 50명대(종합)",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/07/2020060700339.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
+        "title": "[속보]코로나19 신규 확진자 57명···지역발생 53명",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006071030001&code=940601"
     },
     {
         "title": "전세계 코로나19 확진자 700만명·사망자 40만명",
         "link": "http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202006070015&t=NN"
+    },
+    {
+        "title": "[속보] 코로나19 확진 57명 늘어…52명은 ‘수도권’",
+        "link": "http://www.hani.co.kr/arti/society/health/948202.html"
     },
     {
         "title": "“코로나 사태 99일만”...8일 중 1·초 5~6학년 등교 수업",
@@ -25,19 +33,11 @@ var koreaNewsData = [
         "link": "http://www.fnnews.com/news/202006071028472068"
     },
     {
-        "title": "코로나 신규 확진 57명… 이틀째 50명대(종합)",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/07/2020060700339.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "‘코로나19’ 신규 확진자 57명 추가…수도권만 52명",
+        "link": "https://www.donga.com/news/article/all/20200607/101394732/1"
     },
     {
-        "title": "코로나19 신규 확진자 51명 늘어…수도권 42명",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4464327&ref=A"
-    },
-    {
-        "title": "[속보]코로나19 신규 확진자 57명···지역발생 53명",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006071030001&code=940601"
-    },
-    {
-        "title": "\"전 세계 코로나19 확진자 700만 명 넘어…사망은 40만 명\"",
-        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005823428&plink=ORI&cooper=NAVER"
+        "title": "안양시청 \"코로나19 41번 확진자 발생\"[속보]",
+        "link": "http://www.kjdaily.com/read.php3?aid=1591497963513083208"
     }
 ];
