788f5873621ee74b405098bec44bd5bcade7f06b
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-08 10:01:09 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,20 +5,12 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200608052452530?did=1195m"
     },
     {
-        "title": "인천 '거짓말' 학원강사 코로나 완치 후 재입원…경찰 조사 예정",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/08/2020060801897.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "먼저 코로나 종식 선언합니다, 환자 제로 이 국가",
+        "link": "https://news.chosun.com/site/data/html_dir/2020/06/08/2020060801398.html?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
         "title": "[속보]코로나19 신규 확진자 38명···지역발생 33명",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006081026001&code=940601"
-    },
-    {
-        "title": "뉴질랜드 코로나19 종식 선언…감염자 0명",
-        "link": "https://www.nocutnews.co.kr/news/5357478"
-    },
-    {
-        "title": "[속보] 코로나 가리봉으로 번지나, 중국동포 쉼터 8명 확진",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/06/08/2020060802344.html?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
         "title": "서울 확진자 1천명 돌파…中동포교회쉼터 무더기 감염(종합)",
@@ -33,11 +25,19 @@ var koreaNewsData = [
         "link": "http://www.hani.co.kr/arti/area/yeongnam/948406.html"
     },
     {
+        "title": "인천 '거짓말' 학원강사 코로나 완치 후 재입원…경찰 조사 예정",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/08/2020060801897.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
         "title": "[단독] 코로나 수도권 집단감염 여파…'여행주간' 연기 논의",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=03093046625799752"
     },
     {
-        "title": "롯데월드 방문 확진자…정부 “진단검사보다 거리두기 중요”",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200608500014&wlog_tag3=naver"
+        "title": "서울 구로구 중국동포쉼터서 확진자 8명 발생",
+        "link": "https://www.donga.com/news/article/all/20200608/101413638/1"
+    },
+    {
+        "title": "서울시 탁구장 운영 자제 권고…코로나19 확진자 서울 997명",
+        "link": "https://imnews.imbc.com/news/2020/society/article/5802455_32633.html"
     }
 ];
