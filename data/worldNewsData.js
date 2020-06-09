ff406fac845716421cc88c90c99661a397223caf
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-09 05:01:16 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,8 +17,8 @@ var worldNewsData = [
         "link": "https://news.google.com/articles/CAIiEPEv2xBHZSKW1S7VG1PTJr0qGQgEKhAIACoHCAow2Nb3CjDivdcCMJ_5ngY?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
-        "title": "India Reopens Public Places Even As Coronavirus Cases Rise",
-        "link": "https://news.google.com/articles/CAIiEOqKue5qU1dz3_xK3XlcwV8qFggEKg4IACoGCAow9vBNMK3UCDCvpUk?hl=en-US&gl=US&ceid=US%3Aen"
+        "title": "Coronavirus India live updates: cases, deaths and news, today",
+        "link": "https://news.google.com/articles/CBMiQ2h0dHBzOi8vZW4uYXMuY29tL2VuLzIwMjAvMDYvMDkvb3RoZXJfc3BvcnRzLzE1OTE2NTk1MjhfODMzOTI3Lmh0bWzSAUdodHRwczovL2VuLmFzLmNvbS9lbi8yMDIwLzA2LzA5L290aGVyX3Nwb3J0cy8xNTkxNjU5NTI4XzgzMzkyNy5hbXAuaHRtbA?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
         "title": "Satellite data suggests coronavirus may have hit China earlier: Researchers",
@@ -29,15 +29,15 @@ var worldNewsData = [
         "link": "https://news.google.com/articles/CAIiELMnnIusTDk3Md1QipBRHGkqGQgEKhAIACoHCAow2Nb3CjDivdcCMJ_d7gU?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
+        "title": "Live Coronavirus Updates: New York Reopens",
+        "link": "https://news.google.com/articles/CAIiEFoKkv4-nf1BytjGs_In7ooqFwgEKg8IACoHCAowjuuKAzCWrzww5oEY?hl=en-US&gl=US&ceid=US%3Aen"
+    },
+    {
+        "title": "Pa. Reports 351 New Coronavirus Cases, 10 More Deaths",
+        "link": "https://news.google.com/articles/CCAiC09MNEpGdl9ieDlnmAEB?hl=en-US&gl=US&ceid=US%3Aen"
+    },
+    {
         "title": "Coronavirus: World Bank confirms deepest recession since World War Two",
         "link": "https://news.google.com/articles/CBMiLGh0dHBzOi8vbmV3cy51bi5vcmcvZW4vc3RvcnkvMjAyMC8wNi8xMDY1OTAy0gEA?hl=en-US&gl=US&ceid=US%3Aen"
-    },
-    {
-        "title": "Coronavirus live news: Brazil passes 700,000 infections as WHO warns pandemic is worsening",
-        "link": "https://news.google.com/articles/CAIiEEVHmmounkDOi1ssegZYBhoqFwgEKg4IACoGCAowl6p7MN-zCTClss0G?hl=en-US&gl=US&ceid=US%3Aen"
-    },
-    {
-        "title": "Once the coronavirus epicenter in the U.S., New York City begins to reopen",
-        "link": "https://news.google.com/articles/CAIiEA2AijhZx65-mfsPFu1zQfgqGAgEKg8IACoHCAowjtSUCjC30XQwzqe5AQ?hl=en-US&gl=US&ceid=US%3Aen"
     }
 ];
