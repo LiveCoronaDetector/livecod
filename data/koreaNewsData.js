3996bad3f23e4a02841eb0b34c157774e5a30fd8
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-09 21:01:13 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,28 +5,24 @@ var koreaNewsData = [
         "link": "https://news.chosun.com/site/data/html_dir/2020/06/09/2020060903807.html?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
+        "title": "쿠팡 물류센터·요양원 관련 확진자 2명 사망…누적 276명",
+        "link": "https://www.donga.com/news/article/all/20200609/101431007/2"
+    },
+    {
         "title": "\"6월 지역사회 코로나 확진자 313명 중 303명 수도권 발생\"",
         "link": "https://www.nocutnews.co.kr/news/5358012"
     },
     {
-        "title": "쿠팡 물류센터·요양원 관련 확진자 2명 사망…누적 276명",
-        "link": "https://www.donga.com/news/article/all/20200609/101431007/2"
+        "title": "6월 확진자 97%가 수도권…\"수도권 대유행 우려\"(종합)",
+        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=02660086625800080"
     },
     {
         "title": "[속보]강남대성송파학원 코로나19 확진자 발생…영통구 거주 조리보조원",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006091717001&code=940100"
     },
     {
-        "title": "6월 확진자 97%는 수도권서 발생…고속도로 휴게소도 점검",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4466251&ref=A"
-    },
-    {
         "title": "[속보]코로나19 신규 확진자 '어제와 같은' 38명",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006091024001&code=940601"
-    },
-    {
-        "title": "코로나 확진 38명…수도권 지역발병 33명(종합)",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/09/2020060901256.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
         "title": "(2보)춘천 9번째 코로나19 확진자 발생",
@@ -39,5 +35,9 @@ var koreaNewsData = [
     {
         "title": "서울 오늘 8시간만에 확진자 10명 추가…누계 1천28명",
         "link": "http://yna.kr/AKR20200609164900004?did=1195m"
+    },
+    {
+        "title": "중국동포 쉼터서 코로나19 집단감염…방역 사각지대 '불안불안'",
+        "link": "http://yna.kr/AKR20200608158500530?did=1195m"
     }
 ];
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,27 +17,27 @@ var worldNewsData = [
         "link": "https://news.google.com/articles/CAIiEOiTzthR-Y4pxkUfi-QmtjsqGQgEKhAIACoHCAow2Nb3CjDivdcCMJ-thQY?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
-        "title": "Coronavirus pandemic: Updates from around the world",
-        "link": "https://news.google.com/articles/CBMiUWh0dHBzOi8vd3d3LmNubi5jb20vd29ybGQvbGl2ZS1uZXdzL2Nvcm9uYXZpcnVzLXBhbmRlbWljLTA2LTA5LTIwLWludGwvaW5kZXguaHRtbNIBVWh0dHBzOi8vYW1wLmNubi5jb20vY25uL3dvcmxkL2xpdmUtbmV3cy9jb3JvbmF2aXJ1cy1wYW5kZW1pYy0wNi0wOS0yMC1pbnRsL2luZGV4Lmh0bWw?hl=en-US&gl=US&ceid=US%3Aen"
+        "title": "Coronavirus: How Covid-19 has changed the 'big fat Indian wedding'",
+        "link": "https://news.google.com/articles/CBMiMmh0dHBzOi8vd3d3LmJiYy5jb20vbmV3cy93b3JsZC1hc2lhLWluZGlhLTUyODkyOTY30gE2aHR0cHM6Ly93d3cuYmJjLmNvbS9uZXdzL2FtcC93b3JsZC1hc2lhLWluZGlhLTUyODkyOTY3?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
         "title": "Coronavirus may have been spreading in China since August 2019: study",
         "link": "https://news.google.com/articles/CAIiEO-KkzOzdPtC8UqE8zs1f2gqGAgEKg8IACoHCAowhK-LAjD4ySww69W0BQ?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
+        "title": "Coronavirus News: Live Updates",
+        "link": "https://news.google.com/articles/CAIiEPfZAKV3ipIjPvds8Sn-TOMqFwgEKg8IACoHCAowjuuKAzCWrzwwxoIY?hl=en-US&gl=US&ceid=US%3Aen"
+    },
+    {
         "title": "WHO walks back comments on asymptomatic coronavirus spread, says much is still unknown",
         "link": "https://news.google.com/articles/CAIiEOqSOciI2mEH-XeCZutaSF0qGQgEKhAIACoHCAow2Nb3CjDivdcCMJ_5ngY?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
-        "title": "Global Coronavirus Caseload Passes 7 Million",
-        "link": "https://news.google.com/articles/CAIiEFoKkv4-nf1BytjGs_In7ooqFwgEKg8IACoHCAowjuuKAzCWrzwwt4QY?hl=en-US&gl=US&ceid=US%3Aen"
+        "title": "Coronavirus pandemic: Updates from around the world",
+        "link": "https://news.google.com/articles/CBMiUWh0dHBzOi8vd3d3LmNubi5jb20vd29ybGQvbGl2ZS1uZXdzL2Nvcm9uYXZpcnVzLXBhbmRlbWljLTA2LTA5LTIwLWludGwvaW5kZXguaHRtbNIBVWh0dHBzOi8vYW1wLmNubi5jb20vY25uL3dvcmxkL2xpdmUtbmV3cy9jb3JvbmF2aXJ1cy1wYW5kZW1pYy0wNi0wOS0yMC1pbnRsL2luZGV4Lmh0bWw?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
         "title": "Dr. Anthony Fauci says coronavirus turned 'out to be my worst nightmare' and it 'isn't over'",
         "link": "https://news.google.com/articles/CAIiEKfM1bfcscaEfdRQUHWEJXkqGQgEKhAIACoHCAow2Nb3CjDivdcCMJ_5ngY?hl=en-US&gl=US&ceid=US%3Aen"
-    },
-    {
-        "title": "Coronavirus: more than a dozen US states see record high of new cases",
-        "link": "https://news.google.com/articles/CAIiEHkwehkeef5-b1dHgFQQo2IqFggEKg4IACoGCAowl6p7MN-zCTDlkko?hl=en-US&gl=US&ceid=US%3Aen"
     }
 ];
