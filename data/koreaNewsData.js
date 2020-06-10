dcd8dd96c146075f6ca8ee072478b1e79a6d3469
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-10 11:01:18 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -17,27 +17,27 @@ var koreaNewsData = [
         "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005828706&plink=ORI&cooper=NAVER"
     },
     {
-        "title": "삼성전자 수원 연구동에서 코로나 확진자 발생.. 건물 폐쇄",
-        "link": "https://www.sedaily.com/NewsView/1Z3Z1VDV9T"
+        "title": "삼성전자 수원사업장, 청소업체 여직원 코로나19 확진으로 폐쇄(종합)",
+        "link": "http://www.newsis.com/view/?id=NISX20200610_0001055398&cID=13001&pID=13000"
     },
     {
-        "title": "\"쿠팡, 코로나19 확진자에 사과·책임져야\" 청와대 청원 올라와",
-        "link": "http://yna.kr/AKR20200610116400030?did=1195m"
+        "title": "“쿠팡 확진자 은폐로 남편 사경 헤매” 청와대 청원까지 나온 쿠팡책임론",
+        "link": "https://www.sedaily.com/NewsView/1Z3Z1SOYR7"
     },
     {
         "title": "코로나19 확진자 사흘만에 50명으로...수도권 41명[종합]",
         "link": "http://www.fnnews.com/news/202006101017320517"
     },
     {
-        "title": "6월 10일, ‘코로나19’ 전세계 확진자 715만명…입국금지·검역 조치 184개국...",
-        "link": "http://digitalchosun.dizzo.com/site/data/html_dir/2020/06/10/2020061080216.html"
-    },
-    {
         "title": "‘코로나19’ 확진자 머리 손질한 미용실 직원도 감염",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006100954001&code=940100"
     },
     {
-        "title": "공개 기한 지난 코로나19 확진자 동선 정보 삭제",
-        "link": "http://news.ebs.co.kr/ebsnews/allView/20300123/N"
+        "title": "삼성전자 수원사업장서 확진자 발생… 1200여 명 재택근무 돌입",
+        "link": "http://www.busan.com/view/busan/view.php?code=2020061017200703415"
+    },
+    {
+        "title": "서울시 코로나19 확진자 8시간만에 12명 추가… 누계 1041명(상보)",
+        "link": "https://view.asiae.co.kr/article/2020061019404662289"
     }
 ];
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,16 +5,16 @@ var worldNewsData = [
         "link": "https://news.google.com/articles/CBMiQ2h0dHBzOi8vZW4uYXMuY29tL2VuLzIwMjAvMDYvMTAvb3RoZXJfc3BvcnRzLzE1OTE3NjgwMDZfNDg0MTc5Lmh0bWzSAUdodHRwczovL2VuLmFzLmNvbS9lbi8yMDIwLzA2LzEwL290aGVyX3Nwb3J0cy8xNTkxNzY4MDA2XzQ4NDE3OS5hbXAuaHRtbA?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
-        "title": "Surging Coronavirus Cases Push Latin America ‘to the Limit’",
-        "link": "https://news.google.com/articles/CAIiEPfZAKV3ipIjPvds8Sn-TOMqFwgEKg8IACoHCAowjuuKAzCWrzww5oEY?hl=en-US&gl=US&ceid=US%3Aen"
+        "title": "Live updates: Nine states report significant uptick in coronavirus hospitalizations after Memorial Day",
+        "link": "https://news.google.com/articles/CAIiEMfDxYrV0dMD55N3DV_j5AoqGAgEKg8IACoHCAowjtSUCjC30XQwzqe5AQ?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
         "title": "Global report: WHO urges Pakistan to return to lockdown as hospitals struggle",
         "link": "https://news.google.com/articles/CAIiEBzaD494sL0jF_sTopmi_UoqFggEKg4IACoGCAowl6p7MN-zCTDlkko?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
-        "title": "Its Defenses Undone by a Virus, France Seeks Lessons From a Lost War",
-        "link": "https://news.google.com/articles/CAIiEG9CXZuqMDUlX_Wn4n8KWC0qFwgEKg8IACoHCAowjuuKAzCWrzww14QY?hl=en-US&gl=US&ceid=US%3Aen"
+        "title": "Covid-19: France's daily death toll stays below 100 for seventh day in a row",
+        "link": "https://news.google.com/articles/CAIiEFFrz-FV4sbm6A5pqgleocoqGQgEKhAIACoHCAowwrSiCTCFgpgCMPjU3wM?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
         "title": "The World Reopens, Despite Skyrocketing Coronavirus Cases",
@@ -29,8 +29,8 @@ var worldNewsData = [
         "link": "https://news.google.com/articles/CAIiEHCxgwkBXR3_ttXmsGE3-LkqGQgEKhAIACoHCAowocv1CjCSptoCMLrUpgU?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
-        "title": "June 9 coronavirus news",
-        "link": "https://news.google.com/articles/CBMiUWh0dHBzOi8vd3d3LmNubi5jb20vd29ybGQvbGl2ZS1uZXdzL2Nvcm9uYXZpcnVzLXBhbmRlbWljLTA2LTA5LTIwLWludGwvaW5kZXguaHRtbNIBVWh0dHBzOi8vYW1wLmNubi5jb20vY25uL3dvcmxkL2xpdmUtbmV3cy9jb3JvbmF2aXJ1cy1wYW5kZW1pYy0wNi0wOS0yMC1pbnRsL2luZGV4Lmh0bWw?hl=en-US&gl=US&ceid=US%3Aen"
+        "title": "Loosened coronavirus lockdowns spark new concerns in world's poorest regions",
+        "link": "https://news.google.com/articles/CAIiEKG3o-P8injSTMNq5Z6mThQqGQgEKhAIACoHCAowwL2ICzCckocDMKi4uQM?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
         "title": "UN warns 'food systems are failing' amid coronavirus pandemic",
