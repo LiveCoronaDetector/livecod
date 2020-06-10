138126d395549b1a3adb54e24ca5545de672bcbd
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-10 06:01:19 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -17,27 +17,27 @@ var koreaNewsData = [
         "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005828706&plink=ORI&cooper=NAVER"
     },
     {
-        "title": "[속보] 코로나19 신규 확진자 50명···사흘 만에 다시 50명대로",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006101026001&code=940601"
-    },
-    {
         "title": "2주 지난 확진자 동선정보, 인터넷서 없앤다…\"카톡 등 사적채널은 미탐지\"(상...",
         "link": "https://view.asiae.co.kr/article/2020061011575094523"
     },
     {
-        "title": "사생활 침해 없앤 코로나19 확진자 추적·관리 시스템 개발",
-        "link": "http://www.docdocdoc.co.kr/news/articleView.html?idxno=1081484"
+        "title": "[속보] 코로나19 신규 확진자 50명···사흘 만에 다시 50명대로",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006101026001&code=940601"
     },
     {
-        "title": "“코로나19 확진자 동선 2차피해 우려” 정부, 삭제 조치 강화",
-        "link": "http://www.hankookilbo.com/news/npath/202006101174744580?did=NA"
+        "title": "확진자 동선 14일 후 인터넷 삭제...특별여행주간 7월1~19일로 축소",
+        "link": "http://www.fnnews.com/news/202006101151352258"
     },
     {
-        "title": "코로나19 확진자 사흘만에 50명으로...수도권 41명[종합]",
-        "link": "http://www.fnnews.com/news/202006101017320517"
+        "title": "김포 확진자 초등학생 아들 격리해제 하루 앞두고 확진",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/10/2020061002067.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
         "title": "'코로나 충격' 취업자 3개월째 감소…실업자 역대 최대(종합)",
         "link": "http://www.newsis.com/view/?id=NISX20200608_0001052119&cID=10401&pID=10400"
+    },
+    {
+        "title": "동작구청 \"사당5동 코로나19 추가확진자 발생\"[속보]",
+        "link": "http://www.kjdaily.com/read.php3?aid=1591760709513541208"
     }
 ];
