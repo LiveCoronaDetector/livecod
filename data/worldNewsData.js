d6c2d49f8d86cfb677db3f32efbd929cf3392885
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-12 23:31:10 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -33,11 +33,11 @@ var worldNewsData = [
         "link": "https://www.nytimes.com/2020/06/12/opinion/coronavirus-global-hunger.html"
     },
     {
-        "title": "Fauci voices support for World Health Organization after Trump terminates US relationship",
-        "link": "https://www.cnn.com/2020/06/11/politics/fauci-world-health-organization-coronavirus/index.html"
+        "title": "Watch live: The World Health Organization holds a press briefing on the coronavirus pandemic",
+        "link": "https://www.cnbc.com/2020/06/12/watch-live-world-health-organization-holds-press-briefing-on-coronavirus-pandemic.html"
     },
     {
-        "title": "Coronavirus update: Global case tally tops 7.6 million as countries push ahead toward phased reopenings — with mixed results",
-        "link": "https://www.marketwatch.com/story/coronavirus-update-global-case-tally-tops-75-million-as-countries-push-ahead-toward-phased-reopenings-with-mixed-results-2020-06-12"
+        "title": "The new rules of buying and selling a house in a coronavirus world",
+        "link": "https://www.cnn.com/2020/06/12/success/real-estate-home-buying-and-selling-coronavirus/index.html"
     }
 ];
