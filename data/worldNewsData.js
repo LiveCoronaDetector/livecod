caf842a5e8ed694a19afffeb4fbba30c4bc29c41
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-16 23:01:04 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -13,12 +13,12 @@ var worldNewsData = [
         "link": "https://www.cnn.com/travel/article/portugal-greece-safest-destination-coronavirus/index.html"
     },
     {
-        "title": "Coronavirus live news: Beijing says outbreak 'extremely severe'; French police fire teargas at protest",
-        "link": "https://www.theguardian.com/world/live/2020/jun/16/coronavirus-live-news-covid-19-update-global-cases-pass-8-million-beijing-china-partial-lockdown-latest-updates"
+        "title": "Dexamethasone Reduces Coronavirus Deaths, Scientists Say",
+        "link": "https://www.nytimes.com/2020/06/16/world/europe/dexamethasone-coronavirus-covid.html"
     },
     {
-        "title": "Covid-19: The woman who has become India's 'corona voice'",
-        "link": "https://www.bbc.com/news/world-asia-india-53047296"
+        "title": "Twenty Indian soldiers dead after clash with China along disputed border",
+        "link": "https://www.cnn.com/2020/06/16/asia/china-india-border-clash-intl-hnk/index.html"
     },
     {
         "title": "Beijing coronavirus outbreak: travel restricted to tackle 'extremely severe' situation",
