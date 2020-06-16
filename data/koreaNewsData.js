2805efd8200f95430534baa3a17b27fb5010f670
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-16 10:01:21 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,6 +5,10 @@ var koreaNewsData = [
         "link": "http://www.fnnews.com/news/202006160956107196"
     },
     {
+        "title": "[단독]강남 룸살롱 다시 문연지 하루만에…직원 코로나 확진",
+        "link": "http://news.mt.co.kr/mtview.php?no=2020061609270550233"
+    },
+    {
         "title": "[속보] 코로나19 확진자 34명 증가···국내 발생 21명",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006161022001&code=940601"
     },
@@ -13,31 +17,27 @@ var koreaNewsData = [
         "link": "https://biz.chosun.com/site/data/html_dir/2020/06/16/2020061601381.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
-        "title": "WHO \"중국 베이징 코로나19 집단감염, 중요 사건\"(종합)",
-        "link": "http://yna.kr/AKR20200616003051088?did=1195m"
-    },
-    {
         "title": "제주 코로나19 16번째 확진자 발생…접촉자 0명",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4471897&ref=A"
-    },
-    {
-        "title": "152명 확진자 쿠팡·추가 확진자 없는 롯데택배…차이점은(종합)",
-        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=03411206625802376"
-    },
-    {
-        "title": "베이징 확진자 100명 넘었다…\"우한보다 전염력 강해\"",
-        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005838086&plink=ORI&cooper=NAVER"
     },
     {
         "title": "\"코로나19 매우 교활해 언제든 전파가능…확진자 증가할수 있어\"(종합)",
         "link": "http://yna.kr/AKR20200616114651530?did=1195m"
     },
     {
-        "title": "[단독]강남 룸살롱 다시 문연지 하루만에…직원 코로나 확진",
-        "link": "http://news.mt.co.kr/mtview.php?no=2020061609270550233"
+        "title": "WHO \"중국 베이징 코로나19 집단감염, 중요 사건\"(종합)",
+        "link": "http://yna.kr/AKR20200616003051088?did=1195m"
+    },
+    {
+        "title": "[종합] 中 코로나19 재확산 '초비상'…다급한 베이징 지도부",
+        "link": "https://www.hankyung.com/international/article/2020061618657"
     },
     {
         "title": "코로나19 확진 34명 늘어…사흘 연속 30명대",
         "link": "http://www.hani.co.kr/arti/society/health/949532.html"
+    },
+    {
+        "title": "베이징 확진자 100명 넘었다…\"우한보다 전염력 강해\"",
+        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005838086&plink=ORI&cooper=NAVER"
     }
 ];
