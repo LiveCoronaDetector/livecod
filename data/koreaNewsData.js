b7329013afa13d08aaf42b4630bd6f00efd8d685
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-17 06:01:43 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,39 +5,39 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200617086551055?did=1195m"
     },
     {
-        "title": "[속보]서울 시청역에서 확진자… 2호선 안전요원 3명 감염",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/06/17/2020061702622.html?utm_source=naver&utm_medium=original&utm_campaign=news"
-    },
-    {
         "title": "위기의 베이징, 엿새만에 확진자 140명 육박",
         "link": "https://biz.chosun.com/site/data/html_dir/2020/06/17/2020061701822.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
+        "title": "“중간고사 보는데” 전주여고 확진자 나와…학생들 귀가(종합)",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200617500099&wlog_tag3=naver"
     },
     {
         "title": "삼성전자 기흥사업장서 확진자 발생…\"공장 정상가동\"",
         "link": "https://imnews.imbc.com/news/2020/econo/article/5813050_32647.html"
     },
     {
+        "title": "코로나19 확진 43명 늘어…수도권 28명, 대전·충남 6명",
+        "link": "http://www.hani.co.kr/arti/society/health/949725.html"
+    },
+    {
         "title": "올들어 사이버 공격 310만건 발생…코로나19 영향",
         "link": "http://www.inews24.com/view/1274091"
     },
     {
-        "title": "[속보]여의도 나이스그룹 사옥서 코로나19 확진자 발생",
-        "link": "https://www.sedaily.com/NewsView/1Z4298X3ZF"
+        "title": "[종합] 코로나19 확진자 43명 증가...대전도 신규 확진자 증가",
+        "link": "http://www.fnnews.com/news/202006171445118494"
     },
     {
-        "title": "[종합] 코로나19 확진자 43명 증가...수도권 25명, 대전 5명, 해외 12명",
-        "link": "http://www.fnnews.com/news/202006171015415172"
+        "title": "[종합] 서울 지하철2호선 시청역 안전요원 3명 코로나 확진",
+        "link": "https://www.hankyung.com/society/article/2020061777967"
     },
     {
-        "title": "“수도권 집단감염 계속”...코로나19 신규 확진자 43명",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200617500044&wlog_tag3=naver"
+        "title": "[속보]서울 시청역에서 확진자… 2호선 안전요원 3명 감염",
+        "link": "https://news.chosun.com/site/data/html_dir/2020/06/17/2020061702622.html?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
-        "title": "[단독] 코로나 `여름 미스터리`…확산은 여전한데 사망위험은 낮아졌다",
-        "link": "http://news.mk.co.kr/newsRead.php?no=620867&year=2020"
-    },
-    {
-        "title": "코로나19 신규 확진 43명…“수도권 28명·대전 5명·검역 7명”",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4472501&ref=A"
+        "title": "전주서 확진자 발생…전주여고 3학년생",
+        "link": "https://www.nocutnews.co.kr/news/5362689"
     }
 ];
