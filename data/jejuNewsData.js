cce14f2f65df92a8757852e141f3b20f92053be0
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-19 10:31:19 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,6 +5,18 @@ var jejuNewsData = [
         "link": "http://www.fnnews.com/news/202006190709270776"
     },
     {
+        "title": "방글라데시서 입국한 7명 무더기 확진...추가 확진자 나올 가능성도",
+        "link": "https://www.sedaily.com/NewsView/1Z4373T81A"
+    },
+    {
+        "title": "도내 17·18·19번째 확진자 접촉자 총 72명",
+        "link": "http://www.jejunews.com/news/articleView.html?idxno=2166093"
+    },
+    {
+        "title": "방글라데시서 입국 인천·경기·전북 확진자 5명 '같은 항공기 탑승'",
+        "link": "https://www.news1.kr/articles/?3970825"
+    },
+    {
         "title": "해외유입 확진자 증가 '비상'…방글라데시발 항공편 무더기 확진(종합)",
         "link": "http://yna.kr/AKR20200619081451530?did=1195m"
     },
@@ -13,31 +25,19 @@ var jejuNewsData = [
         "link": "https://www.pressian.com/pages/articles/2020061822523328982?utm_source=naver&utm_medium=search"
     },
     {
-        "title": "제주도서 방글라데시 유학생 3명 코로나19 확진…18명 접촉",
-        "link": "https://news.joins.com/article/olink/23400209"
-    },
-    {
-        "title": "제주도, 확진 방글라데시 유학생 접촉 70명 '격리'",
-        "link": "http://www.newsis.com/view/?id=NISX20200619_0001066555&cID=10813&pID=10800"
-    },
-    {
-        "title": "방글라데시서 입국한 7명 무더기 확진...추가 확진자 나올 가능성도",
-        "link": "https://www.sedaily.com/NewsView/1Z4373T81A"
-    },
-    {
-        "title": "방글라데시서 입국 인천·경기·전북 확진자 5명 '같은 항공기 탑승'",
-        "link": "https://www.news1.kr/articles/?3970825"
+        "title": "제주도내 코로나19 확진자 18일 하루만 3명 나와",
+        "link": "https://www.pressian.com/pages/articles/2020061902410290002?utm_source=naver&utm_medium=search"
     },
     {
         "title": "코로나19 한창인데 수백명 모여 포커대회…주최 측 “직원 교육” 변명",
         "link": "https://www.seoul.co.kr/news/newsView.php?id=20200619500176&wlog_tag3=naver"
     },
     {
-        "title": "[속보] 제주도 코로나19 확진자 2명 추가…누적 18명",
-        "link": "https://www.hankyung.com/society/article/2020061846397"
+        "title": "제주도서 방글라데시 유학생 3명 코로나19 확진…18명 접촉",
+        "link": "https://news.joins.com/article/olink/23400209"
     },
     {
-        "title": "세종시, 코로나19 확진자 발생",
-        "link": "http://www.chungnamilbo.co.kr/news/articleView.html?idxno=548204"
+        "title": "[속보] 제주도 코로나19 확진자 2명 추가…누적 18명",
+        "link": "https://www.hankyung.com/society/article/2020061846397"
     }
 ];
