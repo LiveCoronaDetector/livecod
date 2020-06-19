faefe4310318c3ee8aae87e016967e5f309d5353
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-19 08:01:59 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,28 +5,32 @@ var jejuNewsData = [
         "link": "http://www.fnnews.com/news/202006190709270776"
     },
     {
-        "title": "제주도 17·18번째 코로나19 확진자 발생",
-        "link": "http://yna.kr/AKR20200618181900056?did=1195m"
+        "title": "방글라데시서 입국한 7명 무더기 확진...추가 확진자 나올 가능성도",
+        "link": "https://www.sedaily.com/NewsView/1Z4373T81A"
     },
     {
-        "title": "제주도서 방글라데시 유학생 3명 코로나19 확진…18명 접촉",
-        "link": "https://news.joins.com/article/olink/23400209"
+        "title": "방글라데시서 입국 인천·경기·전북 확진자 5명 '같은 항공기 탑승'",
+        "link": "https://www.news1.kr/articles/?3970825"
     },
     {
         "title": "해외유입 확진자 증가 '비상'…방글라데시발 항공편 무더기 확진(종합)",
         "link": "http://yna.kr/AKR20200619081451530?did=1195m"
     },
     {
+        "title": "제주도, 17-18번째 코로나19 확진자 발생",
+        "link": "https://www.pressian.com/pages/articles/2020061822523328982?utm_source=naver&utm_medium=search"
+    },
+    {
+        "title": "제주도서 방글라데시 유학생 3명 코로나19 확진…18명 접촉",
+        "link": "https://news.joins.com/article/olink/23400209"
+    },
+    {
         "title": "[속보] 제주도 코로나19 확진자 2명 추가…누적 18명",
         "link": "https://www.hankyung.com/society/article/2020061846397"
     },
     {
-        "title": "세종시 코로나19 확진자 발생",
-        "link": "https://www.pressian.com/pages/articles/2020061811424539229?utm_source=naver&utm_medium=search"
-    },
-    {
-        "title": "방글라데시서 입국한 7명 무더기 확진...추가 확진자 나올 가능성도",
-        "link": "https://www.sedaily.com/NewsView/1Z4373T81A"
+        "title": "세종시, 코로나19 확진자 발생",
+        "link": "http://www.chungnamilbo.co.kr/news/articleView.html?idxno=548204"
     },
     {
         "title": "[단독] 인천 서구의원들, '이 시국에' 제주도로 의정연수",
@@ -35,9 +39,5 @@ var jejuNewsData = [
     {
         "title": "세종시 48번 확진자 딸 '음성', 14일간 제주도 격리",
         "link": "http://www.sjsori.com/news/articleView.html?idxno=43805"
-    },
-    {
-        "title": "방글라데시서 입국 인천·경기·전북 확진자 5명 '같은 항공기 탑승'",
-        "link": "https://www.news1.kr/articles/?3970825"
     }
 ];
