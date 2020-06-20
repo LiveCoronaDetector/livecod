863b854085e5c887062a2370ba651eca01ad60e2
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-20 21:31:36 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -13,20 +13,20 @@ var jejuNewsData = [
         "link": "http://www.hani.co.kr/arti/area/jeju/950210.html"
     },
     {
+        "title": "제주도 3박4일 여행한 코로나 확진자… 함께 간 지인도 양성",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/20/2020062001888.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
         "title": "해외유입 확진자 31명… 당국 \"크게 우려할 상황 아니다\"",
         "link": "http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202006200041&t=NN"
     },
     {
-        "title": "방글라데시發 확진자, 국내 입국 후 제주행… 같은 항공편 54명 격리",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/20/2020062001038.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "방글라데시서 입국 확진자, 제주행 항공기도 타… 54명 격리",
+        "link": "http://www.segye.com/content/html/2020/06/20/20200620509154.html?OutUrl=naver"
     },
     {
         "title": "\"방글라데시발 확진자 탄 제주행 비행기 탄 54명 자가격리\"",
         "link": "https://news.joins.com/article/olink/23400940"
-    },
-    {
-        "title": "제주도 3박4일 여행한 코로나 확진자… 함께 간 지인도 양성",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/20/2020062001888.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
         "title": "제주 관광객 2명 코로나19 확진…유증상 상태로 여행",
