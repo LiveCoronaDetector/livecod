d7b518e238cec38eade280a7b32d04085675fc34
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-21 07:02:03 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -9,24 +9,28 @@ var jejuNewsData = [
         "link": "http://www.fnnews.com/news/202006190709270776"
     },
     {
-        "title": "제주도 3박4일 여행한 코로나 확진자… 함께 간 지인도 양성",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/20/2020062001888.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "“코로나19 확진자가 탑승했던 택시 찾습니다”...경로 보니",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200621500073&wlog_tag3=naver"
     },
     {
         "title": "\"방글라데시발 확진자 탄 제주행 비행기 탄 54명 자가격리\"",
         "link": "https://news.joins.com/article/olink/23400940"
     },
     {
+        "title": "제주도 3박4일 여행한 코로나 확진자… 함께 간 지인도 양성",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/20/2020062001888.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
         "title": "제주 관광객 2명 코로나19 확진…유증상 상태로 여행",
         "link": "https://www.nocutnews.co.kr/news/5364588"
     },
     {
-        "title": "제주 여행한 관광객 2명 코로나 19 확진에 제주도 비상",
-        "link": "http://www.hani.co.kr/arti/area/jeju/950210.html"
-    },
-    {
         "title": "제주여행 후 관광객 2명 코로나 확진…57명 자가격리",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006211053001&code=620117"
+    },
+    {
+        "title": "제주 여행한 관광객 2명 코로나 19 확진에 제주도 비상",
+        "link": "http://www.hani.co.kr/arti/area/jeju/950210.html"
     },
     {
         "title": "비수도권 확진자 사흘새 2배로…대전발 '전국 확산' 시작됐나",
@@ -35,9 +39,5 @@ var jejuNewsData = [
     {
         "title": "제주도 17·18번째 코로나19 확진자 발생",
         "link": "http://yna.kr/AKR20200618181900056?did=1195m"
-    },
-    {
-        "title": "안산시 코로나19 확진자, 3박 4일 제주도 여행...\"56명 접촉\"",
-        "link": "https://www.ytn.co.kr/_ln/0103_202006200129396033"
     }
 ];
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,8 +5,8 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/06/19/africa/south-africa-gender-violence-pandemic-intl/index.html"
     },
     {
-        "title": "Brazil coronavirus death toll nears 50,000: Live updates",
-        "link": "https://www.aljazeera.com/news/2020/06/brazil-coronavirus-death-toll-nears-50000-live-updates-200620230549593.html"
+        "title": "Donald Trump: Tulsa rally fails to draw expected crowds amid virus fears",
+        "link": "https://www.bbc.com/news/world-us-canada-53121488"
     },
     {
         "title": "‘Israel in second wave of coronavirus, death toll may soar’",
@@ -17,8 +17,8 @@ var worldNewsData = [
         "link": "https://www.washingtonpost.com/world/europe/vaccine-coronavirus-oxford-italy-trials/2020/06/20/4b8f5e0a-af1c-11ea-98b5-279a6479a1e4_story.html"
     },
     {
-        "title": "Coronavirus, not border clash, to top agenda at China-India-Russia talks",
-        "link": "https://www.scmp.com/news/china/diplomacy/article/3089908/coronavirus-not-border-clash-top-agenda-china-india-russia"
+        "title": "Coronavirus pandemic: Updates from around the world",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-06-21-20-intl/index.html"
     },
     {
         "title": "China needs to be held accountable for Covid-19's destruction",
@@ -29,15 +29,15 @@ var worldNewsData = [
         "link": "https://www.forbes.com/sites/carlieporterfield/2020/06/20/daily-us-coronavirus-cases-hit-30000-for-first-time-since-may/"
     },
     {
+        "title": "Trump back in rally form in Tulsa, but the promised crowd doesn't show",
+        "link": "https://news.yahoo.com/trump-back-in-rally-form-in-tulsa-but-the-promised-crowd-doesnt-show-014255164.html"
+    },
+    {
+        "title": "Florida’s Covid-19 surge shows the state's reopening plan is not working",
+        "link": "https://www.theguardian.com/world/2020/jun/20/florida-covid19-surge-reopening-governor-desantis-coronavirus"
+    },
+    {
         "title": "Covid-19 unmasks what cancer patients have long known: The world needs a better and more equitable health system",
         "link": "https://www.cnn.com/2020/06/20/opinions/covid-19-unmasks-what-cancer-patients-have-long-known/index.html"
-    },
-    {
-        "title": "Coronavirus Spread Hits 1-Day High, World Health Organization Says",
-        "link": "https://www.npr.org/sections/coronavirus-live-updates/2020/06/19/880928603/coronavirus-spread-hits-one-day-high-world-health-organization-says"
-    },
-    {
-        "title": "Donald Trump: Tulsa rally fails to draw expected crowds amid virus fears",
-        "link": "https://www.bbc.com/news/world-us-canada-53121488"
     }
 ];
