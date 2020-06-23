f754e12a8d24a6d809734dcb9b66094c7527c8b7
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-23 00:32:34 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,24 +9,20 @@ var koreaNewsData = [
         "link": "http://www.newsis.com/view/?id=NISX20200622_0001068927&cID=10899&pID=10800"
     },
     {
-        "title": "\"전 세계 코로나19 확진자 900만명 넘었다\"",
-        "link": "http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202006230006&t=NN"
-    },
-    {
-        "title": "뉴욕증시, 코로나19 재확산 우려에도 상승…나스닥 사상 최고치",
-        "link": "http://news.tf.co.kr/read/economy/1798757.htm"
-    },
-    {
-        "title": "美 10개주서 신규 확진자 최대치…\"코로나 산불처럼 번져\"",
-        "link": "http://news.tvchosun.com/site/data/html_dir/2020/06/23/2020062390015.html"
-    },
-    {
-        "title": "코로나19 통제에 불만 폭발했나…독일서 수백명 폭동(종합)",
-        "link": "http://yna.kr/AKR20200622159251082?did=1195m"
+        "title": "\"전세계 코로나19 누적 확진자 900만명 돌파\"",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/23/2020062300563.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
         "title": "[속보]코로나19 신규 확진자 17명...지역발생 11명",
         "link": "https://www.sedaily.com/NewsView/1Z44JJ6M5V"
+    },
+    {
+        "title": "부산항 입항 러시아 선박에서 코로나19 집단 확진자 발생",
+        "link": "http://www.kwnews.co.kr/view.asp?aid=220062200146&s=501"
+    },
+    {
+        "title": "美 10개주서 신규 확진자 최대치…\"코로나 산불처럼 번져\"",
+        "link": "http://news.tvchosun.com/site/data/html_dir/2020/06/23/2020062390015.html"
     },
     {
         "title": "대전 22일 오전 코로나19 2명 추가 확진",
@@ -35,6 +31,10 @@ var koreaNewsData = [
     {
         "title": "박원순 \"한달 후 하루 확진자 800여명 우려\"",
         "link": "http://yna.kr/AKR20200622077900004?did=1195m"
+    },
+    {
+        "title": "미국 증시, 나스닥 사상 최고...애플·MS, 코로나 공포 잠재워",
+        "link": "http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202006230015&t=NN"
     },
     {
         "title": "영국 코로나19 신규 확진자 1천명 밑으로…봉쇄조치 이후 최소",
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,6 +25,10 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-asia-53135626"
     },
     {
+        "title": "Half of US states see coronavirus surge as officials warn first wave far from over",
+        "link": "https://www.theguardian.com/world/2020/jun/22/us-coronavirus-spikes-first-wave-not-over"
+    },
+    {
         "title": "COVID-19 is weakening, could die out without vaccine, specialist claims",
         "link": "https://www.jpost.com/health-science/covid-19-is-weakening-could-die-out-without-vaccine-specialist-claims-632324"
     },
@@ -33,11 +37,7 @@ var worldNewsData = [
         "link": "https://www.dw.com/en/coronavirus-latest-german-reproduction-rate-spikes-60-in-a-day/a-53886880"
     },
     {
-        "title": "Coronavirus: Bolsonaro silent as Brazil passes 50,000 deaths; global cases reach 9 million – as it happened",
-        "link": "https://www.theguardian.com/world/live/2020/jun/22/coronavirus-live-news-covid-19-update-china-us-uk-brazil-latest-updates"
-    },
-    {
-        "title": "WHO reports largest single-day increase in coronavirus cases",
-        "link": "https://www.nbcnews.com/news/world/who-reports-largest-single-day-increase-coronavirus-cases-n1231681"
+        "title": "WHO reports biggest one-day rise in coronavirus cases to date",
+        "link": "https://www.cbsnews.com/news/coronavirus-cases-daily-record-world-health-organization/"
     }
 ];
