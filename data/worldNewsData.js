7e4b7dc77f3059e4122fc1541bfb40097537e4a9
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-24 19:31:09 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,8 +5,8 @@ var worldNewsData = [
         "link": "https://www.theguardian.com/world/live/2020/jun/24/coronavirus-live-news-update-dr-fauci-disturbing-us-case-surge-brazil-covid-19-infections-cases-latest-updates"
     },
     {
-        "title": "Live Coronavirus Updates: Nations Scramble to Fend Off Outbreaks",
-        "link": "https://www.nytimes.com/2020/06/24/world/coronavirus-updates.html"
+        "title": "Coronavirus: US cases at highest level for two months",
+        "link": "https://www.bbc.com/news/world-us-canada-53167780"
     },
     {
         "title": "Pilots Discussed Coronavirus, Ignoring Alerts Before Pakistan Crash, Officials Say",
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.cnn.com/videos/world/2020/06/24/china-beijing-coronavirus-testing-site-culver-lkl-intl-hnk-vpx.cnn"
     },
     {
-        "title": "IMF slashes its forecasts for the global economy and warns of soaring debt levels",
-        "link": "https://www.cnbc.com/2020/06/24/imf-global-economy-to-contract-with-coronavirus-recovery-slow.html"
+        "title": "US coronavirus cases at highest level since April | TheHill",
+        "link": "https://thehill.com/policy/healthcare/504253-us-coronavirus-cases-at-highest-level-since-april"
     },
     {
         "title": "Countries across world reimpose restrictions due to coronavirus spikes as US remains adamant about avoiding...",
         "link": "https://www.foxnews.com/world/world-restrictions-coronavirus-new-spike-cases-us-avoid-lockdowns"
     },
     {
-        "title": "Coronavirus: US cases at highest level for two months",
-        "link": "https://www.bbc.com/news/world-us-canada-53167780"
-    },
-    {
         "title": "Coronavirus Live News and Updates",
         "link": "https://www.nytimes.com/2020/06/23/world/coronavirus-updates.html"
+    },
+    {
+        "title": "Novak Djokovic tests positive for coronavirus after Adria Tour event",
+        "link": "https://edition.cnn.com/2020/06/23/tennis/novak-djokovic-coronavirus-adria-tour-spt-intl/index.html"
     }
 ];
