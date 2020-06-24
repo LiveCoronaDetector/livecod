d92769a5d983c0e33166b59f5b776abd934cc4c6
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-24 21:01:40 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,6 +5,10 @@ var jejuNewsData = [
         "link": "http://www.fnnews.com/news/202006241639297869"
     },
     {
+        "title": "제주도내 16번째 코로나19 확진자 퇴원",
+        "link": "https://www.pressian.com/pages/articles/2020062505511168299?utm_source=naver&utm_medium=search"
+    },
+    {
         "title": "해열제 10알 먹고 제주여행 강행한 확진자… 제주도, 손배소 제기",
         "link": "http://www.womennews.co.kr/news/articleView.html?idxno=200126"
     },
@@ -27,10 +31,6 @@ var jejuNewsData = [
     {
         "title": "\"지인과 있게 해달라\" 불안감 호소 코로나 격리자 극단선택(종합2보)",
         "link": "http://yna.kr/AKR20200622054152056?did=1195m"
-    },
-    {
-        "title": "제주서 코로나 격리 20대 여성 사망…'우울증' 있었다는데(상보)",
-        "link": "https://www.news1.kr/articles/?3972382"
     },
     {
         "title": "코로나 격리중 제주 관광객 극단적 선택…공황장애 등 앓아",
