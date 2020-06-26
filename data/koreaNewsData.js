94abe463e189a07b98f7b759d82cd393ab9c9584
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-26 07:01:32 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,39 +5,39 @@ var koreaNewsData = [
         "link": "http://www.fnnews.com/news/202006261014029064"
     },
     {
-        "title": "서울 관악구 왕성교회 확진자 12명…MT 8·성가대 3·예배 1명(종합)",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/26/2020062602419.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
-    },
-    {
-        "title": "[속보] 현대카드 여의도 본사서 코로나19 확진자 발생…해당층 폐쇄",
-        "link": "https://www.hankyung.com/economy/article/2020062629106"
-    },
-    {
-        "title": "전북 26번째 코로나 확진자 발생",
-        "link": "https://www.nocutnews.co.kr/news/5367898"
-    },
-    {
-        "title": "관악구 왕성교회 관련 '코로나19' 확진자 증가…이대부고 교사 등 12명",
-        "link": "http://www.joseilbo.com/news/news_read.php?uid=400924&class=33&grp="
-    },
-    {
-        "title": "코로나19 신규 확진자 39명…서울·대전·충남에 집중",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200626500047&wlog_tag3=naver"
-    },
-    {
-        "title": "대전서 2명 더 코로나19 확진…천안서도 1명 추가(종합)",
-        "link": "http://yna.kr/AKR20200626041851063?did=1195m"
+        "title": "현대카드 여의도 본사서 코로나 확진자 발생",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/26/2020062602291.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
         "title": "서울 도심 덮친 왕성교회發 코로나…광화문 포시즌스호텔 사우나 직원 확진",
         "link": "https://news.chosun.com/site/data/html_dir/2020/06/26/2020062601166.html?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
-        "title": "광화문 포시즌스 호텔서 코로나19 확진자 발생",
-        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=02089366625805656"
+        "title": "코로나19 신규 확진자 39명…서울·대전·충남에 집중",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200626500047&wlog_tag3=naver"
+    },
+    {
+        "title": "서울 왕성교회 관련 확진자 12명…이대부고 교사 등 포함",
+        "link": "https://www.hankyung.com/society/article/2020062625707"
+    },
+    {
+        "title": "\"왕성교회, 대부도 MT서 전파 가능성\"…확진자 12명 8명 MT 참석(종합)",
+        "link": "http://www.newsis.com/view/?id=NISX20200626_0001074415&cID=10201&pID=10200"
+    },
+    {
+        "title": "전북 26번째 코로나 확진자 발생",
+        "link": "https://www.nocutnews.co.kr/news/5367898"
+    },
+    {
+        "title": "대전서 2명 더 코로나19 확진…천안서도 1명 추가(종합)",
+        "link": "http://yna.kr/AKR20200626041851063?did=1195m"
     },
     {
         "title": "[속보] 관악구 왕성교회 신도 4명 코로나 확진... 국내 확진자 39명 추가",
         "link": "https://www.sedaily.com/NewsView/1Z46DRCRR7"
+    },
+    {
+        "title": "[종합] 서울 관악구 왕성교회 확진자 12명으로 증가",
+        "link": "http://www.fnnews.com/news/202006261454253714"
     }
 ];
