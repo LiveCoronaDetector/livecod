3c38a234e746e10cff02e2cea1cf30a8f31ef261
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-27 04:31:01 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,39 +5,39 @@ var koreaNewsData = [
         "link": "https://www.ytn.co.kr/_ln/0103_202006270929094132"
     },
     {
-        "title": "[속보] 교회 집단감염 여파…신규 확진자 사흘만에 다시 50명대로",
-        "link": "http://www.hani.co.kr/arti/society/health/951188.html"
-    },
-    {
-        "title": "[속보] 사흘 만에 국내 코로나19 신규 확진자 50명대로 급증",
-        "link": "http://news.kmib.co.kr/article/view.asp?arcid=0014738419&code=61121111&cp=nv"
+        "title": "신규 확진자 다시 50명대…왕성교회 16명으로 급증 [종합]",
+        "link": "https://www.hankyung.com/society/article/2020062753417"
     },
     {
         "title": "[속보] 코로나19 신규 확진자 51명…지역발생 31명",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202006271019001&code=940100"
     },
     {
+        "title": "코로나19 일일 신규 확진자 51명…교회 집단감염 영향",
+        "link": "https://cnbc.sbs.co.kr/article_hub/10000987900?division=NAVER"
+    },
+    {
         "title": "美, 코로나 하루 확진 4만 명…경제 재개 제동",
         "link": "https://imnews.imbc.com/replay/2020/nwtoday/article/5823824_32531.html"
     },
     {
-        "title": "대전서 30대 코로나19 확진…24일 교회서 예배(종합)",
-        "link": "http://yna.kr/AKR20200627018351063?did=1195m"
+        "title": "군포서 확진자 7명 무더기 발생…모두 안양 주영광교회 신도",
+        "link": "https://www.donga.com/news/article/all/20200627/101711550/2"
     },
     {
-        "title": "뉴욕증시, 코로나19 확진자 최다 소식에 2% 넘게 급락",
-        "link": "https://cnbc.sbs.co.kr/article_hub/10000987888?division=NAVER"
-    },
-    {
-        "title": "현대카드 여의도 본사서 코로나 확진자 발생",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/26/2020062602291.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
-    },
-    {
-        "title": "배달앱 ‘요기요’ 서초사옥서 코로나 확진자 발생",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/26/2020062603184.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "안양 주영광교회 관련 확진자 무더기 발생…이틀새 11명(종합)",
+        "link": "http://yna.kr/AKR20200627026751061?did=1195m"
     },
     {
         "title": "[종합] 코로나19 확진자 51명 증가...해외 20명, 서울 15명, 경기 12명",
         "link": "http://www.fnnews.com/news/202006271018141626"
+    },
+    {
+        "title": "뉴욕증시, 美 코로나19 확진자 최다 기록에 급락…다우 2.8% 하락",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/27/2020062700245.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
+        "title": "코로나19 신규 확진 51명…총 1만 2,653명 확진",
+        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005855955&plink=ORI&cooper=NAVER"
     }
 ];
