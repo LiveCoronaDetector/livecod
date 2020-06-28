aa715f47b85c6dc30d263a765fac9f1661b1e49a
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-28 08:01:31 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,8 +17,8 @@ var worldNewsData = [
         "link": "https://www.foxnews.com/world/americans-unwelcome-eu-borders-reopen-coronavirus"
     },
     {
-        "title": "Coronavirus: How Delhi 'wasted' lockdown to become India's biggest hotspot",
-        "link": "https://news.yahoo.com/coronavirus-delhi-wasted-lockdown-become-032221324.html"
+        "title": "Coronavirus: Delhi struggles to cope with Covid-19 surge",
+        "link": "https://www.bbc.com/news/world-asia-india-53205718"
     },
     {
         "title": "Coronavirus live news: US cases pass 2.5m as Australia considers new lockdown in Melbourne",
@@ -37,7 +37,7 @@ var worldNewsData = [
         "link": "https://www.aljazeera.com/news/2020/06/global-coronavirus-death-toll-nears-500000-live-updates-200627234018796.html"
     },
     {
-        "title": "How community transmission is very different to locally acquired coronavirus",
-        "link": "https://www.abc.net.au/news/health/2020-06-28/what-exactly-is-community-transmission-and-how-is-it-curbed/12397296"
+        "title": "Emergency nurse among 41 new COVID-19 cases in Victoria",
+        "link": "https://www.youtube.com/watch?v=vfsGv0fgfu4"
     }
 ];
