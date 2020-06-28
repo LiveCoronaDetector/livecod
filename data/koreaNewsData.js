b0954f6ce1b2099f3f09543772030bb97daad28f
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-28 14:31:49 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,8 +5,16 @@ var koreaNewsData = [
         "link": "https://biz.chosun.com/site/data/html_dir/2020/06/28/2020062800400.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
-        "title": "전세계 코로나19 확진자 1천만명 넘었다(종합)",
-        "link": "http://yna.kr/AKR20200628005000009?did=1195m"
+        "title": "여의도 현대카드에서 ‘코로나19’ 추가 확진자…왕성교회발 감염 추정",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4481410&ref=A"
+    },
+    {
+        "title": "부산 40대 확진자 해운대 마린시티 등 음식점·주점 방문(종합)",
+        "link": "http://yna.kr/AKR20200628027051051?did=1195m"
+    },
+    {
+        "title": "[속보] 코로나19 확진자 62명 추가...지역 발생 40명",
+        "link": "https://www.ytn.co.kr/_ln/0103_202006281014404144"
     },
     {
         "title": "코로나 전세계 1000만명 감염, 50만명이 사망했다",
@@ -15,6 +23,10 @@ var koreaNewsData = [
     {
         "title": "[속보] 서울 누계 확진자 1300명 넘었다…TK 이어 세번째",
         "link": "https://www.hankyung.com/society/article/2020062877677"
+    },
+    {
+        "title": "전세계 코로나19 확진자 1천만명 넘었다(종합)",
+        "link": "http://yna.kr/AKR20200628005000009?did=1195m"
     },
     {
         "title": "中 베이징 신규 확진자 14명 늘어 311명",
@@ -27,17 +39,5 @@ var koreaNewsData = [
     {
         "title": "대전서 코로나19 확진 2명 더…15일 재확산 이후 65명째(종합)",
         "link": "http://yna.kr/AKR20200628018451063?did=1195m"
-    },
-    {
-        "title": "[속보] 코로나19 확진자 62명 추가...지역 발생 40명",
-        "link": "https://www.ytn.co.kr/_ln/0103_202006281014404144"
-    },
-    {
-        "title": "국내 코로나19 신규 확진자, 8일 만에 60명대로 늘어",
-        "link": "https://cnbc.sbs.co.kr/article_hub/10000987938?division=NAVER"
-    },
-    {
-        "title": "여의도 현대카드에서 ‘코로나19’ 추가 확진자…왕성교회발 감염 추정",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4481410&ref=A"
     }
 ];
