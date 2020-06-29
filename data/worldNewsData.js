489a7ad102f6b4877c97961ece1ab69798e3aa5d
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-29 11:02:12 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/06/29/asia/china-coronavirus-lockdown-hebei-intl-hnk/index.html"
     },
     {
-        "title": "COVID-19 Global Roundup: 10 million infections and 500,000 deaths",
-        "link": "https://news.cgtn.com/news/2020-06-29/Global-COVID-19-Roundup-10-million-infections-and-500-000-deaths-RI0G0sz77a/index.html"
+        "title": "Coronavirus Live Updates: Global Deaths Pass 500,000",
+        "link": "https://www.nytimes.com/2020/06/28/world/coronavirus-updates.html"
     },
     {
-        "title": "Choir of more than 100 people perform without masks at Pence event",
-        "link": "https://www.cnn.com/2020/06/28/politics/mike-pence-dallas-choir-no-masks-church-event/index.html"
+        "title": "Live updates: Global death toll from coronavirus surpasses half a million",
+        "link": "https://www.washingtonpost.com/nation/2020/06/29/coronavirus-live-updates-us/"
     },
     {
         "title": "More than 500,000 people have died from coronavirus worldwide as infections continue to surge",
         "link": "https://www.cnbc.com/2020/06/28/more-than-500000-people-have-died-from-coronavirus-worldwide-as-infections-continue-to-surge.html"
     },
     {
-        "title": "Health secretary: ‘Window is closing’ to stop coronavirus as US cases pass 2.5m",
-        "link": "https://www.theguardian.com/world/2020/jun/28/us-health-secretary-window-closing-stop-coronavirus-cases-pass-25m"
+        "title": "Choir of more than 100 people perform without masks at Pence event",
+        "link": "https://www.cnn.com/2020/06/28/politics/mike-pence-dallas-choir-no-masks-church-event/index.html"
     }
 ];
