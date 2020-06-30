38aba20416986b4ced94b512f35615c1587164e2
LiveCoronaBot <commit-bot@taeuk.me>
2020-06-30 09:01:59 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,12 +5,16 @@ var jejuNewsData = [
         "link": "https://www.nocutnews.co.kr/news/5370139"
     },
     {
+        "title": "[전문] 광주광역시청 45번째 확진자 발생...제주도 여행",
+        "link": "http://www.dailygrid.net/news/articleView.html?idxno=370283"
+    },
+    {
         "title": "'지역확산에서 깜깜이 감염까지'…광주 45번 확진자 추가(종합)",
         "link": "http://yna.kr/AKR20200630179300054?did=1195m"
     },
     {
-        "title": "광주서 코로나 45번 확진자 발생…70대 여성, 2차 검사 중",
-        "link": "https://www.news1.kr/articles/?3981478"
+        "title": "제주도 다녀온 광주 북구 70대 주민, 코로나 1차 양성반응",
+        "link": "http://www.newsis.com/view/?id=NISX20200630_0001078556&cID=10201&pID=10200"
     },
     {
         "title": "코로나19 확산 방지에 전국 해수욕장, 손목밴드·QR코드 등장",
@@ -21,23 +25,19 @@ var jejuNewsData = [
         "link": "http://www.inews24.com/view/1277350"
     },
     {
+        "title": "[속보] 광주광역시청, 코로나 45번 확진자 발생 '제주도 여행·중흥동 거주'",
+        "link": "http://www.ccreview.co.kr/news/articleView.html?idxno=268674"
+    },
+    {
+        "title": "감염경로 모르는 광주 확진자 속출… 제주도 다녀오기도",
+        "link": "http://news.kmib.co.kr/article/view.asp?arcid=0014749776&code=61121111&cp=nv"
+    },
+    {
         "title": "[쇼맥] 코로나 블루 누적…심리방역도 체계적으로",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4482209&ref=A"
     },
     {
-        "title": "광주 70대女 코로나19 1차 '양성'··· 45번째",
-        "link": "http://www.honam.co.kr/detail/c3QycN/607206"
-    },
-    {
         "title": "제주서 ‘시설 격리’ 20대女 숨진 채 발견…확진자와 비행기 접촉",
         "link": "https://www.donga.com/news/article/all/20200622/101624815/2"
-    },
-    {
-        "title": "강남구 코로나 확진자, 제주도에서 패키지 관광",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/06/19/2020061904459.html?utm_source=naver&utm_medium=original&utm_campaign=news"
-    },
-    {
-        "title": "제주서 자가격리 중 숨진 20대 女 코로나 음성 판정",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/06/22/2020062203277.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     }
 ];
--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,28 +5,32 @@ var koreaNewsData = [
         "link": "https://biz.chosun.com/site/data/html_dir/2020/06/30/2020063001465.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
+        "title": "[속보] WHO \"코로나19 종식 근처도 못 가…최악 아직 안왔다\"",
+        "link": "http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202006300083&t=NN"
+    },
+    {
         "title": "코로나 신규 확진자 43명...강원·충북에서도 발생",
         "link": "http://www.hani.co.kr/arti/society/health/951546.html"
     },
     {
-        "title": "신규 확진자 이틀째 40명 대…지역발생 23명·해외유입 20명",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4482763&ref=A"
+        "title": "대전시 116번·117번 코로나19 확진자 이동 동선 공개",
+        "link": "http://www.ohmynews.com/NWS_Web/View/at_pg.aspx?CNTN_CD=A0002654446&CMPT_CD=P0010&utm_source=naver&utm_medium=newsearch&utm_campaign=naver_news"
     },
     {
-        "title": "광주 확진자 3명, 다단계 사무실로 알려진 오피스텔서 모임(종합)",
-        "link": "http://yna.kr/AKR20200629103754054?did=1195m"
+        "title": "대전서 기존 확진자 접촉한 2명 코로나19 확진…보름새 71명째(종합)",
+        "link": "http://yna.kr/AKR20200630047351063?did=1195m"
     },
     {
         "title": "광주서 70대 여성 코로나19 확진…45번째(종합)",
         "link": "https://www.nocutnews.co.kr/news/5370139"
     },
     {
-        "title": "종교모임 관련 확진자 잇따라…왕성교회 31명·광륵사 14명 [종합]",
-        "link": "https://www.hankyung.com/society/article/2020063058707"
-    },
-    {
         "title": "[Pick][Q&A] 코로나 치료제가 공짜라고?…미국은 375만 원",
         "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005860473&plink=ORI&cooper=NAVER"
+    },
+    {
+        "title": "종교모임 관련 확진자 잇따라…왕성교회 31명·광륵사 14명 [종합]",
+        "link": "https://www.hankyung.com/society/article/2020063058707"
     },
     {
         "title": "6월 30일 ‘코로나19’ 전세계 확진자 1019만명…입국금지·검역 조치 179개국...",
@@ -35,9 +39,5 @@ var koreaNewsData = [
     {
         "title": "대전 다단계發 코로나 재확산…학교까지 확진자 속출",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=03191446625806968"
-    },
-    {
-        "title": "\"美 코로나19 사망자 43%, 요양원과 관련\"(종합)",
-        "link": "http://www.newsis.com/view/?id=NISX20200630_0001078455&cID=10101&pID=10100"
     }
 ];
