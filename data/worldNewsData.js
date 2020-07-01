845adce6af3e459a7c4b712074fdb46e935b28bd
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-01 22:01:24 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,28 +5,24 @@ var worldNewsData = [
         "link": "https://www.cnn.com/videos/world/2020/06/30/south-africa-coronavirus-covid-19-pandemic-field-hospital-mckenzie-walk-and-talk-intl-ldn-vpx.cnn"
     },
     {
-        "title": "Coronavirus Live Updates: Some States Halt Reopenings and Indoor Dining as Cases Surge",
+        "title": "Coronavirus Live Updates: As Cases Surge, Some States Halt Reopenings and Indoor Dining",
         "link": "https://www.nytimes.com/2020/07/01/world/coronavirus-updates.html"
     },
     {
-        "title": "Turkey's coronavirus cases surpass 200,000: Live updates",
-        "link": "https://www.aljazeera.com/news/2020/07/coronavirus-cases-14-states-double-live-updates-200701003734077.html"
+        "title": "Coronavirus live news: Brazil death toll exceeds 60,000; West Bank goes into lockdown",
+        "link": "https://www.theguardian.com/world/live/2020/jul/01/coronavirus-live-updates-latest-news-us-buys-global-remdesivir-stocks-brazil-deaths-near-60000"
     },
     {
-        "title": "Coronavirus live updates: California rolls back reopening, NYC pauses indoor dining plans",
-        "link": "https://www.cnbc.com/2020/07/01/coronavirus-live-updates.html"
+        "title": "Italy keeps coronavirus pandemic under control, but many staying home to be safe",
+        "link": "https://www.foxnews.com/world/italy-coronavirus-pandemic-work-home-office"
     },
     {
         "title": "Groom dies after infecting over 100 wedding guests with coronavirus in India",
         "link": "https://nypost.com/2020/07/01/groom-infects-100-guests-with-coronavirus-dies-two-days-later/"
     },
     {
-        "title": "Australia to lock down 300,000 in Melbourne suburbs after coronavirus spike",
-        "link": "https://nypost.com/2020/07/01/australia-to-lock-down-300000-in-melbourne-suburbs-after-coronavirus-spike/"
-    },
-    {
-        "title": "Coronavirus pandemic: Updates from around the world",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-07-01-20-intl/index.html"
+        "title": "Dr. Anthony Fauci says new virus in China has traits of 2009 swine flu and 1918 pandemic flu",
+        "link": "https://www.cnbc.com/2020/06/30/dr-anthony-fauci-says-new-virus-in-china-has-traits-of-2009-h1n1-and-1918-pandemic-flu.html"
     },
     {
         "title": "WHO warns some countries may have to reinstate lockdowns as coronavirus pandemic accelerates",
@@ -39,5 +35,9 @@ var worldNewsData = [
     {
         "title": "Coronavirus: Fauci warns of 100,000 US cases per day",
         "link": "https://www.bbc.com/news/world-us-canada-53237824"
+    },
+    {
+        "title": "5 Things We Need to Keep Children Safe from COVID-19 | Save the Children",
+        "link": "https://www.youtube.com/watch?v=0nUViPRm8mE"
     }
 ];
