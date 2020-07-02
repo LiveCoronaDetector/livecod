29781353d2ad7b1f4815dc06f2e18ffca65728d3
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-02 06:31:09 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,39 +5,39 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200702084651017?did=1195m"
     },
     {
-        "title": "[속보]코로나 확진자로 KT 광화문 사옥 이틀간 폐쇄···일부 직원들 사측 초...",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007021002011&code=920100"
-    },
-    {
-        "title": "[속보]서울 잠실에 있는 삼성SDS 사옥, 코로나19로 폐쇄",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007021140001&code=920100"
-    },
-    {
-        "title": "KT, 코로나19 확진자 발생…전직원 재택 돌입",
-        "link": "http://www.inews24.com/view/1278532"
-    },
-    {
-        "title": "코로나 신규 확진자 54명...광주서 하루동안 22명 발생해",
-        "link": "http://www.hani.co.kr/arti/society/health/951908.html"
-    },
-    {
-        "title": "`코로나19` 확진자 54명중 광주 22명 `초비상`",
-        "link": "http://www.dt.co.kr/contents.html?article_no=2020070202109919711016&ref=naver"
-    },
-    {
-        "title": "WHO “6월 확진자 수가 코로나19 누적 확진자 수의 60%”",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200702500011&wlog_tag3=naver"
-    },
-    {
-        "title": "‘코로나19’ 54명 신규 확진…광주 22명·수도권 23명",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4484785&ref=A"
-    },
-    {
         "title": "[종합]신규 확진자 54명...이틀 연속 50명대",
         "link": "http://www.fnnews.com/news/202007021034080931"
     },
     {
         "title": "삼성SDS, 코로나19 확진자 발생...사옥 폐쇄",
         "link": "https://zdnet.co.kr/view/?no=20200702125622"
+    },
+    {
+        "title": "[속보]코로나 확진자로 KT 광화문 사옥 이틀간 폐쇄···일부 직원들 사측 초...",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007021002011&code=920100"
+    },
+    {
+        "title": "KT, 코로나19 확진자 발생…전직원 재택 돌입",
+        "link": "http://www.inews24.com/view/1278532"
+    },
+    {
+        "title": "KT 광화문 사옥 코로나19 확진자 발생…전원 재택 근무",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4485038&ref=A"
+    },
+    {
+        "title": "`코로나19` 확진자 54명중 광주 22명 `초비상`",
+        "link": "http://www.dt.co.kr/contents.html?article_no=2020070202109919711016&ref=naver"
+    },
+    {
+        "title": "코로나 신규 확진자 54명...광주서 하루동안 22명 발생해",
+        "link": "http://www.hani.co.kr/arti/society/health/951908.html"
+    },
+    {
+        "title": "‘코로나19’ 54명 신규 확진…광주 22명·수도권 23명",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4484785&ref=A"
+    },
+    {
+        "title": "WHO “6월 확진자 수가 코로나19 누적 확진자 수의 60%”",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200702500011&wlog_tag3=naver"
     }
 ];
