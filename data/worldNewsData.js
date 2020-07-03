12fb4005afc14a56614314bd40b84916d555edee
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-03 05:01:43 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,8 +17,8 @@ var worldNewsData = [
         "link": "https://www.nbcnews.com/news/world/e-u-coronavirus-safe-list-why-u-s-was-nowhere-n1232741"
     },
     {
-        "title": "Groom Dead, at Least 100 Sick With Coronavirus After Indian Wedding",
-        "link": "https://www.nytimes.com/2020/07/02/world/asia/india-coronavirus-wedding-groom.html"
+        "title": "Groom dies two days after Indian wedding, 80 people infected with coronavirus",
+        "link": "https://www.cnn.com/2020/07/02/asia/india-wedding-coronavirus-infections-scli-intl/index.html"
     },
     {
         "title": "Quarantine hotel sex scandal linked to coronavirus outbreak in Australian city of Melbourne",
@@ -33,11 +33,11 @@ var worldNewsData = [
         "link": "https://www.foxnews.com/politics/pelosi-coronavirus-trying-to-save-the-world"
     },
     {
-        "title": "Coronavirus vaccine tracker: How close are we to a vaccine?",
-        "link": "https://www.theguardian.com/world/ng-interactive/2020/jul/02/coronavirus-vaccine-tracker-how-close-are-we-to-a-vaccine"
-    },
-    {
         "title": "Texas Mandates Face Coverings as Coronavirus Cases Surge",
         "link": "https://www.wsj.com/articles/coronavirus-latest-news-07-02-2020-11593675909"
+    },
+    {
+        "title": "U.S. sets record for new coronavirus cases, surpassing 55,000",
+        "link": "https://www.washingtonpost.com/nation/2020/07/02/coronavirus-live-updates-us/"
     }
 ];
