0a1086a91fa1f9b389cd9b092f84b22faffacf68
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-04 11:02:00 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,16 +5,12 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200704044300056?did=1195m"
     },
     {
-        "title": "제주 20번째 코로나19 확진자 취업 비자 입국 20대 카자흐스탄인",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4486705&ref=A"
+        "title": "대구 연기학원발 코로나19 추가 확진 없어",
+        "link": "http://www.newsis.com/view/?id=NISX20200704_0001083755&cID=10899&pID=10800"
     },
     {
         "title": "제주도내 18‧19번 코로나19 확진자 15일만에 퇴원",
         "link": "http://www.mediajeju.com/news/articleView.html?idxno=324447"
-    },
-    {
-        "title": "대구 연기학원발 코로나19 추가 확진 없어",
-        "link": "http://www.newsis.com/view/?id=NISX20200704_0001083755&cID=10899&pID=10800"
     },
     {
         "title": "대구서 하루 14명 확진자 발생…다시 확산 추세",
@@ -25,10 +21,6 @@ var jejuNewsData = [
         "link": "https://www.ytn.co.kr/_ln/0115_202007021404532249"
     },
     {
-        "title": "코로나19, 대구 집단감염 때보다 전파 빨라졌다(종합)",
-        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=01646566625831240"
-    },
-    {
         "title": "코로나 재확산...여름휴가 설렘 대신 고민 커진다",
         "link": "https://www.sedaily.com/NewsView/1Z5670E4DR"
     },
@@ -37,7 +29,15 @@ var jejuNewsData = [
         "link": "http://www.segye.com/content/html/2020/07/03/20200703508319.html?OutUrl=naver"
     },
     {
+        "title": "코로나19, 대구 집단감염 때보다 전파 빨라졌다(종합)",
+        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=01646566625831240"
+    },
+    {
         "title": "대구서 하룻사이 코로나 확진 14명…유가초등학교 등",
         "link": "https://www.nocutnews.co.kr/news/5372167"
+    },
+    {
+        "title": "코로나19 증상 속 제주여행…광주 확진자 5명 동선 공개",
+        "link": "http://www.fnnews.com/news/202007011755302345"
     }
 ];
