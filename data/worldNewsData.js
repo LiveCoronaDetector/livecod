c21b311410ae53c4812d4106c22627ffc39ff81d
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-04 19:31:24 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-asia-53188847"
     },
     {
-        "title": "Coronavirus pandemic: Updates from around the world",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-07-04-20-intl/index.html"
-    },
-    {
-        "title": "Tokyo seeks travel curbs as new infections tops 100 for third day, says NHK",
-        "link": "https://www.reuters.com/article/us-health-coronavirus-japan/tokyo-seeks-travel-curbs-as-new-infections-tops-100-for-third-day-says-nhk-idUSKCN24507R"
+        "title": "England has one of the world's worst Covid death rates. Now many fear it's about to drink itself into chaos",
+        "link": "https://www.cnn.com/travel/article/england-pub-opening-coronavirus-chaos/index.html"
     },
     {
         "title": "Live Coronavirus Updates: A More Subdued Fourth",
         "link": "https://www.nytimes.com/2020/07/04/world/coronavirus-updates.html"
     },
     {
-        "title": "Coronavirus Spain: Catalonia locks down area of 210,000 people",
-        "link": "https://www.bbc.com/news/world-europe-53291281"
+        "title": "WHO halts hydroxychloroquine, HIV drugs in COVID trials after failure to reduce death",
+        "link": "https://www.reuters.com/article/us-health-coronavirus-who-drugs/who-halts-hydroxychloroquine-hiv-drugs-in-covid-trials-after-failure-to-reduce-death-idUSKBN2450OJ"
+    },
+    {
+        "title": "Tokyo seeks travel curbs as new infections tops 100 for third day, says NHK",
+        "link": "https://www.reuters.com/article/us-health-coronavirus-japan/tokyo-seeks-travel-curbs-as-new-infections-tops-100-for-third-day-says-nhk-idUSKCN24507R"
     }
 ];
