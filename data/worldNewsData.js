119808f22f1ee4f086e471fb45adaa845bc67276
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-05 21:01:48 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -9,8 +9,8 @@ var worldNewsData = [
         "link": "https://www.cnbc.com/2020/07/05/here-are-five-charts-tracking-the-us-economy-amid-the-coronavirus.html"
     },
     {
-        "title": "Palestinians, Israelis face new lockdowns amid coronavirus surge",
-        "link": "https://www.aljazeera.com/news/2020/07/palestinians-israelis-face-lockdowns-coronavirus-surge-200705092454034.html"
+        "title": "Global report: second local lockdown in Spain; second wave in Israel",
+        "link": "https://www.theguardian.com/world/2020/jul/05/global-report-second-local-lockdown-in-spain-second-wave-in-israel"
     },
     {
         "title": "Coronavirus: Spain imposes local lockdown in Galicia",
@@ -25,10 +25,6 @@ var worldNewsData = [
         "link": "https://www.foxnews.com/world/8000-chinese-nationals-came-into-us-after-trump-travel-ban-coronavirus"
     },
     {
-        "title": "Coronavirus pandemic: Updates from around the world",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-07-05-20-intl/index.html"
-    },
-    {
         "title": "US coronavirus cases dip as world sets record for new cases in 24-hour period",
         "link": "https://www.foxnews.com/health/cases-dip-world-sets-record"
     },
@@ -37,7 +33,11 @@ var worldNewsData = [
         "link": "https://www.theguardian.com/world/live/2020/jul/05/donald-trump-coronavirus-testing-fauci-fourth-july-kanye-west-protests-latest-news-live-updates"
     },
     {
-        "title": "239 Experts With 1 Big Claim: The Coronavirus Is Airborne",
-        "link": "https://www.nytimes.com/2020/07/04/health/239-experts-with-1-big-claim-the-coronavirus-is-airborne.html"
+        "title": "Trump Hosts July 4 Event at White House as U.S. Coronavirus Cases Soar",
+        "link": "https://www.nytimes.com/2020/07/04/world/coronavirus-updates.html"
+    },
+    {
+        "title": "WHO underplaying risk of airborne spread of Covid-19, say scientists",
+        "link": "https://www.theguardian.com/world/2020/jul/05/who-underplaying-risk-of-airborne-spread-of-covid-19-say-scientists"
     }
 ];
