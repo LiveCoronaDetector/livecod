246819148b25c505df01751cf140d28f6a268a16
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-05 07:01:46 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,12 +5,12 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200704044300056?did=1195m"
     },
     {
-        "title": "제주지역 20번째 코로나19 확진자 발생…외국서 입국",
-        "link": "http://www.hani.co.kr/arti/area/jeju/952260.html"
+        "title": "광주 광륵사 관련 확진자 9일새 80명…수도권·대전 소규모 확산",
+        "link": "http://www.newsis.com/view/?id=NISX20200705_0001084185&cID=10201&pID=10200"
     },
     {
-        "title": "코로나19 신규확진 61명, 누적 확진자 1만3091명…사흘째 60명대",
-        "link": "http://www.beyondpost.co.kr/view.php?ud=20200705112214987846a9e4dd7f_30"
+        "title": "제주지역 20번째 코로나19 확진자 발생…외국서 입국",
+        "link": "http://www.hani.co.kr/arti/area/jeju/952260.html"
     },
     {
         "title": "제주, 코로나19 20번 확진자 접촉자 '28명' 격리",
@@ -21,23 +21,23 @@ var jejuNewsData = [
         "link": "https://hankookilbo.com/News/Read/A2020070418300002773?did=NA"
     },
     {
-        "title": "제주 20번 카자흐 국적 확진자와 같은 항공기 28명 자가격리",
-        "link": "https://www.news1.kr/articles/?3986323"
-    },
-    {
-        "title": "광주 광륵사 관련 확진자 9일새 80명…수도권·대전 소규모 확산",
-        "link": "http://www.newsis.com/view/?id=NISX20200705_0001084185&cID=10201&pID=10200"
-    },
-    {
         "title": "제주도내 18‧19번 코로나19 확진자 15일만에 퇴원",
         "link": "http://www.mediajeju.com/news/articleView.html?idxno=324447"
     },
     {
-        "title": "대구 연기학원발 코로나19 추가 확진 없어",
-        "link": "http://www.newsis.com/view/?id=NISX20200704_0001083755&cID=10899&pID=10800"
+        "title": "'연기학원'發 집단감염 대구 추가 확진자 없어(종합)",
+        "link": "https://www.news1.kr/articles/?3985940"
     },
     {
-        "title": "광주 코로나19 ‘광륵사→방판업체→제주도 여행→사랑교회’ 순으로 전파",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200703500191&wlog_tag3=naver"
+        "title": "코로나19 신규확진 61명, 누적 확진자 1만3091명…사흘째 60명대",
+        "link": "http://www.beyondpost.co.kr/view.php?ud=20200705112214987846a9e4dd7f_30"
+    },
+    {
+        "title": "코로나 2차 유행 비상 걸린 비수도권…의료시스템 위태",
+        "link": "http://news.mt.co.kr/mtview.php?no=2020070515201527671"
+    },
+    {
+        "title": "코로나19 재확산에 잔뜩 움추린 휴일…해수욕장 등 한산",
+        "link": "http://yna.kr/AKR20200705034200060?did=1195m"
     }
 ];
