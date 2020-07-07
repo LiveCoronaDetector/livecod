e8058f49ab89147f7da67dfca8da401d2862393f
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-07 13:31:36 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,28 +5,32 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200707081751060?did=1195m"
     },
     {
-        "title": "목포서 40대 여성 코로나19 확진...광주 117번 확진자의 접촉자",
-        "link": "http://www.fnnews.com/news/202007071623193249"
-    },
-    {
-        "title": "'해열제 복용'하며 제주여행 확진자에 1억3천만원 손배소송",
-        "link": "http://yna.kr/AKR20200707117400056?did=1195m"
-    },
-    {
-        "title": "마포구서 확진자 4명 무더기 감염…서울서 확진자 9명 늘어(종합)",
-        "link": "https://www.news1.kr/articles/?3988940"
-    },
-    {
-        "title": "대전 추가 확진자 3명 발생…144명째(종합)",
-        "link": "http://www.newsis.com/view/?id=NISX20200707_0001087267&cID=10899&pID=10800"
+        "title": "오늘 서울 코로나19 확진자 9명 추가…누계 1384명",
+        "link": "https://view.asiae.co.kr/article/2020070719270214274"
     },
     {
         "title": "카자흐스탄發 확진자 일주일간 53명… 해외유입의 40% 넘어",
         "link": "https://biz.chosun.com/site/data/html_dir/2020/07/07/2020070702852.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
+        "title": "대전 추가 확진자 3명 발생…144명째(종합)",
+        "link": "http://www.newsis.com/view/?id=NISX20200707_0001087267&cID=10899&pID=10800"
+    },
+    {
+        "title": "목포서 40대 여성 코로나19 확진...광주 117번 확진자의 접촉자",
+        "link": "http://www.fnnews.com/news/202007071623193249"
+    },
+    {
         "title": "대전서 코로나19 확진자 3명 더 나와…당진·금산도 4명 추가(종합)",
         "link": "http://yna.kr/AKR20200707133251063?did=1195m"
+    },
+    {
+        "title": "코로나19 신규 확진자 44명 ...\"해외 유입 24명\"",
+        "link": "https://www.ytn.co.kr/_ln/0103_202007071146139973"
+    },
+    {
+        "title": "목포 40대 여성 코로나19 확진…전남 29번째(종합)",
+        "link": "http://yna.kr/AKR20200707138200054?did=1195m"
     },
     {
         "title": "코로나19 신규 확진 44명…아시아 지역 유입 환자 증가",
@@ -35,9 +39,5 @@ var koreaNewsData = [
     {
         "title": "코로나 신규 확진자 44명…수도권 12명, 광주 6명",
         "link": "http://www.hani.co.kr/arti/society/health/952552.html"
-    },
-    {
-        "title": "\"코로나 걸리면 죽겠다\" 광주 60대 확진자 전남 영광서 발견",
-        "link": "https://news.joins.com/article/olink/23413504"
     }
 ];
