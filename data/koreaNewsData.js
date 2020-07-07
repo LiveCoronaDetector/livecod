faf2fccdd7b52dcc57e16fc1697884a5ec06e90d
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-07 11:01:44 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,6 +9,22 @@ var koreaNewsData = [
         "link": "http://www.fnnews.com/news/202007071623193249"
     },
     {
+        "title": "'해열제 복용'하며 제주여행 확진자에 1억3천만원 손배소송",
+        "link": "http://yna.kr/AKR20200707117400056?did=1195m"
+    },
+    {
+        "title": "대전 추가 확진자 3명 발생…144명째(종합)",
+        "link": "http://www.newsis.com/view/?id=NISX20200707_0001087267&cID=10899&pID=10800"
+    },
+    {
+        "title": "카자흐스탄發 확진자 일주일간 53명… 해외유입의 40% 넘어",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/07/07/2020070702852.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
+        "title": "대전서 코로나19 확진자 3명 더 나와…당진·금산도 4명 추가(종합)",
+        "link": "http://yna.kr/AKR20200707133251063?did=1195m"
+    },
+    {
         "title": "코로나19 신규 확진 44명…아시아 지역 유입 환자 증가",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4488481&ref=A"
     },
@@ -21,23 +37,7 @@ var koreaNewsData = [
         "link": "https://news.joins.com/article/olink/23413504"
     },
     {
-        "title": "'해열제 복용'하며 제주여행 확진자에 1억3천만원 손배소송",
-        "link": "http://yna.kr/AKR20200707117400056?did=1195m"
-    },
-    {
         "title": "광주서 60대 남성 코로나19 확진자 잠적…경찰 행방 추적",
         "link": "https://www.nocutnews.co.kr/news/5373670"
-    },
-    {
-        "title": "코로나19 신규 확진자 44명 ...\"해외 유입 24명\"",
-        "link": "https://www.ytn.co.kr/_ln/0103_202007071146139973"
-    },
-    {
-        "title": "카자흐스탄發 확진자 일주일간 53명… 해외유입의 40% 넘어",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/07/07/2020070702852.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
-    },
-    {
-        "title": "'돈 벌어야 한다'…잠적 확진자, 코로나 심각성 뒤늦게 인지",
-        "link": "http://yna.kr/AKR20200707082951054?did=1195m"
     }
 ];
