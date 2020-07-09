c98fd76968d2b1f243e498d20ba469a7bd29e967
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-09 14:31:03 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,16 +25,16 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/07/08/asia/australia-hong-kong-coronavirus-intl-hnk/index.html"
     },
     {
-        "title": "US COVID-19 cases crosses 3 Million mark | US Top news | WION",
-        "link": "https://www.youtube.com/watch?v=Cl5IAprHS1A"
+        "title": "Worsening U.S. outbreak prompts tough actions as new coronavirus cases hit record",
+        "link": "https://www.reuters.com/article/us-health-coronavirus-usa/new-jersey-to-make-face-masks-mandatory-outdoors-as-u-s-outbreak-widens-idUSKBN2492HG"
     },
     {
         "title": "Florida emerges as world's new epicenter for COVID-19 | TheHill",
         "link": "https://thehill.com/homenews/state-watch/506492-florida-emerges-as-worlds-new-epicenter-for-covid-19"
     },
     {
-        "title": "WHO sets up panel to review handling of COVID-19 pandemic",
-        "link": "https://www.reuters.com/article/us-health-coronavirus-who/who-sets-up-panel-to-review-handling-of-pandemic-idUSKBN24A1O9"
+        "title": "Whistleblower equates coronavirus transmission to ‘secondhand smoke,’ suggests stores, restaurants mandate masks",
+        "link": "https://www.yahoo.com/lifestyle/whistleblower-says-coronavirus-transmission-is-like-secondhand-smoke-164829282.html"
     },
     {
         "title": "Sweden has become the world’s cautionary tale",
