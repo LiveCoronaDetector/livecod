0b23a3b65f7e478fe20cc786d0fbc4c7a875cb67
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-10 03:01:51 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,10 +5,6 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200710037351530?did=1195m"
     },
     {
-        "title": "광주서 코로나19 확진자 2명 추가 발생...누적 147명",
-        "link": "http://www.fnnews.com/news/202007100812507456"
-    },
-    {
         "title": "[속보]코로나19 신규확진자 45명...해외유입이 더 많아",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007101021001&code=940100"
     },
@@ -17,27 +13,31 @@ var koreaNewsData = [
         "link": "https://biz.chosun.com/site/data/html_dir/2020/07/10/2020071001198.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
-        "title": "수도권·대전·광주 확산세 여전…신규 확진자는 45명",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4491237&ref=A"
+        "title": "[속보]국내 코로나19 신규 확진 45명...지역발생 22명",
+        "link": "https://www.sedaily.com/NewsView/1Z58YX3084"
+    },
+    {
+        "title": "코로나19 어제 신규확진 45명…지역발생 22명 늘어",
+        "link": "https://www.donga.com/news/article/all/20200710/101908310/1"
     },
     {
         "title": "[전문] 대구시, 이틀 연속 \"코로나19 확진자 0명·사망자 0명\"",
         "link": "https://news.imaeil.com/Society/2020071010360661923"
     },
     {
+        "title": "코로나19 확진 45명 증가…국외유입 23명·지역 22명",
+        "link": "http://www.hani.co.kr/arti/society/health/953116.html"
+    },
+    {
+        "title": "광주서 코로나19 확진자 2명 추가 발생...누적 147명",
+        "link": "http://www.fnnews.com/news/202007100812507456"
+    },
+    {
+        "title": "코로나19 신규확진자 45명… 지방이 수도권보다 많다(종합)",
+        "link": "http://moneys.mt.co.kr/news/mwView.php?no=2020071010548069268"
+    },
+    {
         "title": "대구 이틀째 확진자 ‘0’…경북 나흘연속 확진자 없어(종합)",
         "link": "http://www.kukinews.com/newsView/kuk202007100141"
-    },
-    {
-        "title": "美 코로나19 일일확진자 6만5000명 넘어서...역대최대치 경신(상보)",
-        "link": "https://view.asiae.co.kr/article/2020071010192291676"
-    },
-    {
-        "title": "'코로나'에 데인 쿠팡, 자체 기술로 확진자 방지 나선다",
-        "link": "http://www.inews24.com/view/1280783"
-    },
-    {
-        "title": "코로나 확진 45명…서울 확진자 경북 넘었다(종합)",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/07/10/2020071001182.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     }
 ];
