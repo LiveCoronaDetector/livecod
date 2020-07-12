31d7b44cfa0727ba585e932b5c723dbe28864d34
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-12 01:01:56 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -9,8 +9,8 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/07/11/health/us-coronavirus-saturday/index.html"
     },
     {
-        "title": "Fears high as first four coronavirus cases confirmed in Idlib",
-        "link": "https://www.aljazeera.com/news/2020/07/fears-high-coronavirus-cases-confirmed-idlib-200711181238241.html"
+        "title": "Israelis protest against government response to coronavirus",
+        "link": "https://www.reuters.com/article/us-health-coronavirus-israel-protests/israelis-protest-against-government-response-to-coronavirus-idUSKCN24C0RC"
     },
     {
         "title": "Reopened schools in Europe and Asia have largely avoided coronavirus outbreaks. They have lessons for the U.S.",
@@ -21,23 +21,23 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-asia-india-53375744"
     },
     {
-        "title": "EXCLUSIVE: Chinese virologist accuses Beijing of coronavirus cover-up, flees Hong Kong: 'I know how they tr...",
-        "link": "https://www.foxnews.com/world/chinese-virologist-coronavirus-cover-up-flee-hong-kong-whistleblower"
+        "title": "Japanese governor demands U.S. military meeting after coronavirus outbreak at Okinawa bases",
+        "link": "https://www.nbcnews.com/news/world/japanese-governor-demands-u-s-military-meeting-after-coronavirus-outbreak-n1233574"
+    },
+    {
+        "title": "President Trump wears mask for 1st time in public during coronavirus pandemic",
+        "link": "https://abc7.com/amp/president-donald-trump-face-mask-wears-covid/6313164/"
+    },
+    {
+        "title": "Bollywood superstar Amitabh Bachchan hospitalized for coronavirus",
+        "link": "https://www.cnn.com/2020/07/11/entertainment/amitabh-bachchan-bollywood-superstar-coronavirus-trnd/index.html"
     },
     {
         "title": "Virologist Who Fled Hong Kong for US Claims China Covered up Coronavirus Spread: Report",
         "link": "https://in.news.yahoo.com/virologist-fled-us-hong-kong-135400889.html"
     },
     {
-        "title": "Walt Disney World reopens in Florida amid Covid-19 surge",
-        "link": "https://www.bbc.com/news/world-us-canada-53371336"
-    },
-    {
-        "title": "How To Protect Yourself From The Risk Of Airborne Coronavirus Transmission? : Goats and Soda",
-        "link": "https://www.npr.org/sections/goatsandsoda/2020/07/11/889716534/coronavirus-faq-how-do-i-protect-myself-if-the-coronavirus-can-linger-in-the-air"
-    },
-    {
-        "title": "How coronavirus affects the entire body",
-        "link": "https://www.cnn.com/2020/07/10/health/coronavirus-entire-body-effects-columbia/index.html"
+        "title": "Disney World reopens even as coronavirus cases soar in Florida and across U.S.",
+        "link": "https://www.nbcnews.com/news/us-news/disney-world-reopens-even-coronavirus-cases-rise-florida-across-u-n1233579"
     }
 ];
