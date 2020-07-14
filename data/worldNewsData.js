0fb0a9834c0e9cd6cfba20452b7c8d653b859453
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-14 13:31:27 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -9,8 +9,8 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/07/14/politics/donald-trump-states-economy-coronavirus/index.html"
     },
     {
-        "title": "The coronavirus effect on Pakistan's digital divide",
-        "link": "https://www.bbc.com/worklife/article/20200713-the-coronavirus-effect-on-pakistans-digital-divide"
+        "title": "Israeli Data Show School Openings Were a Disaster That Wiped Out Lockdown Gains",
+        "link": "https://www.thedailybeast.com/israeli-data-show-school-openings-were-a-disaster-that-wiped-out-lockdown-gains"
     },
     {
         "title": "Florida records more new COVID-19 cases than all of Europe combined",
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.foxnews.com/media/li-meng-yan-virologist-china-coronavirus-coverup"
     },
     {
-        "title": "Coronavirus Live News and Updates",
-        "link": "https://www.nytimes.com/2020/07/14/world/coronavirus-updates.html"
-    },
-    {
-        "title": "Florida has more Covid-19 than most countries in the world. These stats show how serious the problem is",
-        "link": "https://www.cnn.com/2020/07/13/health/florida-coronavirus-cases-comparisons/index.html"
+        "title": "Coronavirus cases soar by more than 1 million over 5 days",
+        "link": "https://www.cnn.com/2020/07/14/world/million-coronavirus-cases-five-days-intl/index.html"
     },
     {
         "title": "Coronavirus crisis may get \"worse and worse and worse,\" WHO chief warns",
         "link": "https://www.cbsnews.com/news/coronavirus-pandemic-crisis-worse-world-health-organization-chief-warns/"
     },
     {
-        "title": "Coronavirus cases soar by more than 1 million over 5 days",
-        "link": "https://www.cnn.com/2020/07/14/world/million-coronavirus-cases-five-days-intl/index.html"
+        "title": "Florida has more Covid-19 than most countries in the world. These stats show how serious the problem is",
+        "link": "https://www.cnn.com/2020/07/13/health/florida-coronavirus-cases-comparisons/index.html"
+    },
+    {
+        "title": "Coronavirus: California reimposes sweeping restrictions amid virus spike",
+        "link": "https://www.bbc.com/news/world-us-canada-53399080"
     }
 ];
