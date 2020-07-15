5b103e716d83ea4bfd591d71c32bbbe3b78c4670
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-15 07:02:03 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,35 +9,35 @@ var koreaNewsData = [
         "link": "http://www.hani.co.kr/arti/society/health/953717.html"
     },
     {
+        "title": "정부, 해외 코로나 확진자 관리 위해 방역대상 국가 4개국에서 2개국 추가 지...",
+        "link": "http://www.fnnews.com/news/202007151133366873"
+    },
+    {
         "title": "방문판매 모이고, 확진자 잠적…코로나19 경각심 어디갔나(종합)",
         "link": "http://yna.kr/AKR20200715087151054?did=1195m"
+    },
+    {
+        "title": "광주소방본부 상반기 코로나 환자 2126명 이송",
+        "link": "https://www.news1.kr/articles/?3996714"
     },
     {
         "title": "‘코로나19’ 신규 확진 39명…해외 유입 28명·국내 발생 11명",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4494530&ref=A"
     },
     {
-        "title": "[속보]코로나 신규 환자 39명 늘어…해외 유입만 28명",
-        "link": "https://news.joins.com/article/olink/23419643"
-    },
-    {
         "title": "정 총리 \"코로나19 리스크 국가, 방역강화 대상국 추가 지정\"",
         "link": "https://view.asiae.co.kr/article/2020071509161027461"
     },
     {
-        "title": "코로나 확진 39명…지역발생 11명으로 둔화(종합)",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/07/15/2020071501329.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "[단독]\"혈장공여 하고싶다\" 31번 대구 신천지 확진자의 전화",
+        "link": "https://news.joins.com/article/olink/23419662"
     },
     {
         "title": "이용섭 시장, 광주 코로나 기자회견…확진자·자가격리자 생활비 지원(전문)",
         "link": "http://www.namdonews.com/news/articleView.html?idxno=577911"
     },
     {
-        "title": "코로나19 신규 39명 중 28명 해외유입…20일째 두 자릿수",
-        "link": "http://www.dongascience.com/news/view/38198"
-    },
-    {
-        "title": "'혼잡 빨간불' 뜬 해수욕장 못 간다…확진자 발생땐 폐쇄 검토(종합)",
-        "link": "http://news.mt.co.kr/mtview.php?no=2020071511303885478"
+        "title": "[현장영상] 정세균 총리 \"세계 코로나19 확진자 최고치 경신\"",
+        "link": "https://www.ytn.co.kr/_ln/0101_202007150901097187"
     }
 ];
