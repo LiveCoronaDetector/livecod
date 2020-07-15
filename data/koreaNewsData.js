30fddefb1e5eaa889e9bd47725541324277b3fee
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-15 12:01:54 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,10 +5,6 @@ var koreaNewsData = [
         "link": "http://www.hani.co.kr/arti/society/health/953717.html"
     },
     {
-        "title": "[종합] 코로나19 확진자 39명 증가...해외 28명, 서울 6명, 경기 3명",
-        "link": "http://www.fnnews.com/news/202007151013437476"
-    },
-    {
         "title": "청주 코로나19 확진자 발생…이라크 입국 40대",
         "link": "https://www.news1.kr/articles/?3996096"
     },
@@ -17,8 +13,24 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200715087151054?did=1195m"
     },
     {
+        "title": "[속보] 코로나19 신규 확진자 39명···해외 유입 28명",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007151018001&code=940601"
+    },
+    {
         "title": "‘코로나19’ 신규 확진 39명…해외 유입 28명·국내 발생 11명",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4494530&ref=A"
+    },
+    {
+        "title": "코로나19로 119구조·구급출동 늘었다",
+        "link": "http://www.fnnews.com/news/202007151349122952"
+    },
+    {
+        "title": "[단독] '마주앉지 않겠다'는 北…코로나 지원은 받았다",
+        "link": "https://imnews.imbc.com/replay/2020/nwdesk/article/5843555_32524.html"
+    },
+    {
+        "title": "정부, 해외 코로나 확진자 관리 위해 방역대상 국가 4개국에서 2개국 추가 지...",
+        "link": "http://www.fnnews.com/news/202007151133366873"
     },
     {
         "title": "정 총리 \"코로나19 리스크 국가, 방역강화 대상국 추가 지정\"",
@@ -27,17 +39,5 @@ var koreaNewsData = [
     {
         "title": "\"테슬라 프리몬트공장서 130여명 코로나 확진\"",
         "link": "https://biz.chosun.com/site/data/html_dir/2020/07/15/2020071500667.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
-    },
-    {
-        "title": "한 자릿수로 떨어진 수도권 확진자…\"이번 주말 방역조치 완화 논의\"(종합)",
-        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=02673206625834848"
-    },
-    {
-        "title": "[속보]코로나 신규 환자 39명 늘어…해외 유입만 28명",
-        "link": "https://news.joins.com/article/olink/23419643"
-    },
-    {
-        "title": "평창에서 코로나19 확진자 2명 발생…과테말라서 입국한 20대 여성의 모친과 ...",
-        "link": "http://www.kwnews.co.kr/view.asp?aid=220071400149&s=501"
     }
 ];
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,8 +5,8 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-africa-53396057"
     },
     {
-        "title": "Live updates: Coronavirus caseloads soar outside hot spots as states report record fatalities",
-        "link": "https://www.washingtonpost.com/nation/2020/07/15/coronavirus-live-updates-us/"
+        "title": "Los Angeles County has more cases of coronavirus than entire country of Canada, mayor says",
+        "link": "https://www.cbsnews.com/news/coronavirus-los-angeles-county-california-cases-entire-country-canada/"
     },
     {
         "title": "Israelis Fear Schools Reopened Too Soon as Covid-19 Cases Climb",
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.nytimes.com/2020/07/14/us/politics/trump-fact-check-biden-police-coronavirus-china.html"
     },
     {
-        "title": "Florida Breaks Its Record for Most Coronavirus Deaths in a Day",
-        "link": "https://www.nytimes.com/2020/07/14/world/coronavirus-update.html"
-    },
-    {
         "title": "Global surge in coronavirus cases is being fed by the developing world — and the U.S.",
         "link": "https://www.washingtonpost.com/national/global-surge-in-coronavirus-cases-is-being-fed-by-the-developing-world--and-the-us/2020/07/14/1e9ca48e-c605-11ea-8ffe-372be8d82298_story.html"
+    },
+    {
+        "title": "Florida Breaks Its Record for Most Coronavirus Deaths in a Day",
+        "link": "https://www.nytimes.com/2020/07/14/world/coronavirus-update.html"
     },
     {
         "title": "Coronavirus cases soar by more than 1 million over 5 days",
         "link": "https://www.cnn.com/2020/07/14/world/million-coronavirus-cases-five-days-intl/index.html"
     },
     {
-        "title": "Tunisian blogger gets jail time for mocking Koran",
-        "link": "https://www.nydailynews.com/news/world/ny-tunisian-blogger-20200715-6aokcfkw3bbijklgjb6yetp3vu-story.html"
+        "title": "Florida, Texas and California account for about one-fifth of the world's new coronavirus cases",
+        "link": "https://www.nbcnews.com/news/us-news/florida-texas-california-account-about-one-fifth-world-s-new-n1233793"
     }
 ];
