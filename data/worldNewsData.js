4e711b6124a157df89732ea11899e728a7b1eb21
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-15 03:31:14 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,36 +5,36 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-africa-53396057"
     },
     {
-        "title": "Florida Breaks Its Record for Most Covid Deaths in a Day: Live Updates",
-        "link": "https://www.nytimes.com/2020/07/14/world/coronavirus-update.html"
+        "title": "22 states and two territories have set a daily coronavirus record since July 1",
+        "link": "https://www.nbcnews.com/news/us-news/july-2020-united-states-coronavirus-records-n1233828"
     },
     {
         "title": "Israelis Fear Schools Reopened Too Soon as Covid-19 Cases Climb",
         "link": "https://www.wsj.com/articles/israelis-fear-schools-reopened-too-soon-as-covid-19-cases-climb-11594760001"
     },
     {
-        "title": "First known recorded case of baby contracting COVID-19 in womb - Business Insider",
-        "link": "https://www.businessinsider.com/unborn-baby-contracting-coronavirus-mother-womb-france-2020-7"
+        "title": "Germany considers local coronavirus quarantines to avoid second wave",
+        "link": "https://www.foxnews.com/world/germany-considers-local-coronavirus-quarantines-second-wave-virus"
     },
     {
-        "title": "Coronavirus live news: India Covid-19 cases top 900,000 as 133m re-enter lockdown",
-        "link": "https://www.theguardian.com/world/live/2020/jul/15/coronavirus-live-news-update-covid-19-cases-latest-updates-india-lockdown"
+        "title": "India could see a current account surplus again. But it's for 'all the wrong reasons'",
+        "link": "https://www.cnbc.com/2020/07/15/india-current-account-could-be-in-surplus-as-coronavirus-hurts-exports.html"
     },
     {
         "title": "China has just contained the coronavirus. Now it's battling some of the worst floods in decades",
         "link": "https://www.cnn.com/2020/07/14/asia/china-flood-coronavirus-intl-hnk/index.html"
     },
     {
-        "title": "Coronavirus crisis may get \"worse and worse and worse,\" WHO chief warns",
-        "link": "https://www.cbsnews.com/news/coronavirus-pandemic-crisis-worse-world-health-organization-chief-warns/"
-    },
-    {
         "title": "Florida, Texas and California account for about one-fifth of the world's new coronavirus cases",
         "link": "https://www.nbcnews.com/news/us-news/florida-texas-california-account-about-one-fifth-world-s-new-n1233793"
     },
     {
-        "title": "Coronavirus: Blogger Emna Charqui given jail term over Koran-style post",
-        "link": "https://www.bbc.com/news/world-africa-53408262"
+        "title": "Coronavirus crisis may get \"worse and worse and worse,\" WHO chief warns",
+        "link": "https://www.cbsnews.com/news/coronavirus-pandemic-crisis-worse-world-health-organization-chief-warns/"
+    },
+    {
+        "title": "Coronavirus cases soar by more than 1 million over 5 days",
+        "link": "https://www.cnn.com/2020/07/14/world/million-coronavirus-cases-five-days-intl/index.html"
     },
     {
         "title": "Florida has more Covid-19 than most countries in the world. These stats show how serious the problem is",
