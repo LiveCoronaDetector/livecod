a2f4f2f2b26d85014c4317e1c75715b5f5553c7e
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-17 02:31:08 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,39 +5,39 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200717012100056?did=1195m"
     },
     {
-        "title": "[종합] 제주도 확진자 4명 추가 발생 '초비상'...사우나.다방에서도 감염",
-        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=422476"
+        "title": "[속보] 제주 확진자 발생…서울 방문객 접촉자 4명으로 늘어",
+        "link": "https://www.hankyung.com/society/article/2020071788507"
     },
     {
         "title": "제주 코로나 확진자 여파···한림읍 모든 학교, 원격 수업",
         "link": "http://www.newsjeju.net/news/articleView.html?idxno=347522"
     },
     {
+        "title": "제주 1명 추가 확진…'해열제' 먹고 제주여행 광진구 확진자 접촉",
+        "link": "https://www.news1.kr/articles/?3998512"
+    },
+    {
         "title": "[코로나19] 제주도, 광진구 확진자 접촉한 1명 추가 확진",
         "link": "http://www.econovill.com/news/articleView.html?idxno=404977"
     },
     {
-        "title": "제주 1명 추가 확진…'해열제' 먹고 제주여행 광진구 확진자 접촉",
-        "link": "https://www.news1.kr/articles/?3998512"
+        "title": "광진구 코로나19 확진자에 제주 2차감염 '확산'",
+        "link": "https://www.nocutnews.co.kr/news/5379993"
+    },
+    {
+        "title": "\"해열제 먹고 여행\" 제주도 확진자 4명 추가 발생…2차 감염 '비상'",
+        "link": "http://news.tf.co.kr/read/life/1802452.htm"
+    },
+    {
+        "title": "여름철 관광객 붐비는 제주서 코로나 n차 감염 '비상'",
+        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005887845&plink=ORI&cooper=NAVER"
     },
     {
         "title": "코로나 확진 60명…지역발생 수도권 12명·광주 5명(종합)",
         "link": "https://biz.chosun.com/site/data/html_dir/2020/07/17/2020071701320.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
-        "title": "광진구 코로나19 확진자 제주도 방문 동선 공개",
-        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=422466"
-    },
-    {
-        "title": "광진구 코로나19 확진자...확진 전 4박5일 제주 여행",
-        "link": "https://www.pressian.com/pages/articles/2020071622313225327?utm_source=naver&utm_medium=search"
-    },
-    {
-        "title": "“서울 확진자와 접촉” 제주서 코로나 3명 확진",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007162047001&code=620117"
-    },
-    {
-        "title": "원희룡 지사, 코로나19 차단 긴급대책회의 주재",
-        "link": "https://www.pressian.com/pages/articles/2020071706012462258?utm_source=naver&utm_medium=search"
+        "title": "제주여행 온 서울 확진자, 60명 접촉…4명, 2차 감염",
+        "link": "http://www.fnnews.com/news/202007171104298774"
     }
 ];
