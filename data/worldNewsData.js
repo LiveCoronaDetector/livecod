82aee7217fb545494303da6aed951a9d1b21b901
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-17 08:01:35 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,8 +5,8 @@ var worldNewsData = [
         "link": "https://www.scmp.com/news/world/africa/article/3093354/coronavirus-after-surge-cases-south-africa-becomes-africas-clear"
     },
     {
-        "title": "US, UK and Canada accuse Russia of Covid-19 hack",
-        "link": "https://www.cnn.com/videos/politics/2020/07/16/russia-cyberattack-covid-19-coronavirus-vaccine-research-vpx.cnn"
+        "title": "Live updates: U.S. sets new single-day record with more than 70,000 new coronavirus cases",
+        "link": "https://www.washingtonpost.com/nation/2020/07/17/coronavirus-live-updates-us/"
     },
     {
         "title": "Israel’s second coronavirus wave threatens Netanyahu’s hold on power",
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.foxnews.com/media/mike-pompeo-china-coronavirus-human-transmission-who"
     },
     {
-        "title": "Trump administration weighs travel ban for Chinese Communist Party members, source says",
-        "link": "https://www.cnbc.com/2020/07/17/us-weighs-travel-ban-for-chinese-communist-party-members-source-says.html"
-    },
-    {
         "title": "Brazil Passes 75000 Coronavirus Deaths As Bolsonaro Stands By Hydroxychloroquine | NBC News NOW",
         "link": "https://www.youtube.com/watch?v=Xf9FVlGw9lg"
+    },
+    {
+        "title": "Trump administration weighs travel ban for Chinese Communist Party members, source says",
+        "link": "https://www.cnbc.com/2020/07/17/us-weighs-travel-ban-for-chinese-communist-party-members-source-says.html"
     },
     {
         "title": "Colombian cartels killing anyone who doesn't obey coronavirus lockdown orders: report",
         "link": "https://www.foxnews.com/world/colombian-cartels-coronavirus-lockdown-orders"
     },
     {
-        "title": "Spain to cull nearly 100,000 mink in coronavirus outbreak",
-        "link": "https://www.theguardian.com/world/2020/jul/17/spain-to-cull-nearly-100000-mink-in-coronavirus-outbreak"
+        "title": "Coronavirus live news: US records world record 77,300 new infections in one day",
+        "link": "https://www.theguardian.com/world/live/2020/jul/17/coronavirus-live-news-brazil-cases-near-2m-as-more-countries-re-introduce-restrictions"
     }
 ];
