9f5eddb248fd25083ef5184261e43650b7522eb7
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-21 21:31:18 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,8 +5,8 @@ var worldNewsData = [
         "link": "https://www.aljazeera.com/news/2020/07/sounds-alarm-spread-coronavirus-africa-200720184812122.html"
     },
     {
-        "title": "Coronavirus Live Updates",
-        "link": "https://www.nytimes.com/2020/07/21/world/coronavirus-covid-19.html"
+        "title": "Fauci says he has not been invited to Trump's coronavirus briefing – live",
+        "link": "https://www.theguardian.com/us-news/live/2020/jul/21/coronavirus-covid-19-donald-trump-mike-pompeo-china-joe-biden-portland-live-updates"
     },
     {
         "title": "California coronavirus cases top 400,000 – Live updates",
@@ -17,16 +17,12 @@ var worldNewsData = [
         "link": "https://www.cnbc.com/2020/07/21/germanys-coronavirus-response-masterful-science-communication.html"
     },
     {
-        "title": "India now has more than 1 million coronavirus cases. What does that mean -- and how did the country get here?",
-        "link": "https://www.cnn.com/2020/07/18/asia/india-coronavirus-one-million-data-intl-hnk/index.html"
+        "title": "India coronavirus: 'Our neighbours made us Covid-19 pariahs'",
+        "link": "https://news.yahoo.com/india-coronavirus-neighbours-made-us-233055302.html"
     },
     {
         "title": "Chinese hackers charged by Justice Department with trying to steal US coronavirus research",
         "link": "https://www.foxnews.com/world/chinese-hackers-charged-steal-usa-coronavirus-research"
-    },
-    {
-        "title": "Coronavirus: Why are Americans so angry about masks?",
-        "link": "https://www.bbc.com/news/world-us-canada-53477121"
     },
     {
         "title": "'Flying blind': US failure to report vital coronavirus data is hobbling response",
@@ -35,6 +31,10 @@ var worldNewsData = [
     {
         "title": "Sweden had one of the most relaxed COVID-19 lockdowns in the world. There's growing evidence that it help..",
         "link": "https://markets.businessinsider.com/news/stocks/sweden-coronavirus-economy-relaxed-lockdown-stronger-rest-of-europe-2020-7-1029415101"
+    },
+    {
+        "title": "Live updates: U.S. may have 10 times more coronavirus cases than reported; Walmart to close on Thanksgiving",
+        "link": "https://www.washingtonpost.com/nation/2020/07/21/covid-live-updates-us/"
     },
     {
         "title": "Coronavirus: EU leaders reach recovery deal after marathon summit",
