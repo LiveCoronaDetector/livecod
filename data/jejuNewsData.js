d85054d0dc0695615fe1cc8561d93a89c55366b5
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-21 16:31:30 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -13,6 +13,10 @@ var jejuNewsData = [
         "link": "https://www.pressian.com/pages/articles/2020072022004422437?utm_source=naver&utm_medium=search"
     },
     {
+        "title": "제주도교육청, 한림읍 초·중·고 대면수업 22일 재개",
+        "link": "http://www.fnnews.com/news/202007201526312777"
+    },
+    {
         "title": "광진구 확진자 다녀간 제주 한림, 5일간 1524명 검사",
         "link": "http://news.kmib.co.kr/article/view.asp?arcid=0014821692&code=61122027&cp=nv"
     },
@@ -27,10 +31,6 @@ var jejuNewsData = [
     {
         "title": "제주 26번 확진자 접촉 4명→16명으로 늘어났다",
         "link": "http://www.jejusori.net/news/articleView.html?idxno=318179"
-    },
-    {
-        "title": "제주도교육청, 한림읍 초·중·고 대면수업 22일 재개",
-        "link": "http://www.fnnews.com/news/202007201526312777"
     },
     {
         "title": "제주 코로나19 26번째 확진자 발생…첫 3차 감염",
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -13,20 +13,16 @@ var worldNewsData = [
         "link": "https://www.aljazeera.com/news/2020/07/coronavirus-outbreak-australia-victoria-grows-live-updates-200720235536147.html"
     },
     {
-        "title": "E.U. Adopts Groundbreaking Stimulus to Fight Coronavirus Recession",
-        "link": "https://www.nytimes.com/2020/07/20/world/europe/eu-stimulus-coronavirus.html"
+        "title": "Germany's coronavirus response is a master class in science communication",
+        "link": "https://www.cnbc.com/2020/07/21/germanys-coronavirus-response-masterful-science-communication.html"
     },
     {
-        "title": "India coronavirus: Nearly one in four in Delhi had Covid-19, study says",
-        "link": "https://www.bbc.com/news/world-asia-india-53485039"
+        "title": "India now has more than 1 million coronavirus cases. What does that mean -- and how did the country get here?",
+        "link": "https://www.cnn.com/2020/07/18/asia/india-coronavirus-one-million-data-intl-hnk/index.html"
     },
     {
         "title": "Iran reports record daily toll of 229 coronavirus deaths",
         "link": "https://news.yahoo.com/iran-reports-record-daily-toll-229-coronavirus-deaths-102238309.html"
-    },
-    {
-        "title": "Coronavirus Live Updates: C.D.C. Says Case Numbers Could Be 2-13 Times Higher Than Reported in Parts of U.S.",
-        "link": "https://www.nytimes.com/2020/07/21/world/coronavirus-covid-19.html"
     },
     {
         "title": "Republicans Begin New Relief Talks as Hopes Fade for Rapid Recovery",
@@ -39,5 +35,9 @@ var worldNewsData = [
     {
         "title": "Coronavirus: EU leaders reach recovery deal after marathon summit",
         "link": "https://www.bbc.com/news/world-europe-53481542"
+    },
+    {
+        "title": "New Treatment for Covid-19 Shows Promise, but Scientists Urge Caution",
+        "link": "https://www.nytimes.com/2020/07/20/world/covid-19-treatment-synairgen-interferon-beta.html"
     }
 ];
