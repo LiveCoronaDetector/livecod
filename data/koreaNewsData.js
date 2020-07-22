fd7bffac99c34ceadcb1934600786bc3341d978c
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-22 06:01:49 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,39 +5,39 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200722021651504?did=1195m"
     },
     {
-        "title": "전방부대 장병 13명 무더기로 ‘코로나19’ 감염",
-        "link": "http://www.hani.co.kr/arti/politics/defense/954659.html"
-    },
-    {
         "title": "포천 육군부대 13명 코로나19 '집단감염'…주둔지 전 병력 격리(종합2보)",
         "link": "http://yna.kr/AKR20200722021652504?did=1195m"
     },
     {
-        "title": "文 “이겨가고 있다” 발언 뒤 이틀째 확진자 급증…다시 60명대(종합)",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200722500034&wlog_tag3=naver"
-    },
-    {
-        "title": "[종합] 육군 전방부대서 코로나19 최소 8명 감염···확산 비상",
-        "link": "https://www.sedaily.com/NewsView/1Z5EHF84DK"
-    },
-    {
-        "title": "광주 코로나19 확진자 4명 추가 발생…누적 환자 191명",
-        "link": "https://www.nocutnews.co.kr/news/5382346"
-    },
-    {
-        "title": "전 세계 코로나19 누적 확진자 1천500만 명 넘었다",
-        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005894709&plink=ORI&cooper=NAVER"
+        "title": "[속보]포천 육군 전방부대서 코로나19 확진자 8명 발생…확산 비상",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007220844001&code=940100"
     },
     {
         "title": "포천 육군 부대서 코로나19 집단감염…최소 8명 확인",
         "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005894335&plink=ORI&cooper=NAVER"
     },
     {
-        "title": "日, 코로나19 치료제로 항염증약 '덱사메타손' 승인",
-        "link": "https://imnews.imbc.com/news/2020/world/article/5850367_32640.html"
+        "title": "文 “이겨가고 있다” 발언 뒤 이틀째 확진자 급증…다시 60명대(종합)",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200722500034&wlog_tag3=naver"
     },
     {
-        "title": "[속보] 전세계 코로나19 확진자 1500만명 넘어…확산세 더 빨라졌다",
-        "link": "https://www.hankyung.com/international/article/2020072249207"
+        "title": "[속보] 코로나19 신규 확진 63명···국내 발생 29명",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007221032001&code=940601"
+    },
+    {
+        "title": "美 CDC \"코로나 실제 감염자, 확진자의 최대 13배\"",
+        "link": "http://news.tvchosun.com/site/data/html_dir/2020/07/22/2020072290044.html"
+    },
+    {
+        "title": "코로나19 어제 63명 신규확진…해외유입 34명",
+        "link": "http://www.busan.com/view/busan/view.php?code=2020072210150516300"
+    },
+    {
+        "title": "광주 코로나19 확진자 4명 추가 발생…누적 환자 191명",
+        "link": "https://www.nocutnews.co.kr/news/5382346"
+    },
+    {
+        "title": "‘코로나19’ 확진자 63명 추가…다시 ‘60명’ 대",
+        "link": "http://www.ichannela.com/news/main/news_detailPage.do?publishId=000000212492"
     }
 ];
