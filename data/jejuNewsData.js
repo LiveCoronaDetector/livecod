20bc4acbf79df2a6333a76277211f1cc3ef0d762
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-25 14:01:40 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -9,16 +9,20 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200720159251056?did=1195m"
     },
     {
-        "title": "제주 26번 확진자 접촉자 '4→16→15명' 정정",
-        "link": "http://www.jejusori.net/news/articleView.html?idxno=318257"
+        "title": "코로나19, 선생님의 열정·헌신 '달라진 학교 이야기'",
+        "link": "http://www.jejusori.net/news/articleView.html?idxno=318305"
+    },
+    {
+        "title": "제주 애월읍 확진자 15명 접촉 확인...왜 '2일 동선'만 공개?",
+        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=423098"
     },
     {
         "title": "제주 17번 확진자 방글라데시 유학생 퇴원…현재 7명 치료 중",
         "link": "http://www.newsis.com/view/?id=NISX20200724_0001107099&cID=10813&pID=10800"
     },
     {
-        "title": "제주도 25번째 코로나 확진자 발생...'일본서 입국'",
-        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=422601"
+        "title": "제주도 코로나19 확진자 발생...제주대병원서 입원 치료",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200718500048&wlog_tag3=naver"
     },
     {
         "title": "'거짓진술' 송파 60번·강남 91번 확진자 고발 조치",
@@ -35,9 +39,5 @@ var jejuNewsData = [
     {
         "title": "코로나19로 제주 애월 한 초등학교 등교 거부",
         "link": "http://www.hani.co.kr/arti/area/jeju/954738.html"
-    },
-    {
-        "title": "\"제주도, 코로나19 확진자 100명 넘으면 의료체계 붕괴 위험\"",
-        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=422983"
     }
 ];
