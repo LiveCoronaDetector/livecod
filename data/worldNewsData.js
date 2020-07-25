232086ec4556362872ffa3e1054a78720b65f5f0
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-25 01:31:15 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -21,8 +21,8 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/07/24/india/india-covid-rape-intl-scli/index.html"
     },
     {
-        "title": "Gold surges to record close as U.S.-China tensions escalate and coronavirus concerns rise",
-        "link": "https://www.cnbc.com/2020/07/24/gold-surges-to-record-close-as-us-china-tensions-escalate-and-coronavirus-concerns-rise.html"
+        "title": "Coronavirus live news: Close watch on Victoria, Australia, and record infections globally",
+        "link": "https://www.theguardian.com/world/live/2020/jul/25/coronavirus-live-news-close-watch-on-victoria-australia-and-record-infections-globally"
     },
     {
         "title": "Live Coronavirus News Updates",
@@ -33,8 +33,8 @@ var worldNewsData = [
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-07-24-20-intl/index.html"
     },
     {
-        "title": "Did Sweden's coronavirus strategy succeed or fail?",
-        "link": "https://www.bbc.com/news/world-europe-53498133"
+        "title": "Coronavirus: The week when everything changed for Trump",
+        "link": "https://www.bbc.com/news/world-us-canada-53532880"
     },
     {
         "title": "WHO slams 'unacceptable' remarks by U.S. Secretary of State Mike Pompeo",
