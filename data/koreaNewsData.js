8a9ec51fd211cd0c7fa3ca9347a6bbb9e7ae3eed
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-25 07:01:35 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -13,20 +13,20 @@ var koreaNewsData = [
         "link": "http://news.tvchosun.com/site/data/html_dir/2020/07/25/2020072590008.html"
     },
     {
+        "title": "'국경봉쇄' 베트남서 100일 만에 경로 불명 코로나19 발생(종합)",
+        "link": "http://yna.kr/AKR20200725031951084?did=1195m"
+    },
+    {
         "title": "방역당국 \"코로나19 일일 신규 확진자 수 내일 100명 넘을 듯\"",
         "link": "http://yna.kr/AKR20200724103700530?did=1195m"
     },
     {
-        "title": "[속보] 해외유입 확진자에 의한 지역사회 전파 총 8건…15명 감염",
-        "link": "https://www.hankyung.com/life/article/2020072578417"
-    },
-    {
-        "title": "수도권 곳곳에서 확진자 잇달아 발생…러시아 선박 8명 추가",
+        "title": "수도권 곳곳서 확진자 잇따라 발생…러시아 선박 8명 추가",
         "link": "https://www.seoul.co.kr/news/newsView.php?id=20200725500057&wlog_tag3=naver"
     },
     {
-        "title": "‘집단감염’ 포천 군부대 코로나19 추가 확진자 없어",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4502556&ref=A"
+        "title": "코로나19 신규 확진자 113명…115일 만에 첫 세 자리",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4502553&ref=A"
     },
     {
         "title": "신규 확진자 113명...이라크 귀국 노동자·러시아 선박 영향",
@@ -37,7 +37,7 @@ var koreaNewsData = [
         "link": "http://www.fnnews.com/news/202007251111582170"
     },
     {
-        "title": "코로나19 신규 확진자 113명…115일 만에 첫 세 자리",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4502553&ref=A"
+        "title": "코로나 해외유입 확진 지역전파 8건·15명 감염",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007251526001&code=940100"
     }
 ];
