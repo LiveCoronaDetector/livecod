fac8272ea2ec270f4e6e81bb7bceaf07fa785116
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-27 05:01:53 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,6 +5,10 @@ var koreaNewsData = [
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=02191046625838784"
     },
     {
+        "title": "[속보]\"월북 탈북민, 코로나 확진자·접촉자로 분류된 바 없어\"",
+        "link": "https://www.nocutnews.co.kr/news/5384884"
+    },
+    {
         "title": "전남서 코로나19 37번째 확진자 발생…해외입국 40대 여성",
         "link": "http://www.fnnews.com/news/202007270943428752"
     },
@@ -13,8 +17,8 @@ var koreaNewsData = [
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007270838001&code=940100"
     },
     {
-        "title": "월북 탈북민, 코로나19 확진자·접촉자 분류된 바 없어",
-        "link": "https://www.nocutnews.co.kr/news/5384908"
+        "title": "코로나19 하루 신규 확진자 25명…지역감염 9명·해외유입 16명",
+        "link": "http://www.dongascience.com/news/view/38485"
     },
     {
         "title": "[단독]두 달간 사망자 발생 속도 급감…'코로나19' 약해졌나",
@@ -25,19 +29,15 @@ var koreaNewsData = [
         "link": "https://news.imaeil.com/Society/2020072710243074675"
     },
     {
-        "title": "[속보] 코로나 확진자 25명 추가… 해외유입 16명",
-        "link": "http://health.chosun.com/site/data/html_dir/2020/07/27/2020072701632.html"
-    },
-    {
-        "title": "7월 확진자 755명이 외국인…'공짜치료' 안 해준다",
-        "link": "http://news.mt.co.kr/mtview.php?no=2020072617335194152"
+        "title": "피서철 전세계 코로나 비상...확진자 1600만명 훌쩍넘어(종합)",
+        "link": "https://view.asiae.co.kr/article/2020072711224951248"
     },
     {
         "title": "[종합] 코로나19 확진자 25명 증가...해외 16명, 서울 6명",
         "link": "http://www.fnnews.com/news/202007271015382995"
     },
     {
-        "title": "[속보] 코로나 신규확진 25명… 서울 9명",
-        "link": "http://news.kmib.co.kr/article/view.asp?arcid=0014844975&code=61121111&cp=nv"
+        "title": "국내 코로나19 신규 확진 25명…국내 발생 9명·해외 유입 16명",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4503277&ref=A"
     }
 ];
