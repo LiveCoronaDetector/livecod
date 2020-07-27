11fb2bec2754e5b16b34114755a2c52f0f2ed1f7
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-27 18:01:34 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,12 +9,16 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200727129152004?did=1195m"
     },
     {
+        "title": "[속보] 정부서울청사 3층 폐쇄 해제...확진자의 접촉자 모두 '음성'",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007270838001&code=940100"
+    },
+    {
         "title": "코로나19 확진자 20명대 '진정세'…지역발생 9명(종합)",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=02191046625838784"
     },
     {
-        "title": "[속보] 정부서울청사 3층 폐쇄 해제...확진자의 접촉자 모두 '음성'",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202007270838001&code=940100"
+        "title": "QR코드 찍지도 않고 유흥주점 들어간 제주 26번 확진자 고발",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200727500090&wlog_tag3=naver"
     },
     {
         "title": "전남서 코로나19 37번째 확진자 발생…해외입국 40대 여성",
@@ -35,9 +39,5 @@ var koreaNewsData = [
     {
         "title": "월북 탈북민, 코로나19 확진자·접촉자 분류된 바 없어",
         "link": "https://www.nocutnews.co.kr/news/5384908"
-    },
-    {
-        "title": "서울시청서 코로나19 확진자 1명 발생…1개층 폐쇄",
-        "link": "https://www.donga.com/news/article/all/20200727/102178544/2"
     }
 ];
