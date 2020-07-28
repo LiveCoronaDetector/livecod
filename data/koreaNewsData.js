fa39c09ce1d00557556c93b0be3b0c76c47b5ce3
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-28 04:01:56 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,12 +9,12 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200727047051084?did=1195m"
     },
     {
-        "title": "코로나19 신규 확진자 28명…이틀째 20명대 유지",
-        "link": "http://www.kwnews.co.kr/nview.asp?aid=220072700144"
+        "title": "코로나 신규 확진자 28명…국외유입 23명",
+        "link": "http://www.hani.co.kr/arti/society/health/955479.html"
     },
     {
-        "title": "국내 코로나19 신규 확진 28명…해외 유입 23명·국내 발생 5명",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4504269&ref=A"
+        "title": "코로나19 신규 확진자 28명…이틀째 20명대 유지",
+        "link": "http://www.kwnews.co.kr/nview.asp?aid=220072700144"
     },
     {
         "title": "서울시청서 코로나19 확진자 발생…11층 폐쇄(종합)",
@@ -33,11 +33,11 @@ var koreaNewsData = [
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=02191046625838784"
     },
     {
-        "title": "전남서 코로나19 37번째 확진자 발생…해외입국 40대 여성",
-        "link": "http://www.fnnews.com/news/202007270943428752"
+        "title": "QR코드 찍지도 않고 유흥주점 들어간 제주 26번 확진자 고발",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200727500090&wlog_tag3=naver"
     },
     {
-        "title": "트럼프, 안보보좌관 코로나 확진에 \"최근에 그 사람 안봐\"",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/07/28/2020072800375.html?utm_source=naver&utm_medium=original&utm_campaign=news"
+        "title": "전남서 코로나19 37번째 확진자 발생…해외입국 40대 여성",
+        "link": "http://www.fnnews.com/news/202007270943428752"
     }
 ];
