5ef1b902f9c4baf64e31c708a26246beeca7adc1
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-28 06:01:42 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -21,23 +21,23 @@ var koreaNewsData = [
         "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005902555&plink=ORI&cooper=NAVER"
     },
     {
-        "title": "베트남, 다낭發 코로나 19 재확산 조짐",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/07/28/2020072800158.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "국내 코로나19 신규 확진 28명…해외 유입 23명·국내 발생 5명",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4504269&ref=A"
     },
     {
         "title": "전남서 코로나19 37번째 확진자 발생…해외입국 40대 여성",
         "link": "http://www.fnnews.com/news/202007270943428752"
     },
     {
-        "title": "국내 코로나19 신규 확진 28명…해외 유입 23명·국내 발생 5명",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4504269&ref=A"
-    },
-    {
         "title": "트럼프, 안보보좌관 코로나 확진에 \"최근에 그 사람 안봐\"",
         "link": "https://news.chosun.com/site/data/html_dir/2020/07/28/2020072800375.html?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
-        "title": "코로나19 신규 확진 28명…지역발생 5명·해외유입 23명",
-        "link": "http://news.tvchosun.com/site/data/html_dir/2020/07/28/2020072890034.html"
+        "title": "베트남, 다낭發 코로나 19 재확산 조짐",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/07/28/2020072800158.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
+        "title": "'트럼프 최측근' 오브라이언 국가안보보좌관 코로나19 확진",
+        "link": "http://www.ohmynews.com/NWS_Web/View/at_pg.aspx?CNTN_CD=A0002661990&CMPT_CD=P0010&utm_source=naver&utm_medium=newsearch&utm_campaign=naver_news"
     }
 ];
