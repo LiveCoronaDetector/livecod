c1e2d0297c164be81142c555a76ebfa2dc08cc6e
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-28 14:02:04 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -9,12 +9,12 @@ var worldNewsData = [
         "link": "https://www.npr.org/sections/coronavirus-live-updates/2020/07/27/895940087/brazils-bolsonaro-is-accused-of-crime-against-humanity-over-coronavirus-neglect"
     },
     {
-        "title": "Over the red line: Hospitals report over 100% capacity",
-        "link": "https://www.jpost.com/breaking-news/coronavirus-testing-drops-to-12000-only-1029-diagnosed-with-the-virus-636449"
+        "title": "Israel’s leading demographer warns of post-COVID-19 exodus by young, educated Israelis",
+        "link": "https://www.haaretz.com/israel-news/.premium-demographer-warns-of-post-pandemic-covid-19-coronavirus-exodus-israelis-1.9024614"
     },
     {
-        "title": "Spain quarantine: coronavirus is now ruining summer vacation plans",
-        "link": "https://www.cnn.com/travel/article/summer-vacations-spain-quarantine-gbr-intl/index.html"
+        "title": "Coronavirus: German officials 'very concerned' by rising cases",
+        "link": "https://www.bbc.com/news/world-europe-53566880"
     },
     {
         "title": "India's PM says the country's fight against Covid-19 has proven the world wrong",
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.scmp.com/economy/article/3094811/china-needs-weigh-economic-consequences-coronavirus-stimulus-top-beijing"
     },
     {
+        "title": "Live Coronavirus News Updates and Analysis",
+        "link": "https://www.nytimes.com/2020/07/28/world/coronavirus-covid-19.html"
+    },
+    {
+        "title": "Hong Kong was a pandemic poster child. Now it’s a cautionary tale.",
+        "link": "https://www.washingtonpost.com/world/asia_pacific/hong-kong-was-a-pandemic-poster-child-now-its-a-cautionary-tale/2020/07/28/c6ccbd72-cfe4-11ea-826b-cc394d824e35_story.html"
+    },
+    {
         "title": "Coronavirus 'most severe health emergency' WHO has faced",
         "link": "https://www.bbc.com/news/world-53557577"
     },
     {
         "title": "Opinion | What America's Coronavirus Response Looks Like Abroad",
         "link": "https://www.nytimes.com/2020/07/27/opinion/covid-19-global-response.html"
-    },
-    {
-        "title": "The world's biggest coronavirus vaccine study begins, a U.S. trial that will include 30,000 people to see if the shots really work",
-        "link": "https://www.cnbc.com/2020/07/27/the-worlds-biggest-coronavirus-vaccine-study-begins-a-us-trial-that-will-include-30000-people-to-see-if-the-shots-really-work.html"
-    },
-    {
-        "title": "Even countries that got coronavirus under control are now struggling. That's deeply concerning for the rest of the world",
-        "link": "https://www.cnn.com/2020/07/28/asia/coronavirus-hong-kong-vietnam-china-intl-hnk/index.html"
     }
 ];
