aef660123130fdf790a050e7fa7e96d9619470fb
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-31 00:47:57 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -17,8 +17,20 @@ var jejuNewsData = [
         "link": "http://news.bbsi.co.kr/news/articleView.html?idxno=997829"
     },
     {
+        "title": "용인 125번 확진자, 전북도청 다녀가…도청 방역 비상",
+        "link": "http://www.jjan.kr/news/articleView.html?idxno=2088537"
+    },
+    {
         "title": "광진구 20번 확진자 사태, 제주 n차 감염 고비 넘기나",
         "link": "http://www.jejunews.com/news/articleView.html?idxno=2168798"
+    },
+    {
+        "title": "본격적인 휴가 시즌…커지는 '코로나19 재확산' 우려",
+        "link": "http://www.newsquest.co.kr/news/articleView.html?idxno=77985"
+    },
+    {
+        "title": "코로나19 확진자 27일 전북도청, 완주군청 방문",
+        "link": "http://www.jeollailbo.com/news/articleView.html?idxno=604573"
     },
     {
         "title": "제주도 서울 광진구發 코로나19 감염 'n차'로 가나?",
@@ -27,17 +39,5 @@ var jejuNewsData = [
     {
         "title": "제주도 26번 확진자 출입명부 미작성 유흥업소 업주 고발",
         "link": "https://www.seoul.co.kr/news/newsView.php?id=20200723500133&wlog_tag3=naver"
-    },
-    {
-        "title": "제주도 25번째 코로나 확진자 발생...'일본서 입국'",
-        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=422601"
-    },
-    {
-        "title": "5개월만에 재개하는 LPGA... '투어불참' 유소연 \"미국 코로나 상황 안 좋다\"",
-        "link": "http://www.newspim.com/news/view/20200730000027"
-    },
-    {
-        "title": "[코로나19] 용인 125번 확진자 접촉자 발생 동향",
-        "link": "https://www.wikitree.co.kr/articles/557536"
     }
 ];
