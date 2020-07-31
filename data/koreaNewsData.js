1d1e183639e822fdff3e403903f6da3d13e43e3e
LiveCoronaBot <commit-bot@taeuk.me>
2020-07-31 07:31:33 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -13,28 +13,28 @@ var koreaNewsData = [
         "link": "http://www.hani.co.kr/arti/society/health/955977.html"
     },
     {
-        "title": "홍천캠핑장 다녀온 김포시 일가족 3명 코로나19 확진",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200731500097&wlog_tag3=naver"
-    },
-    {
-        "title": "[단독]경기도, '한미연합훈련 취소' 건의…\"코로나19 극복ㆍ한반도 평화 실현...",
-        "link": "http://www.kyeonggi.com/news/articleView.html?idxno=2308221"
-    },
-    {
-        "title": "도쿄 코로나 신규 확진자 463명…연일 최다 기록",
-        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005909720&plink=ORI&cooper=NAVER"
+        "title": "코로나19 신규확진자 36명…지역발생 다시 두자릿수",
+        "link": "http://www.ichannela.com/news/main/news_detailPage.do?publishId=000000213729"
     },
     {
         "title": "코로나 확진 36명…지역 14명·해외유입 22명(종합)",
         "link": "https://biz.chosun.com/site/data/html_dir/2020/07/31/2020073101303.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
-        "title": "감비아서 입국한 인천 송도 거주 50대 코로나19 확진(종합)",
-        "link": "http://yna.kr/AKR20200731075551065?did=1195m"
-    },
-    {
         "title": "[단독] 확진자 '밀접 접촉자' 빠르고 정확하게 찾는다",
         "link": "https://www.ytn.co.kr/_ln/0106_202007310437568320"
+    },
+    {
+        "title": "홍천캠핑장 다녀온 김포시 일가족 3명 코로나19 확진",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200731500097&wlog_tag3=naver"
+    },
+    {
+        "title": "도쿄 코로나 신규 확진자 463명…연일 최다 기록",
+        "link": "http://yna.kr/AKR20200731098800073?did=1195m"
+    },
+    {
+        "title": "경기도 \"평택 코로나 확진자 70%가 주한미군...한미훈련 취소해달라\" 건의",
+        "link": "https://www.pressian.com/pages/articles/2020073115592756125?utm_source=naver&utm_medium=search"
     },
     {
         "title": "[전문] 강동구청 49번째 확진자 발생...천호3동",
