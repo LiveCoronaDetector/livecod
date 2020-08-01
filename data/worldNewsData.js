b266f56e20236ad463f2c5c2963093c5ae1b685d
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-01 13:31:35 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -29,15 +29,15 @@ var worldNewsData = [
         "link": "https://www.cnbc.com/2020/07/31/world-health-organization-reports-largest-ever-single-day-increase-in-coronavirus-cases-across-globe.html"
     },
     {
+        "title": "Coronavirus: Mexico's death toll becomes world's third highest",
+        "link": "https://www.bbc.com/news/world-latin-america-53618808"
+    },
+    {
         "title": "Vietnam records first Covid-19 deaths",
         "link": "https://www.bbc.com/news/world-asia-53606917"
     },
     {
         "title": "A second wave of coronavirus? Scientists say the world is still deep in the first.",
         "link": "https://www.washingtonpost.com/world/2020/07/31/second-wave-coronavirus-scientists-say-world-is-still-deep-first/"
-    },
-    {
-        "title": "Salvini immunity: Former Italian Prime Minister loses legal immunity over migration policy",
-        "link": "https://www.youtube.com/watch?v=cz_JHGA7ezo"
     }
 ];
