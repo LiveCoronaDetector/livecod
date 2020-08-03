6abe38aecc0163537f175c24542b9c2f43a67e25
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-03 22:31:24 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -13,8 +13,20 @@ var koreaNewsData = [
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008031018001&code=940100"
     },
     {
+        "title": "LA 총영사관 긴급 폐쇄…직원 1명, 코로나19 확진자와 접촉",
+        "link": "https://news.joins.com/article/olink/23434904"
+    },
+    {
         "title": "코로나19 신규 확진 23명...해외 유입 지속",
         "link": "https://www.ytn.co.kr/_ln/0103_202008031053240914"
+    },
+    {
+        "title": "WHO \"코로나19 특효약 없을 수도\"",
+        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005913874&plink=ORI&cooper=NAVER"
+    },
+    {
+        "title": "WHO \"코로나19 특효약 없을 수도 있다\"",
+        "link": "https://www.nocutnews.co.kr/news/5389065"
     },
     {
         "title": "일본 코로나 신규 확진자 감염경로 불명 비율 50% 넘어",
@@ -27,17 +39,5 @@ var koreaNewsData = [
     {
         "title": "코로나19 신규 확진자 사흘째 30명대…해외유입이 22명",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4508147&ref=A"
-    },
-    {
-        "title": "WHO \"코로나19 특효약 없을 수도 있다\"",
-        "link": "https://www.nocutnews.co.kr/news/5389065"
-    },
-    {
-        "title": "“홍천 캠핑장 확진자, 할리스커피 갔었다”… 연결고리 확인",
-        "link": "http://news.kmib.co.kr/article/view.asp?arcid=0014872134&code=61121111&cp=nv"
-    },
-    {
-        "title": "코로나19 신규 확진자 23명 中 지역발생 3명 87일만에 최저",
-        "link": "http://www.dongascience.com/news/view/38671"
     }
 ];
