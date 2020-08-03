5cdc08201aa5b8c5cc3f9814194fb7510db307e7
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-03 04:31:33 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,6 +25,10 @@ var worldNewsData = [
         "link": "https://www.foxnews.com/world/australia-declares-state-of-disaster-in-victoria-coronavirus-cases-spike"
     },
     {
+        "title": "Coronavirus live news: Global cases pass 18m as Melbourne braces for further lockdown measures",
+        "link": "https://www.theguardian.com/world/live/2020/aug/03/coronavirus-live-news-global-cases-near-18m-as-melbourne-braces-for-further-lockdown-measures"
+    },
+    {
         "title": "Live Coronavirus Updates: Tracking Global News, Cases and Deaths",
         "link": "https://www.nytimes.com/2020/08/02/world/coronavirus-covid-19.html"
     },
@@ -33,11 +37,7 @@ var worldNewsData = [
         "link": "https://www.thejakartapost.com/news/2020/08/03/iran-reports-highest-virus-cases-for-nearly-a-month.html"
     },
     {
-        "title": "A coronavirus vaccine won’t change the world right away",
-        "link": "https://www.washingtonpost.com/health/2020/08/02/covid-vaccine/"
-    },
-    {
-        "title": "US entering 'different' phase of coronavirus outbreak",
-        "link": "https://www.bbc.com/news/world-us-canada-53632649"
+        "title": "August 2 coronavirus news",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-02-20-intl/index.html"
     }
 ];
