f0eed0c8c4a2313e90244cc715a7579669e3ba71
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-03 05:01:51 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,8 +5,8 @@ var worldNewsData = [
         "link": "https://www.nbcnews.com/news/world/australian-state-declares-coronavirus-state-disaster-south-africa-tops-500-n1235603"
     },
     {
-        "title": "Coronavirus update: Birx says U.S. has entered a ‘new phase’ of pandemic as cases, deaths rise",
-        "link": "https://www.washingtonpost.com/nation/2020/08/02/coronavirus-covid-updates/"
+        "title": "Live updates: U.S. enters ‘new phase’ as coronavirus outbreaks become ‘extraordinarily widespread,’ says Birx",
+        "link": "https://www.washingtonpost.com/nation/2020/08/03/coronavirus-covid-live-updates-us/"
     },
     {
         "title": "Coronavirus: Iran cover-up of deaths revealed by data leak",
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.foxnews.com/world/australia-declares-state-of-disaster-in-victoria-coronavirus-cases-spike"
     },
     {
-        "title": "Coronavirus live news: Global cases pass 18m as Melbourne braces for further lockdown measures",
-        "link": "https://www.theguardian.com/world/live/2020/aug/03/coronavirus-live-news-global-cases-near-18m-as-melbourne-braces-for-further-lockdown-measures"
-    },
-    {
-        "title": "Live Coronavirus Updates: Tracking Global News, Cases and Deaths",
-        "link": "https://www.nytimes.com/2020/08/02/world/coronavirus-covid-19.html"
-    },
-    {
         "title": "Iran reports highest virus cases for nearly a month - The Jakarta Post",
         "link": "https://www.thejakartapost.com/news/2020/08/03/iran-reports-highest-virus-cases-for-nearly-a-month.html"
     },
     {
-        "title": "August 2 coronavirus news",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-02-20-intl/index.html"
+        "title": "Coronavirus live news: Global cases pass 18m as Melbourne braces for further lockdown measures",
+        "link": "https://www.theguardian.com/world/live/2020/aug/03/coronavirus-live-news-global-cases-near-18m-as-melbourne-braces-for-further-lockdown-measures"
+    },
+    {
+        "title": "A coronavirus vaccine won’t change the world right away",
+        "link": "https://www.washingtonpost.com/health/2020/08/02/covid-vaccine/"
+    },
+    {
+        "title": "US entering 'different' phase of coronavirus outbreak",
+        "link": "https://www.bbc.com/news/world-us-canada-53632649"
     }
 ];
