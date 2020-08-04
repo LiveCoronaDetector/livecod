9b7f1a90767d019642e2bb160522645a0098b88d
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-04 23:01:44 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -13,10 +13,6 @@ var koreaNewsData = [
         "link": "https://www.seoul.co.kr/news/newsView.php?id=20200805009012&wlog_tag3=naver"
     },
     {
-        "title": "트럼프 \"한국 코로나19 통계 날조 모를 일\"",
-        "link": "https://imnews.imbc.com/replay/2020/nwtoday/article/5864448_32531.html"
-    },
-    {
         "title": "프랑스, 코로나19 중환자 넉달만에 증가…\"가을 제2 확산 우려\"(종합)",
         "link": "http://yna.kr/AKR20200804168451085?did=1195m"
     },
@@ -30,7 +26,7 @@ var koreaNewsData = [
     },
     {
         "title": "코로나 확진자 수능 병원서 치른다…자가격리자는 별도 시험장",
-        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005914500&plink=ORI&cooper=NAVER"
+        "link": "https://imnews.imbc.com/news/2020/society/article/5863838_32633.html"
     },
     {
         "title": "코로나 신규 확진자 34명...나흘만에 다시 30명대",
@@ -39,5 +35,9 @@ var koreaNewsData = [
     {
         "title": "청주 확진자 6명 이슬람 종교행사 참석…지역감염 비상",
         "link": "http://yna.kr/AKR20200804169400064?did=1195m"
+    },
+    {
+        "title": "서울 코로나19 확진자 9명 늘어, 사망자는 12명",
+        "link": "http://www.fnnews.com/news/202008041051528424"
     }
 ];
