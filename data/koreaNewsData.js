61ed4dfc5ae13c1df2c6ae1965a46a171e319baa
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-04 04:01:43 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,24 +5,16 @@ var koreaNewsData = [
         "link": "http://www.fnnews.com/news/202008041014497879"
     },
     {
-        "title": "코로나 확진자 수능 병원서 치른다…자가격리자는 별도 시험장",
-        "link": "http://yna.kr/AKR20200804056300530?did=1195m"
-    },
-    {
-        "title": "LA 총영사관 긴급 폐쇄…직원 1명 코로나 확진자와 접촉",
-        "link": "https://cnbc.sbs.co.kr/article_hub/10000991776?division=NAVER"
-    },
-    {
-        "title": "자가격리·확진자도 수능 본다…병원·별도시험장서 응시(종합)",
-        "link": "http://www.newsis.com/view/?id=NISX20200804_0001117855&cID=10201&pID=10200"
-    },
-    {
         "title": "[속보] 코로나19 신규 확진자 34명···지역 발생 13명",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008041021001&code=940601"
     },
     {
-        "title": "[속보] 코로나19 어제 34명 신규 확진…지역 발생 13명·해외유입 21명",
-        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005914395&plink=ORI&cooper=NAVER"
+        "title": "LA 총영사관 긴급 폐쇄…직원 1명, 코로나19 확진자와 접촉",
+        "link": "http://yna.kr/AKR20200804009600075?did=1195m"
+    },
+    {
+        "title": "코로나 확진자 수능, 병원 등 치료시설서 치른다",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20200804500059&wlog_tag3=naver"
     },
     {
         "title": "WHO \"코로나19 특효약 없을 수도 있다\"",
@@ -33,11 +25,19 @@ var koreaNewsData = [
         "link": "https://imnews.imbc.com/news/2020/society/article/5863809_32633.html"
     },
     {
-        "title": "청주서 외국인 3명 코로나19 확진…충북 76명으로 늘어(종합)",
-        "link": "http://yna.kr/AKR20200804021651064?did=1195m"
+        "title": "WHO \"코로나, 특효약 없을 수도 있다\"",
+        "link": "https://news.chosun.com/site/data/html_dir/2020/08/03/2020080303531.html?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
-        "title": "세인트루이스 확진자 13명으로 늘어…김광현은 건강",
-        "link": "http://news.tvchosun.com/site/data/html_dir/2020/08/04/2020080490057.html"
+        "title": "자가격리·확진자도 수능 본다…병원·별도시험장서 응시(종합)",
+        "link": "http://www.newsis.com/view/?id=NISX20200804_0001117855&cID=10201&pID=10200"
+    },
+    {
+        "title": "코로나19 어제 34명 신규 확진…국내 발생 13명",
+        "link": "http://www.ichannela.com/news/main/news_detailPage.do?publishId=000000214160"
+    },
+    {
+        "title": "청주서 외국인 3명 코로나19 확진…충북 76명으로 늘어(종합)",
+        "link": "http://yna.kr/AKR20200804021651064?did=1195m"
     }
 ];
