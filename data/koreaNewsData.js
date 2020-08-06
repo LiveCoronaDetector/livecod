c8917aaf02e9d32b3b892575b39abd64395dbc5e
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-06 06:02:04 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,24 +5,12 @@ var koreaNewsData = [
         "link": "http://www.fnnews.com/news/202008061021112417"
     },
     {
-        "title": "[속보] 코로나19 신규 확진자 43명…지역발생 23명",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008061013001&code=940601"
-    },
-    {
         "title": "코로나 신규 확진자 43명…8일만에 40명대로",
         "link": "http://www.hani.co.kr/arti/society/health/956688.html"
     },
     {
-        "title": "코로나19 신규 확진 43명...국내발생 23명으로 더 많아",
-        "link": "https://www.ytn.co.kr/_ln/0103_202008061219507560"
-    },
-    {
-        "title": "코로나19 지역감염 증가…신규확진자 8일만에 40명대[종합]",
-        "link": "https://www.hankyung.com/society/article/2020080690597"
-    },
-    {
-        "title": "‘코로나19’ 신규 확진 33명…국내 발생 15명·해외 유입 18명",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4510097&ref=A"
+        "title": "[속보] 코로나19 신규 확진자 43명…지역발생 23명",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008061013001&code=940601"
     },
     {
         "title": "日 코로나19 신규 확진 1351명…아이치현 긴급사태 선언",
@@ -33,8 +21,20 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200806075200060?did=1195m"
     },
     {
-        "title": "[2보] 고양서 7명 코로나19 확진…환자 가족·접촉자",
-        "link": "http://yna.kr/AKR20200806068800060?did=1195m"
+        "title": "코로나19 어제 43명 신규 확진…해외유입 20명·지역발생 23명",
+        "link": "http://news.tvchosun.com/site/data/html_dir/2020/08/06/2020080690041.html"
+    },
+    {
+        "title": "일본 '고투 트래블' 이후 코로나 확진 2.4배로 늘어",
+        "link": "https://imnews.imbc.com/news/2020/world/article/5865753_32640.html"
+    },
+    {
+        "title": "코로나19 신규 확진 43명...국내발생 23명으로 더 많아",
+        "link": "https://www.ytn.co.kr/_ln/0103_202008061219507560"
+    },
+    {
+        "title": "‘코로나19’ 신규 확진 33명…국내 발생 15명·해외 유입 18명",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4510097&ref=A"
     },
     {
         "title": "코로나19에도 영국인들을 웃게 만든 것",
