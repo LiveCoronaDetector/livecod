fd6082f247c8063fe0d2027ebdc6d92e5151861b
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-06 15:31:08 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,35 +9,35 @@ var koreaNewsData = [
         "link": "https://view.asiae.co.kr/article/2020080620023885129"
     },
     {
-        "title": "[속보] 코로나19 신규 확진자 43명…지역발생 23명",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008061013001&code=940601"
-    },
-    {
         "title": "일본 코로나 신규 확진 1천500명 육박…연일 폭증세(종합)",
         "link": "http://yna.kr/AKR20200806159751073?did=1195m"
-    },
-    {
-        "title": "[종합] 코로나19 확진자 43명 증가...지역 23명 중 경기 11명",
-        "link": "http://www.fnnews.com/news/202008061021112417"
-    },
-    {
-        "title": "코로나19 ‘홍천 캠핑-강남 커피점-V빌딩’ 연관성은?",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4511199&ref=A"
     },
     {
         "title": "'동선 복잡하고 감염경로 깜깜이' 부산 170번 확진자가 심상찮다",
         "link": "http://www.busan.com/view/busan/view.php?code=2020080619222285817"
     },
     {
-        "title": "창궐 더 심해졌다…전세계 코로나19 확진 1천900만명(종합)",
-        "link": "http://yna.kr/AKR20200806159951009?did=1195m"
+        "title": "독일 코로나19 하루 신규 확진자, 3개월만에 1천명 넘어서(종합)",
+        "link": "http://yna.kr/AKR20200806158251082?did=1195m"
+    },
+    {
+        "title": "[속보] 코로나19 신규 확진자 43명…지역발생 23명",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008061013001&code=940601"
+    },
+    {
+        "title": "[종합] 코로나19 확진자 43명 증가...지역 23명 중 경기 11명",
+        "link": "http://www.fnnews.com/news/202008061021112417"
     },
     {
         "title": "일본 '고투 트래블' 이후 코로나 확진 2.4배로 늘어",
         "link": "https://imnews.imbc.com/news/2020/world/article/5865753_32640.html"
     },
     {
-        "title": "코로나19 어제 43명 신규 확진…해외유입 20명·지역발생 23명",
-        "link": "http://news.tvchosun.com/site/data/html_dir/2020/08/06/2020080690041.html"
+        "title": "코로나19 ‘홍천 캠핑-강남 커피점-V빌딩’ 연관성은?",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4511199&ref=A"
+    },
+    {
+        "title": "창궐 더 심해졌다…전세계 코로나19 확진 1천900만명(종합)",
+        "link": "http://yna.kr/AKR20200806159951009?did=1195m"
     }
 ];
