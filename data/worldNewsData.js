196c9319653b310171ac834d0874b038b83a9b41
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-09 10:30:58 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,20 +5,20 @@ var worldNewsData = [
         "link": "https://en.as.com/en/2020/08/09/latest_news/1596949669_897073.html"
     },
     {
-        "title": "The Coronavirus Could Be Controlled by October With the Right Steps",
-        "link": "https://www.nytimes.com/2020/08/08/opinion/sunday/coronavirus-response-testing-lockdown.html"
+        "title": "Coronavirus Live Updates: Latest News and Analysis",
+        "link": "https://www.nytimes.com/2020/08/08/world/coronavirus-updates.html"
     },
     {
         "title": "US downgrades COVID-19 risk for 20 countries, Israel not included",
         "link": "https://www.jpost.com/international/us-downgrades-covid-19-risk-for-20-countries-israel-not-included-637968"
     },
     {
-        "title": "Fingers crossed at France's brasseries and cafes as tourist quarantines loom",
-        "link": "https://www.theguardian.com/world/2020/aug/08/french-tourist-chiefs-on-knife-edge-as-country-may-be-next-on-uk-quarantine-list"
+        "title": "Europe's biggest countries are seeing Covid surges -- but not this one",
+        "link": "https://www.cnn.com/2020/08/09/europe/italy-coronavirus-return-normal-intl/index.html"
     },
     {
-        "title": "Deadly fire at coronavirus facility in south India",
-        "link": "https://www.aljazeera.com/news/2020/08/deadly-fire-coronavirus-facility-south-india-200809064454139.html"
+        "title": "India's Coronavirus Cases Top 2 Million : Coronavirus Live Updates",
+        "link": "https://www.npr.org/sections/coronavirus-live-updates/2020/08/07/900112972/indias-coronavirus-cases-top-2-million-more-than-62-000-reported-in-one-day"
     },
     {
         "title": "Japan's coronavirus fatigue is fueling defiance in Tokyo, even as the case count rises",
@@ -33,11 +33,11 @@ var worldNewsData = [
         "link": "https://www.cnet.com/news/bill-gates-says-coronavirus-will-end-for-the-rich-world-by-end-of-2021/"
     },
     {
-        "title": "Coronavirus: Johnson urged to help out travellers stuck in quarantine",
-        "link": "https://www.theguardian.com/world/2020/aug/08/boris-johnson-compensate-travellers-coronavirus-quarantine"
-    },
-    {
         "title": "A look at the United States' unique failure in responding to coronavirus",
         "link": "https://www.startribune.com/a-look-at-the-united-states-unique-failure-in-responding-to-coronavirus/572053602/"
+    },
+    {
+        "title": "Covid-19: only half of Britons would definitely have vaccination",
+        "link": "https://www.theguardian.com/world/2020/aug/09/only-half-of-britons-would-definitely-have-covid-19-vaccination"
     }
 ];
