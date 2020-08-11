b36767229a03daf536300c02069649417fb7e64d
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-11 08:31:31 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,10 +5,6 @@ var koreaNewsData = [
         "link": "https://imnews.imbc.com/news/2020/society/article/5870267_32633.html"
     },
     {
-        "title": "코로나19 신규 확진자 34명...부산 9명, 서울·경기 13명",
-        "link": "http://www.hani.co.kr/arti/society/health/957301.html"
-    },
-    {
         "title": "코로나19 신규 확진자 34명…국내 발생 23명·해외 유입 11명",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4514320&ref=A"
     },
@@ -17,12 +13,16 @@ var koreaNewsData = [
         "link": "http://www.munhwa.com/news/view.html?no=20200811MW164644893652"
     },
     {
-        "title": "잠실야구장에 코로나 확진자 방문…\"소독 후 경기 정상 진행\"",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/08/11/2020081102634.html?utm_source=naver&utm_medium=original&utm_campaign=news"
+        "title": "잠실구장 코로나 확진자 방문…역학조사 결과 '경기해도 괜찮아'",
+        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005925926&plink=ORI&cooper=NAVER"
     },
     {
         "title": "남대문시장 중앙상가에서도 확진자…1명 추가 감염(종합)",
         "link": "http://yna.kr/AKR20200811069951004?did=1195m"
+    },
+    {
+        "title": "코로나19 신규 확진자 34명...부산 9명, 서울·경기 13명",
+        "link": "http://www.hani.co.kr/arti/society/health/957301.html"
     },
     {
         "title": "부산서 또 코로나19 무더기 확진…러 어선 선원 4명+확진자 접촉자 5명",
@@ -33,11 +33,11 @@ var koreaNewsData = [
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008101014001&code=940601"
     },
     {
-        "title": "양천구, 73번 동거인 74번째 코로나19 확진자 발생",
-        "link": "https://view.asiae.co.kr/article/2020081113583380044"
+        "title": "[속보] 코로나 신규 확진 34명... 부산서 9명 대거 확진",
+        "link": "https://hankookilbo.com/News/Read/A2020081109420002791?did=NA"
     },
     {
-        "title": "해외유입 확진자 중 '변이 바이러스' 3건 확인",
-        "link": "http://health.chosun.com/site/data/html_dir/2020/08/11/2020081101134.html"
+        "title": "수도권 교회 발 n차 감염 확산...남대문시장 중앙상가도 확진자",
+        "link": "https://www.ytn.co.kr/_ln/0103_202008111645527312"
     }
 ];
