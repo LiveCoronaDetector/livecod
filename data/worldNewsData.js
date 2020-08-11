74e4e9ea76c1086ca45a6cf88c0583ef8f720f4b
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-11 01:31:17 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,12 +5,12 @@ var worldNewsData = [
         "link": "https://en.as.com/en/2020/08/10/latest_news/1597036584_738849.html"
     },
     {
-        "title": "Live updates: Trump dismisses coronavirus cases in children, says they’re a ‘tiny fraction’ of deaths",
-        "link": "https://www.washingtonpost.com/nation/2020/08/10/coronavirus-covid-live-updates-us/"
+        "title": "Canadians are vandalising cars with US plates in anger over border crossings to escape coronavirus",
+        "link": "https://www.independent.co.uk/news/world/americas/coronavirus-us-canada-border-cars-covid-19-a9663516.html"
     },
     {
         "title": "Coronavirus in Europe: France extends mask use as Greece says it is in second wave",
-        "link": "https://www.theguardian.com/world/2020/aug/10/coronavirus-in-europe-france-extends-mask-use-as-greece-says-it-is-in-second-wave"
+        "link": "https://news.google.com/articles/CAIiEHuxTFu1iAD0CLCTzazrg8wqFwgEKg4IACoGCAowl6p7MN-zCTDZ4PsF?hl=en-US&gl=US&ceid=US%3Aen"
     },
     {
         "title": "Fears coronavirus travel ban imminent as Greece's cases hit pandemic high",
@@ -29,15 +29,15 @@ var worldNewsData = [
         "link": "https://www.cnet.com/news/bill-gates-says-coronavirus-pandemic-will-be-over-for-the-rich-world-by-end-of-2021/"
     },
     {
+        "title": "World hits grim milestone of 20 million reported coronavirus cases",
+        "link": "https://www.cnn.com/2020/08/10/health/20-million-coronavirus-cases-intl/index.html"
+    },
+    {
         "title": "Coronavirus live news: Global cases top 20m as WHO chief says ‘it’s never too late to turn outbreak around'",
         "link": "https://www.theguardian.com/world/live/2020/aug/11/coronavirus-live-news-who-chief-says-its-never-too-late-to-turn-outbreak-around-as-cases-near-20m"
     },
     {
         "title": "Mexico’s Coronavirus Struggle Worsened by Fear of Hospitals",
         "link": "https://www.nytimes.com/2020/08/10/world/americas/mexico-coronavirus-hospitals.html"
-    },
-    {
-        "title": "Greece records daily high of coronavirus cases as Brits flock there for summer holidays",
-        "link": "https://www.mirror.co.uk/news/world-news/greece-records-daily-high-coronavirus-22500055"
     }
 ];
