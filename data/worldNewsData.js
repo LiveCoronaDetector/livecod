61bf2b7a7ee6d61158686d10bcbef268610fbdec
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-11 20:01:37 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -29,15 +29,15 @@ var worldNewsData = [
         "link": "https://www.nytimes.com/2020/08/11/world/coronavirus-covid-19.html"
     },
     {
+        "title": "The latest on the coronavirus pandemic: Live updates",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-11-20-intl/index.html"
+    },
+    {
+        "title": "Global report: coronavirus cases pass 20m as WHO points to 'green shoots of hope'",
+        "link": "https://www.theguardian.com/world/2020/aug/11/global-report-coronavirus-cases-pass-20m-as-who-points-to-green-shoots-of-hope"
+    },
+    {
         "title": "World hits grim milestone of 20 million reported coronavirus cases",
         "link": "https://www.cnn.com/2020/08/10/health/20-million-coronavirus-cases-intl/index.html"
-    },
-    {
-        "title": "Coronavirus: Belgium beach brawl prompts call to limit visitors",
-        "link": "https://www.theguardian.com/world/2020/aug/11/coronavirus-belgium-beach-brawl-call-limit-visitors"
-    },
-    {
-        "title": "Bill Gates: Coronavirus pandemic will end 'for the rich world' by late 2021",
-        "link": "https://www.cnet.com/news/bill-gates-says-coronavirus-pandemic-will-be-over-for-rich-world-by-late-2021/"
     }
 ];
