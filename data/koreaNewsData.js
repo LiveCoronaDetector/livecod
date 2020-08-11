305366eb9df3978ed3ae7b9c8ee6a1c58c893c3d
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-11 20:31:30 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,6 +5,10 @@ var koreaNewsData = [
         "link": "https://imnews.imbc.com/news/2020/society/article/5870267_32633.html"
     },
     {
+        "title": "\"코로나를 이렇게 막다니\" 강릉해수욕장 주목한 BBC",
+        "link": "https://news.chosun.com/site/data/html_dir/2020/08/11/2020081103405.html?utm_source=naver&utm_medium=original&utm_campaign=news"
+    },
+    {
         "title": "남대문시장 중앙상가에서도 확진자…1명 추가 감염(종합)",
         "link": "http://yna.kr/AKR20200811069951004?did=1195m"
     },
@@ -21,23 +25,19 @@ var koreaNewsData = [
         "link": "http://www.busan.com/view/busan/view.php?code=2020081109480925046"
     },
     {
+        "title": "잠실구장에 코로나19 확진자 입장 확인",
+        "link": "http://www.munhwa.com/news/view.html?no=20200811MW164644893652"
+    },
+    {
+        "title": "남대문시장 추가 확진자 안 나와…영업 재개",
+        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=03817926625866336"
+    },
+    {
         "title": "[속보] 코로나19 신규 확진자 28명···지역발생 17명",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008101014001&code=940601"
     },
     {
         "title": "교회발 감염 확산…남대문시장 다른 상가서도 확진자",
         "link": "http://news.jtbc.joins.com/article/article.aspx?news_id=NB11964193"
-    },
-    {
-        "title": "\"코로나를 이렇게 막다니\" 강릉해수욕장 주목한 BBC",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/08/11/2020081103405.html?utm_source=naver&utm_medium=original&utm_campaign=news"
-    },
-    {
-        "title": "잠실구장에 코로나19 확진자 입장 확인",
-        "link": "http://www.munhwa.com/news/view.html?no=20200811MW164644893652"
-    },
-    {
-        "title": "'확진자 방문' 잠실구장, 방역 완료… \"경기 진행 이상없어\"",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/08/11/2020081103321.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     }
 ];
