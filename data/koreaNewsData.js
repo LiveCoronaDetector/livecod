5fd0b97303910f01b7b592694af7576296d1ae10
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-11 23:01:27 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,16 +5,20 @@ var koreaNewsData = [
         "link": "https://imnews.imbc.com/news/2020/society/article/5870267_32633.html"
     },
     {
-        "title": "남대문시장 중앙상가에서도 확진자…1명 추가 감염(종합)",
-        "link": "http://yna.kr/AKR20200811069951004?did=1195m"
-    },
-    {
         "title": "코로나19 신규 확진자 34명…국내 발생 23명·해외 유입 11명",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4514320&ref=A"
     },
     {
         "title": "코로나19 신규 확진자 34명...부산 9명, 서울·경기 13명",
         "link": "http://www.hani.co.kr/arti/society/health/957301.html"
+    },
+    {
+        "title": "남대문시장 중앙상가에서도 확진자…1명 추가 감염(종합)",
+        "link": "http://yna.kr/AKR20200811069951004?did=1195m"
+    },
+    {
+        "title": "남대문시장 추가 확진자 안 나와…영업 재개",
+        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=03817926625866336"
     },
     {
         "title": "부산서 또 코로나19 무더기 확진…러 어선 선원 4명+확진자 접촉자 5명",
@@ -35,9 +39,5 @@ var koreaNewsData = [
     {
         "title": "\"코로나를 이렇게 막다니\" 강릉해수욕장 주목한 BBC",
         "link": "https://news.chosun.com/site/data/html_dir/2020/08/11/2020081103405.html?utm_source=naver&utm_medium=original&utm_campaign=news"
-    },
-    {
-        "title": "'확진자 방문' 잠실구장, 방역 완료… \"경기 진행 이상없어\"",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/08/11/2020081103321.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     }
 ];
