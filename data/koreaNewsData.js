de6fda467d7c25e14bdf1e85017b80d71619f608
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-11 13:02:02 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,22 +5,6 @@ var koreaNewsData = [
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4514320&ref=A"
     },
     {
-        "title": "잠실구장에 코로나19 확진자 입장 확인",
-        "link": "http://www.munhwa.com/news/view.html?no=20200811MW164644893652"
-    },
-    {
-        "title": "BBC \"한국 해수욕장이 코로나19에도 안전한 이유 3가지\"",
-        "link": "http://yna.kr/AKR20200811115700009?did=1195m"
-    },
-    {
-        "title": "수도권 교회 발 n차 감염 확산...남대문시장 중앙상가도 확진자",
-        "link": "https://www.ytn.co.kr/_ln/0103_202008111645527312"
-    },
-    {
-        "title": "부산부경보건고 성인반 확진자의 가족 4명 추가확진... 관련 확진자 9명 확인",
-        "link": "http://www.busan.com/view/busan/view.php?code=2020081117485898839"
-    },
-    {
         "title": "남대문시장 중앙상가에서도 확진자…1명 추가 감염(종합)",
         "link": "http://yna.kr/AKR20200811069951004?did=1195m"
     },
@@ -33,11 +17,27 @@ var koreaNewsData = [
         "link": "http://www.hani.co.kr/arti/society/health/957301.html"
     },
     {
-        "title": "영진호 선원과 확진자 접촉 감염 무더기 발생...9명 확진 판정",
-        "link": "https://www.pressian.com/pages/articles/2020081110203168759?utm_source=naver&utm_medium=search"
-    },
-    {
         "title": "남대문시장 추가 확진자 안 나와…영업 재개",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=03817926625866336"
+    },
+    {
+        "title": "부산서 또 코로나19 무더기 확진…러 어선 선원 4명+확진자 접촉자 5명",
+        "link": "http://www.busan.com/view/busan/view.php?code=2020081109480925046"
+    },
+    {
+        "title": "[속보] 코로나19 신규 확진자 28명···지역발생 17명",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008101014001&code=940601"
+    },
+    {
+        "title": "잠실야구장에 코로나 확진자 방문…\"소독 후 경기 정상 진행\"",
+        "link": "https://news.chosun.com/site/data/html_dir/2020/08/11/2020081102634.html?utm_source=naver&utm_medium=original&utm_campaign=news"
+    },
+    {
+        "title": "잠실구장에 코로나19 확진자 입장 확인",
+        "link": "http://www.munhwa.com/news/view.html?no=20200811MW164644893652"
+    },
+    {
+        "title": "부산 174번 확진자 다니던 고교 성인반 5명 추가 확진",
+        "link": "https://view.asiae.co.kr/article/2020081114381921606"
     }
 ];
