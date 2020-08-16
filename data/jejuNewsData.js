ad7b1015754dbbd671bff50e62bcd1ca075e6ae5
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-16 10:02:01 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -9,10 +9,6 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200816018600065?did=1195m"
     },
     {
-        "title": "제주여행 김포 코로나 확진자, 유명 관광지 둘러봤다",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/08/16/2020081601448.html?utm_source=naver&utm_medium=original&utm_campaign=news"
-    },
-    {
         "title": "김포시 코로나 확진자 일행 제주도 여행 동선 공개",
         "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=424974"
     },
@@ -21,23 +17,27 @@ var jejuNewsData = [
         "link": "https://www.pressian.com/pages/articles/2020081611473425510?utm_source=naver&utm_medium=search"
     },
     {
-        "title": "김포시 코로나19 확진자 확진 전 제주 여행",
-        "link": "http://www.jejunews.com/news/articleView.html?idxno=2169836"
-    },
-    {
         "title": "\"잠잠했는데...\" 충북서 코로나 확진자 3명 발생",
         "link": "https://news.chosun.com/site/data/html_dir/2020/08/16/2020081600337.html?utm_source=naver&utm_medium=original&utm_campaign=news"
-    },
-    {
-        "title": "코로나19 확진 김포시 거주 교인…확진 전 제주도 여행",
-        "link": "http://www.fnnews.com/news/202008161131385165"
     },
     {
         "title": "충북서 코로나19 확진자 3명 나와",
         "link": "http://www.hani.co.kr/arti/area/yeongnam/957954.html"
     },
     {
+        "title": "김포시 코로나19 확진자 확진 전 제주 여행",
+        "link": "http://www.jejunews.com/news/articleView.html?idxno=2169836"
+    },
+    {
         "title": "충북 코로나19 하루 3명 확진…지역 확산 비상",
         "link": "https://www.nocutnews.co.kr/news/5395641"
+    },
+    {
+        "title": "사랑제일교회 107명 확진자 ‘빙산의 일각’…3차 대유행 현실화하나",
+        "link": "http://www.segye.com/content/html/2020/08/16/20200816512133.html?OutUrl=naver"
+    },
+    {
+        "title": "제주 코로나 25번 확진자 퇴원...3명 남아",
+        "link": "http://www.jejusori.net/news/articleView.html?idxno=318989"
     }
 ];
