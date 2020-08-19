1ea7efb0b61224eb7a56bf23c8ae1feabdbdd25e
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-19 16:01:48 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -9,7 +9,7 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/08/19/politics/cnn-poll-us-coronavirus-response-embarrassed/index.html"
     },
     {
-        "title": "Iran coronavirus death toll passes 20,000: Live updates",
+        "title": "Spain sets new post-lockdown coronavirus cases record: Live",
         "link": "https://www.aljazeera.com/news/2020/08/worldwide-coronavirus-cases-22-million-live-updates-200818232532477.html"
     },
     {
@@ -25,10 +25,6 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/08/19/australia/australia-astrazeneca-covid-19-vaccine-intl-hnk-scli-scn/index.html"
     },
     {
-        "title": "Covid-19 News: Live Updates",
-        "link": "https://www.nytimes.com/2020/08/19/world/covid-19-coronavirus.html"
-    },
-    {
         "title": "The latest on the coronavirus pandemic: Live updates",
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-19-20-intl/index.html"
     },
@@ -39,5 +35,9 @@ var worldNewsData = [
     {
         "title": "WHO warns young people are emerging as main spreaders of the coronavirus",
         "link": "https://www.washingtonpost.com/health/who-warns-young-people-are-emerging-as-main-spreaders-of-the-coronavirus/2020/08/18/1822ee92-e18f-11ea-b69b-64f7b0477ed4_story.html"
+    },
+    {
+        "title": "Merkel Warns over Virus Spike",
+        "link": "https://www.youtube.com/watch?v=dJlZz2L3eDM"
     }
 ];
