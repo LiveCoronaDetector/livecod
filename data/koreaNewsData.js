7aaabe2577c1f67472ae7b208339541d07e31143
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-20 03:01:35 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,35 +9,35 @@ var koreaNewsData = [
         "link": "http://news.tf.co.kr/read/economy/1807908.htm"
     },
     {
-        "title": "[종합] 코로나 288명 신규확진…지역발생 276명·1주일간 1576명",
-        "link": "https://www.hankyung.com/life/article/2020082063117"
+        "title": "코로나19 신규확진 288명, 일주일새 누적 확진 1576명(종합)",
+        "link": "http://www.newstomato.com/ReadNews.aspx?no=991159"
     },
     {
-        "title": "전광훈 \"정부가 무한대로 검사 강요해 확진자수 늘려\"",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/08/20/2020082000912.html?utm_source=naver&utm_medium=original&utm_campaign=news"
+        "title": "[종합] 코로나 288명 신규확진…지역발생 276명·1주일간 1576명",
+        "link": "https://www.hankyung.com/life/article/2020082063117"
     },
     {
         "title": "[종합] 코로나19 확진자 288명 증가...지역 276명 중 서울 135명, 경기 81명",
         "link": "http://www.fnnews.com/news/202008201028118398"
     },
     {
+        "title": "[속보]사랑제일교회 · 광화문집회 집단감염 코로나19 대유행 도화선 되나",
+        "link": "http://www.kwnews.co.kr/nview.asp?aid=220081900139"
+    },
+    {
         "title": "수도권만 226명…일주일간 확진자 1500명 넘어(종합)",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=02335366625869288"
     },
     {
+        "title": "철원·화성 육군 신병교육대서 입영장정 2명 코로나19 확진(종합)",
+        "link": "http://yna.kr/AKR20200820050651504?did=1195m"
+    },
+    {
+        "title": "[속보]성북구 체대입시학원 확진자 19명···11개 고교 학생",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008201150001&code=940100"
+    },
+    {
         "title": "[속보]사랑제일교회 관련 확진자 630명...양성률 19.3%",
         "link": "https://www.sedaily.com/NewsView/1Z6NI3MUZ5"
-    },
-    {
-        "title": "인천서 해외입국자 등 6명 코로나19 추가 확진(종합)",
-        "link": "http://yna.kr/AKR20200820022551065?did=1195m"
-    },
-    {
-        "title": "[속보]광화문 집회 참가 김진태 전 의원, 코로나19 '음성'",
-        "link": "http://www.kwnews.co.kr/nview.asp?aid=220081900143"
-    },
-    {
-        "title": "코로나 어제 288명 신규확진…지역발생 276명",
-        "link": "http://www.ichannela.com/news/main/news_detailPage.do?publishId=000000216287"
     }
 ];
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -13,31 +13,31 @@ var worldNewsData = [
         "link": "https://www.aljazeera.com/news/2020/08/worldwide-coronavirus-cases-22-million-live-updates-200818232532477.html"
     },
     {
-        "title": "Spain, France And Germany Report Most Coronavirus Cases Since Their Lockdowns",
-        "link": "https://www.npr.org/sections/coronavirus-live-updates/2020/08/19/903967293/spain-france-and-germany-report-most-coronavirus-cases-since-their-lockdowns"
+        "title": "Coronavirus live news: France, Italy, Spain hit record post-lockdown cases",
+        "link": "https://www.theguardian.com/world/live/2020/aug/20/coronavirus-live-news-france-italy-spain-hit-record-post-lockdown-cases"
     },
     {
         "title": "Where Pseudoscience Is Spreading",
         "link": "https://www.theatlantic.com/international/archive/2020/08/amitabh-bachchan-india-coronavirus/615310/"
     },
     {
-        "title": "Local Officials in China Hid Coronavirus Dangers From Beijing, U.S. Agencies Find",
-        "link": "https://www.nytimes.com/2020/08/19/world/asia/china-coronavirus-beijing-trump.html"
+        "title": "Australia secures deal for potential Covid-19 vaccine, will provide it free to all citizens",
+        "link": "https://www.cnn.com/2020/08/19/australia/australia-astrazeneca-covid-19-vaccine-intl-hnk-scli-scn/index.html"
     },
     {
-        "title": "Live Covid-19 Updates",
-        "link": "https://www.nytimes.com/2020/08/19/world/covid-19-coronavirus.html"
-    },
-    {
-        "title": "Coronavirus live news: France, Italy, Spain hit record post-lockdown cases",
-        "link": "https://www.theguardian.com/world/live/2020/aug/20/coronavirus-live-news-france-italy-spain-hit-record-post-lockdown-cases"
+        "title": "Spain, France And Germany Report Most Coronavirus Cases Since Their Lockdowns",
+        "link": "https://www.npr.org/sections/coronavirus-live-updates/2020/08/19/903967293/spain-france-and-germany-report-most-coronavirus-cases-since-their-lockdowns"
     },
     {
         "title": "WHO warns young people are emerging as main spreaders of the coronavirus",
         "link": "https://www.washingtonpost.com/health/who-warns-young-people-are-emerging-as-main-spreaders-of-the-coronavirus/2020/08/18/1822ee92-e18f-11ea-b69b-64f7b0477ed4_story.html"
     },
     {
-        "title": "Wuhan coronavirus: From silent streets to packed pools",
-        "link": "https://www.bbc.com/news/world-asia-china-53816511"
+        "title": "Italy at a crossroads as fears grow of Covid-19 second wave",
+        "link": "https://www.theguardian.com/world/2020/aug/19/italy-at-a-crossroads-as-fears-grow-of-covid-19-second-wave"
+    },
+    {
+        "title": "Covid-19: Live Updates",
+        "link": "https://www.nytimes.com/2020/08/18/world/coronavirus-covid.html"
     }
 ];
