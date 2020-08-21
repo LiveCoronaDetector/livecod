bbd90b2c711f638899dadfeb38b508f46ff80f51
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-21 16:31:29 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,10 +5,6 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200821040300004?did=1195m"
     },
     {
-        "title": "'광화문 집회' 투입 경찰관, 추가 확진자 확인…총 5명(종합)",
-        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=03375126625869616"
-    },
-    {
         "title": "[속보] 코로나19 신규확진자 324명... 국내발생 315명",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008211017011&code=940100"
     },
@@ -17,16 +13,12 @@ var koreaNewsData = [
         "link": "https://www.ytn.co.kr/_ln/0103_202008211144052960"
     },
     {
-        "title": "[종합] 일 평균 확진자 수 246명…거리두기 3단계 격상 두고 정부 고심",
-        "link": "https://view.asiae.co.kr/article/2020082122261362682"
+        "title": "시총 1·2위도 코로나에 뚫렸다…대기업 방역망 '초비상'",
+        "link": "https://www.hankyung.com/economy/article/202008212091i"
     },
     {
         "title": "[단독]제주 중문골프장 캐디 1명 코로나19 확진 판정",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=01918806625869616"
-    },
-    {
-        "title": "시총 1·2위도 코로나에 뚫렸다…대기업 방역망 '초비상'",
-        "link": "https://www.hankyung.com/economy/article/202008212091i"
     },
     {
         "title": "사랑제일교회 관련 확진자 어제까지 739명…12개 시·도서 나와",
@@ -39,5 +31,13 @@ var koreaNewsData = [
     {
         "title": "경찰청서도 확진자 발생…경찰관 잇따른 격리에 치안 우려",
         "link": "http://www.hani.co.kr/arti/society/society_general/958759.html"
+    },
+    {
+        "title": "광복절 집회 투입 경찰 코로나 검사 완료…확진 5명(종합)",
+        "link": "https://www.nocutnews.co.kr/news/5398880"
+    },
+    {
+        "title": "[종합] 일 평균 확진자 수 246명…거리두기 3단계 격상 두고 정부 고심",
+        "link": "https://view.asiae.co.kr/article/2020082122261362682"
     }
 ];
