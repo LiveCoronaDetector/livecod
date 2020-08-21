30c0b4276e4dd9a8ef461cd3f170bcc8a71e4017
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-21 03:31:17 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,24 +5,28 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-africa-53847699"
     },
     {
-        "title": "Coronavirus live news: US regulator vows to resign if vaccine rushed; Ardern hits back at Trump",
+        "title": "Coronavirus live news: rightwing rally linked to South Korea surge; Ardern hits back at Trump",
         "link": "https://www.theguardian.com/world/live/2020/aug/21/coronavirus-live-news-latin-america-deaths-near-250000-russia-to-start-mass-vaccine-trials"
     },
     {
-        "title": "Century-old vaccine limits COVID spread, so bring it back, urge Israeli experts",
-        "link": "https://www.timesofisrael.com/century-old-vaccine-limits-covid-spread-so-bring-it-back-say-israeli-experts/"
+        "title": "Spain sets new post-lockdown coronavirus cases record: Live",
+        "link": "https://www.aljazeera.com/news/2020/08/worldwide-coronavirus-cases-22-million-live-updates-200818232532477.html"
     },
     {
         "title": "Germany, Spain record highest daily coronavirus infection rate since April as cases surge across Europe",
         "link": "https://www.cnbc.com/2020/08/20/coronavirus-germany-spain-record-highest-daily-cases-since-april.html"
     },
     {
-        "title": "Endless first wave: how Indonesia failed to control coronavirus",
-        "link": "https://www.reuters.com/article/us-health-coronavirus-indonesia-insight/endless-first-wave-how-indonesia-failed-to-control-coronavirus-idUSKCN25G02J"
+        "title": "Tampa Bay districts hope to hire school nurses before the start of the school year",
+        "link": "https://www.youtube.com/watch?v=-4jcB2VWhDA"
     },
     {
         "title": "Controversy over Australian PM's vaccine comments points to the next coronavirus nightmare",
         "link": "https://www.cnn.com/2020/08/20/australia/australia-morrison-coronavirus-vaccine-intl-hnk/index.html"
+    },
+    {
+        "title": "School Nurses Are on the Coronavirus Front Lines. But Many Schools Don’t Have One.",
+        "link": "https://www.nytimes.com/2020/08/20/us/schools-reopening-nurses-covid.html"
     },
     {
         "title": "China defends Wuhan's massive pool party",
@@ -35,9 +39,5 @@ var worldNewsData = [
     {
         "title": "European countries see highest daily cases in months",
         "link": "https://uk.news.yahoo.com/france-germany-italy-and-spain-see-highest-daily-coronavirus-cases-since-lifting-lockdown-increasing-second-wave-fears-162315200.html"
-    },
-    {
-        "title": "At least 15 states reporting Covid cases at colleges and universities",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-20-20-intl/h_1a78846f0fc3b30b40da31eeed7d565d"
     }
 ];
