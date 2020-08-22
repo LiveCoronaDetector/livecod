27562bdd0c5f26f01c5beb609e2e9a4b819136f3
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-22 16:01:54 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,7 +5,7 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-africa-53847699"
     },
     {
-        "title": "At least 800,000 coronavirus deaths recorded worldwide: Live news",
+        "title": "Global coronavirus death toll hits 800,000: Live news",
         "link": "https://www.aljazeera.com/news/2020/08/200822013536155.html"
     },
     {
@@ -14,30 +14,30 @@ var worldNewsData = [
     },
     {
         "title": "Thousands crowd into an indoor concert in experiment on how to return to normality",
-        "link": "https://www.cnn.com/travel/article/germany-coronavirus-tim-bendzko-concert-wellness-scn-grm-intl/index.html"
+        "link": "https://www.cnn.com/travel/amp/germany-coronavirus-tim-bendzko-concert-wellness-scn-grm-intl/"
     },
     {
-        "title": "Fresh Coronavirus cases reported in 41 schools in Germany's capital after reopening",
-        "link": "https://www.youtube.com/watch?v=sjMcrSa_DwQ"
+        "title": "Covid-19 cases tied to the Sturgis motorcycle rally in South Dakota have reached across state lines",
+        "link": "https://www.cnn.com/2020/08/21/us/sturgis-motorcyle-rally-sd-covid-nebraska-trnd/index.html"
     },
     {
         "title": "German Cases Jump as Europe Surge is Less Deadly: Virus Update",
-        "link": "https://www.bloomberg.com/tosv2.html?vid=&uuid=80a65440-e48c-11ea-838f-7d8f24ec5755&url=L25ld3MvYXJ0aWNsZXMvMjAyMC0wOC0yMS91LXMtY2FzZXMtYXJlLXN0ZWFkeS13aGlsZS1ldXJvcGUtYmF0dGxlcy1zdXJnZS12aXJ1cy11cGRhdGU="
-    },
-    {
-        "title": "The latest on the coronavirus pandemic",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-22-20-intl/index.html"
+        "link": "https://www.bloomberg.com/tosv2.html?vid=&uuid=ca199840-e490-11ea-86b0-290f0a310ecd&url=L25ld3MvYXJ0aWNsZXMvMjAyMC0wOC0yMS91LXMtY2FzZXMtYXJlLXN0ZWFkeS13aGlsZS1ldXJvcGUtYmF0dGxlcy1zdXJnZS12aXJ1cy11cGRhdGU="
     },
     {
         "title": "Coronavirus pandemic could be over within two years - WHO head",
         "link": "https://www.bbc.com/news/world-53870798"
     },
     {
-        "title": "C.D.C. Suggests Some Child-Care Centers Can Reopen Safely",
-        "link": "https://www.nytimes.com/2020/08/21/world/covid-coronavirus.html"
+        "title": "The latest on the coronavirus pandemic",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-22-20-intl/index.html"
     },
     {
-        "title": "UK's cheap food could fuel Covid-19 spread, says WHO envoy",
-        "link": "https://www.theguardian.com/world/2020/aug/21/uk-cheap-food-could-fuel-covid-19-spread-says-who-envoy"
+        "title": "Coronavirus Updates: COVID Cases Emerge After Massive Sturgis, South Dakota Motorcycle Rally",
+        "link": "https://gothamist.com/news/coronavirus-updates-august-22-2020"
+    },
+    {
+        "title": "Global report: WHO says world could rein in pandemic in less than two years",
+        "link": "https://www.theguardian.com/world/2020/aug/22/global-report-who-says-world-could-rein-in-pandemic-in-less-than-two-years"
     }
 ];
