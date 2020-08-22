f54bb138f954b20a0e990945ad800cef8e7ff7fb
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-22 23:31:21 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,20 +9,28 @@ var koreaNewsData = [
         "link": "https://cnbc.sbs.co.kr/article_hub/10000993744?division=NAVER"
     },
     {
-        "title": "충남 아산 삼성디스플레이 공사장에 코로나19 확진자 발생",
-        "link": "http://yna.kr/AKR20200822026700003?did=1195m"
+        "title": "‘킹키부츠’ 출연 배우, 코로나19 확진자와 밀접 접촉…23일 공연 취소(전문...",
+        "link": "http://mksports.co.kr/view/2020/865262/"
     },
     {
         "title": "쿠팡 일산 배송센터, 코로나19 확진자 방문으로 폐쇄",
         "link": "https://imnews.imbc.com/news/2020/econo/article/5882705_32647.html"
     },
     {
-        "title": "코로나19 확진자 전국서 속출…사랑제일교회 관련 누적 확진자 796명",
-        "link": "http://www.hani.co.kr/arti/society/society_general/958897.html"
+        "title": "코로나19 확진자 전국서 속출…사랑제일교회 누적 796명",
+        "link": "http://yna.kr/AKR20200822041200530?did=1195m"
     },
     {
         "title": "코로나19 신규 확진자 332명…이틀 연속 300명대",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008221012001&code=940601"
+    },
+    {
+        "title": "전남서 코로나19 확진자 10명 추가 발생...누적확진자 70명",
+        "link": "http://www.fnnews.com/news/202008221228495499"
+    },
+    {
+        "title": "충남 아산 삼성디스플레이 사업장서 코로나19 확진자 나와",
+        "link": "https://cnbc.sbs.co.kr/article_hub/10000993748?division=NAVER"
     },
     {
         "title": "전세계 코로나19 사망 80만명 넘어… 두달반만에 2배",
@@ -31,13 +39,5 @@ var koreaNewsData = [
     {
         "title": "외교부 청사에도 코로나19 확진자 발생…22일 자로 2명",
         "link": "https://www.news1.kr/articles/?4034293"
-    },
-    {
-        "title": "WHO 사무총장 \"코로나19 2년 내 종식 희망\"",
-        "link": "http://yna.kr/AKR20200822002700109?did=1195m"
-    },
-    {
-        "title": "전남서 코로나19 확진자 10명 추가 발생...누적확진자 70명",
-        "link": "http://www.fnnews.com/news/202008221228495499"
     }
 ];
