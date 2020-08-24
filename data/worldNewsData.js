6de2466bee50070da32a2f6352566d7ba8182c19
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-24 07:30:58 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -37,7 +37,7 @@ var worldNewsData = [
         "link": "https://www.theverge.com/2020/8/23/21397929/who-health-kids-masks-coronavirus"
     },
     {
-        "title": "Covid-19 Updates: White House Claims a 'Therapeutic Breakthrough' Is Coming",
-        "link": "https://www.nytimes.com/2020/08/23/world/covid-19-coronavirus.html"
+        "title": "Why do Covid fatalities remain low when infection numbers are rising?",
+        "link": "https://www.theguardian.com/world/2020/aug/23/why-do-covid-fatalities-remain-low-when-infection-numbers-are-rising"
     }
 ];
