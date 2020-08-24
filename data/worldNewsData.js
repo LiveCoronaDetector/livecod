31b5adfcc51b02ea17faf24a35ebf10b7b6c25be
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-24 06:01:58 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.scmp.com/news/china/society/article/3098505/coronavirus-china-has-been-using-covid-19-vaccine-candidate-key"
     },
     {
-        "title": "The latest on the coronavirus pandemic: Live updates",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-24-20-intl/index.html"
-    },
-    {
-        "title": "World Health Organization advises kids 12 and older should wear masks to prevent coronavirus spread",
-        "link": "https://www.theverge.com/2020/8/23/21397929/who-health-kids-masks-coronavirus"
+        "title": "Coronavirus: Trump authorizes plasma treatment amid attacks on FDA",
+        "link": "https://www.theguardian.com/world/2020/aug/23/coronavirus-donald-trump-plasma-treatment-fda-deep-state-navarro"
     },
     {
         "title": "Ten countries kept out Covid. But did they win?",
         "link": "https://www.bbc.com/news/world-asia-53831063"
     },
     {
-        "title": "Covid-19 Updates: White House Claims a 'Therapeutic Breakthrough' Is Coming",
-        "link": "https://www.nytimes.com/2020/08/23/world/covid-19-coronavirus.html"
+        "title": "World Health Organization advises kids 12 and older should wear masks to prevent coronavirus spread",
+        "link": "https://www.theverge.com/2020/8/23/21397929/who-health-kids-masks-coronavirus"
+    },
+    {
+        "title": "Why do Covid fatalities remain low when infection numbers are rising?",
+        "link": "https://www.theguardian.com/world/2020/aug/23/why-do-covid-fatalities-remain-low-when-infection-numbers-are-rising"
     }
 ];
