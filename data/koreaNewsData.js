3d75f23f694088dad0555dbd5d10a68c5c4e4a8d
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-24 15:01:30 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,20 +5,12 @@ var koreaNewsData = [
         "link": "https://www.nocutnews.co.kr/news/5399501"
     },
     {
-        "title": "[전문]문대통령 “코로나, 신천지때보다 엄중…불법행위 좌시 않겠다”",
-        "link": "http://news.heraldcorp.com/view.php?ud=20200824000803"
-    },
-    {
         "title": "서울 코로나 누계 오늘 3천명 돌파…오후 6시까지 79명 추가",
         "link": "http://yna.kr/AKR20200824156700004?did=1195m"
     },
     {
-        "title": "[속보] 충북 거주 80대 노인, 코로나 확진 이튿날 사망",
-        "link": "https://www.hankyung.com/life/article/2020082410507"
-    },
-    {
-        "title": "확진자 나온 러시아 선박서 도주 선원 6시간 만에 잡혀",
-        "link": "http://www.busan.com/view/busan/view.php?code=2020082419174811675"
+        "title": "文대통령 \"지금 코로나 못막으면 3단계 불가피...최대 위기\"",
+        "link": "http://www.joseilbo.com/news/news_read.php?uid=404634&class=32&grp="
     },
     {
         "title": "쿠팡 잠실본사 폐쇄…\"재택근무 직원 코로나19 확진\"",
@@ -33,8 +25,16 @@ var koreaNewsData = [
         "link": "http://www.fnnews.com/news/202008241406526308"
     },
     {
-        "title": "쿠팡 인천4 물류센터 내 추가 확진자 0명",
-        "link": "http://isplus.live.joins.com/news/article/article.asp?total_id=23855189"
+        "title": "쿠팡 확진자 나온 인천4물류센터 재가동…\"추가 감염 없다\"",
+        "link": "https://www.hankyung.com/life/article/202008248606g"
+    },
+    {
+        "title": "경남, 밤새 코로나19 확진자 1명 추가…총 196명(종합)",
+        "link": "http://www.newsis.com/view/?id=NISX20200824_0001139629&cID=10812&pID=10800"
+    },
+    {
+        "title": "LG전자 인니 공장서 200명 코로나19 감염…\"30일까지 폐쇄\"",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/08/24/2020082403697.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
         "title": "종로구 SK사옥 첫 확진자 발생…건물 전체 폐쇄",
