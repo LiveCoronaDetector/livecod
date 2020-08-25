356ad99bc08a744b17e5cade512af01972497893
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-25 05:31:21 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,20 +5,20 @@ var jejuNewsData = [
         "link": "https://www.pressian.com/pages/articles/2020082504492651139?utm_source=naver&utm_medium=search"
     },
     {
-        "title": "제주 29번째 확진자 발생…\"용인 개척교회 다녀온 목사\"(종합)",
-        "link": "http://yna.kr/AKR20200824163951056?did=1195m"
-    },
-    {
         "title": "잠잠하던 제주서 밤새 코로나19 확진자 5명 발생",
         "link": "http://www.hani.co.kr/arti/area/jeju/959200.html"
     },
     {
-        "title": "제주도내 29번째 코로나19 확진자 발생",
-        "link": "http://www.mediajeju.com/news/articleView.html?idxno=325430"
-    },
-    {
         "title": "제주도, 코로나19 확진자 5명 동선 공개…수도권 방문 4명",
         "link": "http://yna.kr/AKR20200825089000056?did=1195m"
+    },
+    {
+        "title": "제주 29번째 확진자 발생…\"용인 개척교회 다녀온 목사\"(종합)",
+        "link": "http://yna.kr/AKR20200824163951056?did=1195m"
+    },
+    {
+        "title": "제주도내 29번째 코로나19 확진자 발생",
+        "link": "http://www.mediajeju.com/news/articleView.html?idxno=325430"
     },
     {
         "title": "하룻밤새 코로나19 확진자 4명 잇따라 발생…방역 비상",
@@ -33,11 +33,11 @@ var jejuNewsData = [
         "link": "https://imnews.imbc.com/replay/2020/nwtoday/article/5885464_32531.html"
     },
     {
-        "title": "제주지역 코로나19 확진자 5명 1차 역학조사 공개",
-        "link": "https://www.nocutnews.co.kr/news/5400436"
+        "title": "[코로나19]제주, 하룻밤 새 코로나19 확진자 5명 발생...제주 방역당국 '멘붕...",
+        "link": "http://news.bbsi.co.kr/news/articleView.html?idxno=3001854"
     },
     {
-        "title": "JDC, 직원 코로나19 확진 특별방역…재택근무 전환",
-        "link": "http://www.newsis.com/view/?id=NISX20200825_0001141298&cID=10813&pID=10800"
+        "title": "제주지역 코로나19 확진자 5명 1차 역학조사 공개",
+        "link": "https://www.nocutnews.co.kr/news/5400436"
     }
 ];
