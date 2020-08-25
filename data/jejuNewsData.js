0ca032d56105d91264de9385175c0ec66aa4d79c
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-25 10:01:30 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,28 +5,32 @@ var jejuNewsData = [
         "link": "https://www.pressian.com/pages/articles/2020082504492651139?utm_source=naver&utm_medium=search"
     },
     {
-        "title": "제주도, 코로나19 확진자 5명 동선 공개…수도권 방문 4명",
-        "link": "http://yna.kr/AKR20200825089000056?did=1195m"
-    },
-    {
         "title": "제주도 하룻밤새 코로나19 확진자 5명 발생",
         "link": "https://www.pressian.com/pages/articles/2020082509434271947?utm_source=naver&utm_medium=search"
     },
     {
-        "title": "잠잠하던 제주서 밤새 코로나19 확진자 5명 발생",
-        "link": "http://www.hani.co.kr/arti/area/jeju/959200.html"
+        "title": "제주도, 코로나19 확진자 5명 동선 공개…수도권 방문 4명",
+        "link": "http://yna.kr/AKR20200825089000056?did=1195m"
     },
     {
         "title": "제주도내 29번째 코로나19 확진자 발생",
         "link": "http://www.mediajeju.com/news/articleView.html?idxno=325430"
     },
     {
-        "title": "제주서 코로나19 확진 잇따라…24~25일 5명 발생",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202008250829001&code=620117"
+        "title": "수도권 코로나 재확산 영향 잠잠하던 제주도 확진자 급증",
+        "link": "http://yna.kr/AKR20200825042800056?did=1195m"
+    },
+    {
+        "title": "제주도 34번째 코로나19 확진자 발생...'JDC 직장 동료'",
+        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=425675"
     },
     {
         "title": "JDC, 직원 코로나19 확진 특별방역…재택근무 전환",
         "link": "http://www.newsis.com/view/?id=NISX20200825_0001141298&cID=10813&pID=10800"
+    },
+    {
+        "title": "제주에서 확진자 하루 만에 4명 추가 발생…\"동선 파악 중\"",
+        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005945964&plink=ORI&cooper=NAVER"
     },
     {
         "title": "원희룡 지사, 코로나19 확진자 동선 신속히 공개 지시",
@@ -35,9 +39,5 @@ var jejuNewsData = [
     {
         "title": "제주지역 코로나19 확진자 5명 1차 역학조사 공개",
         "link": "https://www.nocutnews.co.kr/news/5400436"
-    },
-    {
-        "title": "제주 코로나 31번 확진자는 중등교사...\"학생 접촉 없어\"",
-        "link": "http://www.jejusori.net/news/articleView.html?idxno=319309"
     }
 ];
