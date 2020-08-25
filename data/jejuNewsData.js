4213c475698e3669ef4979ec8b58853a0d17400e
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-25 03:01:45 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -13,31 +13,31 @@ var jejuNewsData = [
         "link": "http://www.hani.co.kr/arti/area/jeju/959200.html"
     },
     {
+        "title": "제주도 하룻밤새 코로나19 확진자 5명 발생",
+        "link": "https://www.pressian.com/pages/articles/2020082509434271947?utm_source=naver&utm_medium=search"
+    },
+    {
         "title": "제주도내 29번째 코로나19 확진자 발생",
         "link": "http://www.mediajeju.com/news/articleView.html?idxno=325430"
     },
     {
-        "title": "제주도 하룻밤새 코로나19 확진자 5명 발생",
-        "link": "https://www.pressian.com/pages/articles/2020082509434271947?utm_source=naver&utm_medium=search"
+        "title": "제주도, 코로나19 30번.31번 확진자 1차 역학조사 결과 공개",
+        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=425625"
     },
     {
         "title": "수도권 코로나 재확산 영향 잠잠하던 제주도 확진자 급증",
         "link": "http://yna.kr/AKR20200825042800056?did=1195m"
     },
     {
+        "title": "JDC 코로나19 확진자 발생…필수인원외 재택근무",
+        "link": "https://www.news1.kr/articles/?4036491"
+    },
+    {
         "title": "서울 확진자 3천 명 넘어…'집단감염' 전국 확산세",
         "link": "https://imnews.imbc.com/replay/2020/nwtoday/article/5885464_32531.html"
     },
     {
-        "title": "JDC, 직원 코로나19 확진 특별방역…재택근무 전환",
-        "link": "http://www.newsis.com/view/?id=NISX20200825_0001141298&cID=10813&pID=10800"
-    },
-    {
         "title": "[2보] \"제주도 29번 확진자, 용인시 교회 방문했다가 감염 추정\"",
         "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=425573"
-    },
-    {
-        "title": "제주도 '4명' 연이어 코로나19 확진...차단방역 초비상",
-        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=425578"
     }
 ];
