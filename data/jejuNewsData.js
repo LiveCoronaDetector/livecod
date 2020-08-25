8e8882f77ac673b605d27a742e182b780345885d
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-25 05:01:29 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -13,16 +13,16 @@ var jejuNewsData = [
         "link": "http://www.hani.co.kr/arti/area/jeju/959200.html"
     },
     {
-        "title": "제주도 하룻밤새 코로나19 확진자 5명 발생",
-        "link": "https://www.pressian.com/pages/articles/2020082509434271947?utm_source=naver&utm_medium=search"
+        "title": "제주도내 29번째 코로나19 확진자 발생",
+        "link": "http://www.mediajeju.com/news/articleView.html?idxno=325430"
     },
     {
         "title": "제주도, 코로나19 확진자 5명 동선 공개…수도권 방문 4명",
         "link": "http://yna.kr/AKR20200825089000056?did=1195m"
     },
     {
-        "title": "제주도내 29번째 코로나19 확진자 발생",
-        "link": "http://www.mediajeju.com/news/articleView.html?idxno=325430"
+        "title": "하룻밤새 코로나19 확진자 4명 잇따라 발생…방역 비상",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4524561&ref=A"
     },
     {
         "title": "수도권 코로나 재확산 영향 잠잠하던 제주도 확진자 급증",
@@ -33,11 +33,11 @@ var jejuNewsData = [
         "link": "https://imnews.imbc.com/replay/2020/nwtoday/article/5885464_32531.html"
     },
     {
-        "title": "JDC, 직원 코로나19 확진 특별방역…재택근무 전환",
-        "link": "http://www.newsis.com/view/?id=NISX20200825_0001141298&cID=10813&pID=10800"
-    },
-    {
         "title": "제주지역 코로나19 확진자 5명 1차 역학조사 공개",
         "link": "https://www.nocutnews.co.kr/news/5400436"
+    },
+    {
+        "title": "JDC, 직원 코로나19 확진 특별방역…재택근무 전환",
+        "link": "http://www.newsis.com/view/?id=NISX20200825_0001141298&cID=10813&pID=10800"
     }
 ];
