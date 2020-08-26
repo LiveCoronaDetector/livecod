b08cfcf908cd7771f6ee56011e47e0dc61ded625
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-26 09:30:58 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -13,24 +13,24 @@ var jejuNewsData = [
         "link": "https://www.pressian.com/pages/articles/2020082504492651139?utm_source=naver&utm_medium=search"
     },
     {
-        "title": "제주 코로나19 35번째 확진자 나와…제주 34번 확진자와 접촉자",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4525613&ref=A"
-    },
-    {
         "title": "제주 코로나19 34번째 확진자 접촉자 17명…동선 9곳 공개",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4525777&ref=A"
+    },
+    {
+        "title": "제주 코로나19 35번째 확진자 나와…제주 34번 확진자와 접촉자",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4525613&ref=A"
     },
     {
         "title": "제주도 35번째 코로나 확진자 나와, JDC 직원 접촉자",
         "link": "http://www.newsjeju.net/news/articleView.html?idxno=349616"
     },
     {
-        "title": "제주도내 29번째 코로나19 확진자 발생",
-        "link": "http://www.mediajeju.com/news/articleView.html?idxno=325430"
-    },
-    {
         "title": "잠잠하던 제주서 밤새 코로나19 확진자 5명 발생",
         "link": "http://www.hani.co.kr/arti/area/jeju/959200.html"
+    },
+    {
+        "title": "제주도내 29번째 코로나19 확진자 발생",
+        "link": "http://www.mediajeju.com/news/articleView.html?idxno=325430"
     },
     {
         "title": "3일간 7명 확진…제주 코로나 확산에 방역당국 '초긴장'",
