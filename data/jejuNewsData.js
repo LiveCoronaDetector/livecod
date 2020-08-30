0012e3d09211d1e3e42f7ec181bb0f53d5d292df
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-30 11:31:01 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -9,24 +9,28 @@ var jejuNewsData = [
         "link": "http://news.kmib.co.kr/article/view.asp?arcid=0014959342&code=61122027&cp=nv"
     },
     {
-        "title": "제주 게하발 코로나19 확진자 속출…거짓진술에 방역망 흔들",
-        "link": "http://www.hani.co.kr/arti/area/jeju/959871.html"
+        "title": "제주도, 다음달부터 코로나 재난문자 영문으로 제공",
+        "link": "https://news.chosun.com/site/data/html_dir/2020/08/30/2020083000719.html?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
         "title": "제주, 확진자 도청·시청도 방문…직원들 '패닉'(종합)",
         "link": "http://www.newsis.com/view/?id=NISX20200830_0001147715&cID=10813&pID=10800"
     },
     {
-        "title": "제주도 코로나19 확진 2명 추가...확진자 다녀간 온천 방문",
-        "link": "http://www.getnews.co.kr/view.php?ud=2020083017004661866a6b374224_16"
-    },
-    {
-        "title": "제주도, 다음달부터 코로나 재난문자 영문으로 제공",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/08/30/2020083000719.html?utm_source=naver&utm_medium=original&utm_campaign=news"
+        "title": "제주도 코로나19 확진자 2명 추가 발생",
+        "link": "http://news.kmib.co.kr/article/view.asp?arcid=0014959689&code=61121111&cp=nv"
     },
     {
         "title": "코로나19 확진자 방문한 제주도청·시청 부서 일시 폐쇄",
         "link": "https://www.nocutnews.co.kr/news/5403555"
+    },
+    {
+        "title": "[종합] 제주도청-시청 긴급 '폐쇄' 조치...확진자 방문 '발칵' ",
+        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=426055"
+    },
+    {
+        "title": "제주 코로나 확진자 2명 추가... 누적 44명",
+        "link": "https://hankookilbo.com/News/Read/A2020083017080003200?did=NA"
     },
     {
         "title": "제주 코로나 39번째 확진자, 32명과 접촉",
@@ -35,9 +39,5 @@ var jejuNewsData = [
     {
         "title": "제주도 신규확진 4명 늘어…3명은 온천·숙박업소 관련",
         "link": "http://news.jtbc.joins.com/article/article.aspx?news_id=NB11967001"
-    },
-    {
-        "title": "[종합] 제주도청-시청 긴급 '폐쇄' 조치...확진자 방문 '발칵' ",
-        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=426055"
     }
 ];
