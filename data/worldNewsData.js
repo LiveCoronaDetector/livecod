905925e8971a1ff7ac2bec7080921858507acc3e
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-30 19:31:37 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,12 +5,12 @@ var worldNewsData = [
         "link": "https://en.as.com/en/2020/08/30/latest_news/1598769034_793758.html"
     },
     {
-        "title": "Global coronavirus cases exceed 25 million: Live news",
-        "link": "https://www.aljazeera.com/news/2020/08/brazil-coronavirus-deaths-surpass-120000-live-updates-200829232301925.html"
+        "title": "U.S. coronavirus cases approach 6 million as Midwest, schools face outbreaks",
+        "link": "https://www.reuters.com/article/us-health-coronavirus-usa/u-s-coronavirus-cases-approach-6-million-as-midwest-schools-face-outbreaks-idUSKBN25Q0KS"
     },
     {
-        "title": "Afghanistan, Pakistan see surge in polio cases amid coronavirus pandemic",
-        "link": "https://www.foxnews.com/world/afghanistan-surge-polio-coronavirus-pandemic"
+        "title": "Turkish mayors accuse government of coronavirus cover-up",
+        "link": "https://www.ft.com/content/b181a997-90c2-4941-848f-77f26c15b0f4"
     },
     {
         "title": "Germany coronavirus: Anger after attempt to storm Reichstag",
@@ -25,16 +25,16 @@ var worldNewsData = [
         "link": "https://www.washingtonpost.com/world/asia_pacific/japan-coronavirus-elderly-death-rate/2020/08/29/f30f3ca8-e2da-11ea-82d8-5e55d47e90ca_story.html"
     },
     {
-        "title": "Right-wing protesters in Berlin try to storm Reichstag | DW News",
-        "link": "https://www.youtube.com/watch?v=S7zK8BuKSp4"
-    },
-    {
         "title": "The latest on the coronavirus pandemic: Live updates",
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-30-20-intl/index.html"
     },
     {
-        "title": "Coronavirus cases surpass 25 million as pandemic strains nations around the world",
-        "link": "https://www.cnbc.com/2020/08/30/global-coronavirus-cases-surpass-25-million-as-pandemic-strains-nations-around-the-world.html"
+        "title": "Right-wing protesters in Berlin try to storm Reichstag | DW News",
+        "link": "https://www.youtube.com/watch?v=S7zK8BuKSp4"
+    },
+    {
+        "title": "Coronavirus Cases Top 25 Million Globally As India Emerges As A New Epicenter",
+        "link": "https://www.npr.org/2020/08/30/907630621/coronavirus-cases-top-25-million-globally-as-india-emerges-as-a-new-epicenter"
     },
     {
         "title": "Covid-19 News: Live Updates",
