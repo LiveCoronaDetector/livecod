5f310db334c93d77e17617915f5f8f7bf2976fc9
LiveCoronaBot <commit-bot@taeuk.me>
2020-08-31 01:31:43 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,27 +17,27 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-europe-53964147"
     },
     {
-        "title": "Coronavirus: India surpasses US for highest single-day rise in Covid-19 cases",
-        "link": "https://www.bbc.com/news/world-asia-india-53969118"
+        "title": "India Records World's Highest Single-Day Rise In Covid-19 Cases",
+        "link": "https://www.youtube.com/watch?v=j8negYILvOw"
     },
     {
         "title": "Japan has the world’s oldest population. Yet it dodged a coronavirus crisis at elder-care facilities.",
         "link": "https://www.washingtonpost.com/world/asia_pacific/japan-coronavirus-elderly-death-rate/2020/08/29/f30f3ca8-e2da-11ea-82d8-5e55d47e90ca_story.html"
     },
     {
-        "title": "'Small events add up to a lot': Limited gatherings quietly emerge as source of coronavirus infections",
-        "link": "https://www.usatoday.com/story/news/nation/2020/08/30/covid-19-small-gatherings-quietly-significant-source-infections/5650677002/"
-    },
-    {
-        "title": "Right-wing protesters in Berlin try to storm Reichstag | DW News",
-        "link": "https://www.youtube.com/watch?v=S7zK8BuKSp4"
+        "title": "India reports nearly 80,000 new coronavirus cases in one day",
+        "link": "https://www.nydailynews.com/coronavirus/ny-coronavirus-india-record-single-day-number-cases-20200830-ateyq236qvdafpyvdpbxou3ky4-story.html"
     },
     {
         "title": "The latest on the coronavirus pandemic: Live updates",
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-08-30-20-intl/index.html"
     },
     {
-        "title": "European Leaders Weigh New Lockdowns as Cases Rise",
-        "link": "https://www.nytimes.com/2020/08/29/world/covid-19-coronavirus.html"
+        "title": "Right-wing protesters in Berlin try to storm Reichstag | DW News",
+        "link": "https://www.youtube.com/watch?v=S7zK8BuKSp4"
+    },
+    {
+        "title": "Coronavirus Cases Top 25 Million Globally As India Emerges As A New Epicenter",
+        "link": "https://www.npr.org/2020/08/30/907630621/coronavirus-cases-top-25-million-globally-as-india-emerges-as-a-new-epicenter"
     }
 ];
