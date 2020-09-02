587e9f6dc9f3d401d17fafb466f791d5a173b0c7
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-02 08:31:29 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,36 +5,36 @@ var koreaNewsData = [
         "link": "http://www.ichannela.com/news/main/news_detailPage.do?publishId=000000217932"
     },
     {
+        "title": "\"확진자 발생해도 연장근무\" 쿠팡 노동자, 검찰 고발",
+        "link": "http://www.fnnews.com/news/202009021544447783"
+    },
+    {
         "title": "정은경 \"코로나19 폭발적 확산 억제…신규확진 100명 이하 목표\"(종합)",
         "link": "http://yna.kr/AKR20200902119551530?did=1195m"
     },
     {
-        "title": "[속보]코로나19 신규 확진자 267명···지역 감염 253명",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202009020932001&code=940601"
+        "title": "대전 80대 코로나19 확진자 숨져…3번째 사망자",
+        "link": "http://www.hani.co.kr/arti/area/chungcheong/960360.html"
     },
     {
         "title": "[속보] 코로나19 신규 확진 267명…위·중증 환자 124명",
         "link": "http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202009020087&t=NN"
     },
     {
-        "title": "울산 ‘고스톱 확진자’ 12명으로 늘어",
-        "link": "http://www.hani.co.kr/arti/area/yeongnam/960363.html"
-    },
-    {
         "title": "[단독]코로나 확진 한화 선수들, 원룸 옥상서 고기굽고 술자리",
         "link": "https://news.joins.com/article/olink/23457361"
     },
     {
+        "title": "[속보]코로나19 신규 확진자 267명···지역 감염 253명",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202009020932001&code=940601"
+    },
+    {
+        "title": "코로나19 누적 확진자 2만명 넘어…추석승차권 예매 연기",
+        "link": "http://news.tvchosun.com/site/data/html_dir/2020/09/02/2020090290003.html"
+    },
+    {
         "title": "코로나 확진 267명…4일 연속 200명대(종합)",
         "link": "https://biz.chosun.com/site/data/html_dir/2020/09/02/2020090200871.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
-    },
-    {
-        "title": "코로나19 확진자 200명대 유지했지만…중환자 급증 이어져(종합)",
-        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=02141846625896184"
-    },
-    {
-        "title": "[종합] 운동시설 등 다양한 장소서 코로나19 확진자 발생",
-        "link": "http://www.fnnews.com/news/202009021444172377"
     },
     {
         "title": "현대아울렛 가산점, 코로나19 확진자 나와 휴점",
