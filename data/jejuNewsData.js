ef604922ebdd25db7dcd56b0eaaf8d1eb48d7015
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-03 11:02:19 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -9,35 +9,35 @@ var jejuNewsData = [
         "link": "http://www.fnnews.com/news/202009020025153557"
     },
     {
+        "title": "제주 헌팅포자 감성주점 등 코로나19 고강도 안전 점검",
+        "link": "https://www.pressian.com/pages/articles/2020090311382630842?utm_source=naver&utm_medium=search"
+    },
+    {
         "title": "확진자 늘어난 제주 코로나19 치료병상 확대",
         "link": "https://www.nocutnews.co.kr/news/5406494"
     },
     {
-        "title": "제주도내 47번째 확진자 자택수리 위해 입도",
-        "link": "https://www.pressian.com/pages/articles/2020090218351861978?utm_source=naver&utm_medium=search"
+        "title": "도내 47번 확진자 접촉자 9명",
+        "link": "http://www.jejunews.com/news/articleView.html?idxno=2171031"
     },
     {
         "title": "수능 응시원서 접수 3일부터…자가격리자·확진자 대리제출 가능",
         "link": "http://yna.kr/AKR20200901046000530?did=1195m"
     },
     {
-        "title": "제주도, '2차 재난긴급 생활지원금' 현장접수",
-        "link": "http://www.newsjeju.net/news/articleView.html?idxno=350032"
-    },
-    {
-        "title": "게스트하우스발 서울·울산 확진자 2명 발생",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=4531461&ref=A"
-    },
-    {
-        "title": "제주 헌팅포자 감성주점 등 코로나19 고강도 안전 점검",
-        "link": "https://www.pressian.com/pages/articles/2020090311382630842?utm_source=naver&utm_medium=search"
-    },
-    {
-        "title": "[속보]제주 47번째 코로나19 확진자 발생…울산 확진자 접촉",
-        "link": "https://www.news1.kr/articles/?4045238"
+        "title": "제주 코로나19 47번째 확진자 나와…역학조사 중",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=4530542&ref=A"
     },
     {
         "title": "강남 아파트 등 집단감염↑...서울 확진자 다시 100명대",
         "link": "http://www.fnnews.com/news/202009021144417201"
+    },
+    {
+        "title": "제주도, '2차 재난긴급 생활지원금' 현장접수",
+        "link": "http://www.newsjeju.net/news/articleView.html?idxno=350032"
+    },
+    {
+        "title": "은평구 175번 확진자도 제주 게하發…'루프탑정원'서 파티",
+        "link": "https://www.news1.kr/articles/?4046082"
     }
 ];
