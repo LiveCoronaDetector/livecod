2ae156c9294417d105219575c8b70a4569beb00a
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-05 15:45:48 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -9,8 +9,8 @@ var worldNewsData = [
         "link": "https://www.cnbc.com/2020/09/04/key-coronavirus-forecast-predicts-over-410000-total-us-deaths-by-jan-1.html"
     },
     {
-        "title": "Brazil records 50,000 cases in 24 hours as death toll passes 125,000 – as it happened",
-        "link": "https://www.theguardian.com/world/live/2020/sep/04/coronavirus-live-news-brazil-cases-pass-4m-israel-to-impose-partial-lockdown"
+        "title": "Ultra-Orthodox Jews clash with secular Israeli officials over coronavirus measures",
+        "link": "https://www.washingtonpost.com/world/middle_east/ultra-orthodox-jews-clash-with-secular-israeli-officials-over-coronavirus-measures/2020/09/04/97bb37be-ee0e-11ea-bd08-1b10132b458f_story.html"
     },
     {
         "title": "Germany's virus response won plaudits. But protests over vaccines and masks show it's a victim of its own success",
@@ -21,23 +21,23 @@ var worldNewsData = [
         "link": "https://www.nytimes.com/2020/09/05/world/asia/india-economy-coronavirus.html"
     },
     {
-        "title": "Coronavirus: Arrests at Australia anti-lockdown protests",
-        "link": "https://news.yahoo.com/coronavirus-arrests-australia-anti-lockdown-111702790.html"
+        "title": "Iraq may 'lose control' after record coronavirus cases: Live",
+        "link": "https://www.aljazeera.com/news/2020/09/7000-health-workers-dead-coronavirus-amnesty-live-200904001933968.html"
     },
     {
         "title": "Controversial Forecast Of COVID-19 Death Toll Worldwide : Goats and Soda",
         "link": "https://www.npr.org/sections/goatsandsoda/2020/09/04/909783162/new-global-coronavirus-death-forecast-is-chilling-and-controversial"
     },
     {
-        "title": "Coronavirus vaccine: Russian COVID-19 vaccine shows to produce antibody response",
-        "link": "https://www.youtube.com/watch?v=UZiIJt1lL9M"
+        "title": "Russia vaccine 'passes early trial test' | Covid-19 | Vaccine | WION | Coronavirus",
+        "link": "https://www.youtube.com/watch?v=FWgE3up-zT4"
     },
     {
         "title": "The U.S. vs. the world: How major nations are recovering from coronavirus",
         "link": "https://www.politico.com/news/2020/09/04/get-ready-for-a-long-recession-408911"
     },
     {
-        "title": "WHO tempers quick vaccine hopes",
-        "link": "https://news.yahoo.com/tempers-quick-vaccine-hopes-162152624.html"
+        "title": "Cook Islands former PM dies of coronavirus in New Zealand",
+        "link": "https://www.theguardian.com/world/2020/sep/05/cook-islands-former-pm-dies-of-coronavirus-in-new-zealand"
     }
 ];
