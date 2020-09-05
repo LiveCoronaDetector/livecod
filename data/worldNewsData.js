bb9b4f3a670ee89bc93b47ddc9ccb48f060ce71e
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-05 15:04:13 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -9,8 +9,8 @@ var worldNewsData = [
         "link": "https://www.cnbc.com/2020/09/04/key-coronavirus-forecast-predicts-over-410000-total-us-deaths-by-jan-1.html"
     },
     {
-        "title": "Israel leads world in new coronavirus infections per capita - report",
-        "link": "https://www.jpost.com/israel-news/israel-reportedly-leading-world-in-new-coronavirus-infections-per-capita-641122"
+        "title": "Brazil records 50,000 cases in 24 hours as death toll passes 125,000 – as it happened",
+        "link": "https://www.theguardian.com/world/live/2020/sep/04/coronavirus-live-news-brazil-cases-pass-4m-israel-to-impose-partial-lockdown"
     },
     {
         "title": "Germany's virus response won plaudits. But protests over vaccines and masks show it's a victim of its own success",
@@ -21,23 +21,23 @@ var worldNewsData = [
         "link": "https://www.nytimes.com/2020/09/05/world/asia/india-economy-coronavirus.html"
     },
     {
-        "title": "Melbourne anti-coronavirus lockdown protest turns violent: Live",
-        "link": "https://www.aljazeera.com/news/2020/09/coronavirus-model-forecasts-19-million-deaths-2020-live-200905003313705.html"
+        "title": "Coronavirus: Arrests at Australia anti-lockdown protests",
+        "link": "https://news.yahoo.com/coronavirus-arrests-australia-anti-lockdown-111702790.html"
     },
     {
         "title": "Controversial Forecast Of COVID-19 Death Toll Worldwide : Goats and Soda",
         "link": "https://www.npr.org/sections/goatsandsoda/2020/09/04/909783162/new-global-coronavirus-death-forecast-is-chilling-and-controversial"
     },
     {
-        "title": "Russia vaccine 'passes early trial test' | Covid-19 | Vaccine | WION | Coronavirus",
-        "link": "https://www.youtube.com/watch?v=FWgE3up-zT4"
+        "title": "Coronavirus vaccine: Russian COVID-19 vaccine shows to produce antibody response",
+        "link": "https://www.youtube.com/watch?v=UZiIJt1lL9M"
     },
     {
-        "title": "The latest on the coronavirus pandemic: Live updates",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-05-20-intl/index.html"
+        "title": "The U.S. vs. the world: How major nations are recovering from coronavirus",
+        "link": "https://www.politico.com/news/2020/09/04/get-ready-for-a-long-recession-408911"
     },
     {
-        "title": "Watch live: The World Health Organization holds a press briefing on the coronavirus pandemic as nations race to develop vaccine",
-        "link": "https://www.cnbc.com/2020/09/04/watch-live-world-health-organization-holds-briefing-on-coronavirus.html"
+        "title": "WHO tempers quick vaccine hopes",
+        "link": "https://news.yahoo.com/tempers-quick-vaccine-hopes-162152624.html"
     }
 ];
