318e976c271a47c82fe93f082e06b0c8877accb9
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-06 18:31:19 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -25,16 +25,16 @@ var koreaNewsData = [
         "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005966919&plink=ORI&cooper=NAVER"
     },
     {
+        "title": "영국 코로나19 일일 신규 확진자 3천명…5월 중순 이후 최다",
+        "link": "http://yna.kr/AKR20200907001000085?did=1195m"
+    },
+    {
         "title": "대구·경북 ‘동충하초’發 확진자 속출…n차 집단감염 우려",
         "link": "https://www.chosun.com/national/regional/yeongnam/2020/09/06/DN5YQZGV45ENBMUSIOSU3UOJPY/?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
         "title": "코로나 신규확진자 나흘 연속 100명대 기록",
         "link": "https://www.pressian.com/pages/articles/2020090610085662692?utm_source=naver&utm_medium=search"
-    },
-    {
-        "title": "영국 코로나19 일일 신규 확진자 3천명…5월 중순 이후 최다",
-        "link": "http://yna.kr/AKR20200907001000085?did=1195m"
     },
     {
         "title": "파주 스타벅스 관련 코로나19 확진자 2명 늘어…누적 70명",
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -13,8 +13,8 @@ var worldNewsData = [
         "link": "https://www.washingtonpost.com/world/middle_east/ultra-orthodox-jews-clash-with-secular-israeli-officials-over-coronavirus-measures/2020/09/04/97bb37be-ee0e-11ea-bd08-1b10132b458f_story.html"
     },
     {
-        "title": "UK records highest daily coronavirus cases since May: Live news",
-        "link": "https://www.aljazeera.com/news/2020/09/mask-rally-italy-coronavirus-deaths-inch-live-news-200905233428336.html"
+        "title": "Former Italian PM Berlusconi remains hospitalized in 'most delicate phase' of coronavirus: personal doctor",
+        "link": "https://www.foxnews.com/world/italy-berlusconi-coronavirus-delicate-phase"
     },
     {
         "title": "Coronavirus Shatters India's Economy",
@@ -29,8 +29,8 @@ var worldNewsData = [
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-06-20-intl/index.html"
     },
     {
-        "title": "Coronavirus live news: nearly 3,000 people in UK test positive in last 24 hours",
-        "link": "https://www.theguardian.com/world/live/2020/sep/06/coronavirus-live-news-victoria-awaits-lockdown-decision-anti-mask-protests-held-in-europe"
+        "title": "Political leaders are raising ‘false hopes’ about coronavirus vaccines",
+        "link": "https://www.theguardian.com/world/2020/sep/06/political-leaders-are-raising-false-hopes-about-coronavirus-vaccines"
     },
     {
         "title": "Controversial Forecast Of COVID-19 Death Toll Worldwide : Goats and Soda",
