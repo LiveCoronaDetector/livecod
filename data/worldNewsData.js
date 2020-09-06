6f056e0422faa46c914039190709220445ab3b73
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-06 19:02:18 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -13,8 +13,8 @@ var worldNewsData = [
         "link": "https://www.washingtonpost.com/world/middle_east/ultra-orthodox-jews-clash-with-secular-israeli-officials-over-coronavirus-measures/2020/09/04/97bb37be-ee0e-11ea-bd08-1b10132b458f_story.html"
     },
     {
-        "title": "Former Italian PM Berlusconi remains hospitalized in 'most delicate phase' of coronavirus: personal doctor",
-        "link": "https://www.foxnews.com/world/italy-berlusconi-coronavirus-delicate-phase"
+        "title": "UK records highest daily coronavirus cases since May: Live news",
+        "link": "https://www.aljazeera.com/news/2020/09/mask-rally-italy-coronavirus-deaths-inch-live-news-200905233428336.html"
     },
     {
         "title": "Coronavirus Shatters India's Economy",
@@ -29,15 +29,15 @@ var worldNewsData = [
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-06-20-intl/index.html"
     },
     {
-        "title": "Political leaders are raising ‘false hopes’ about coronavirus vaccines",
-        "link": "https://www.theguardian.com/world/2020/sep/06/political-leaders-are-raising-false-hopes-about-coronavirus-vaccines"
-    },
-    {
         "title": "Controversial Forecast Of COVID-19 Death Toll Worldwide : Goats and Soda",
         "link": "https://www.npr.org/sections/goatsandsoda/2020/09/04/909783162/new-global-coronavirus-death-forecast-is-chilling-and-controversial"
     },
     {
         "title": "Northeastern University Dismisses 11 Students for Breaking Virus Rules but Keeps Their Tuition",
         "link": "https://www.nytimes.com/2020/09/05/world/coronavirus-covid.html"
+    },
+    {
+        "title": "Political leaders are raising ‘false hopes’ about coronavirus vaccines",
+        "link": "https://www.theguardian.com/world/2020/sep/06/political-leaders-are-raising-false-hopes-about-coronavirus-vaccines"
     }
 ];
