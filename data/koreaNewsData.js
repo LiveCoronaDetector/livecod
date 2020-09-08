17793b4c0a048bfa2323b25a0c0df1999d6180a5
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-08 02:02:26 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,39 +5,39 @@ var koreaNewsData = [
         "link": "http://news.kbs.co.kr/news/view.do?ncd=4534447&ref=A"
     },
     {
-        "title": "코로나로 5명 숨져 사망자 총 341명으로 늘어",
-        "link": "http://www.kwnews.co.kr/nview.asp?aid=220090700127"
+        "title": "현대중공업 조선소 코로나19 확진자 3명 발생",
+        "link": "http://yna.kr/AKR20200908024300057?did=1195m"
+    },
+    {
+        "title": "한강 뚝섬 편의점에 마스크 착용 불량 확진자 방문",
+        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=01561286625898152"
+    },
+    {
+        "title": "[종합] 코로나19 확진자 6일째 100명대...서울 67, 경기 29명",
+        "link": "http://www.fnnews.com/news/202009080937253138"
+    },
+    {
+        "title": "코로나19 신규 확진 136명, 엿새째 100명대…사망 5명",
+        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005969972&plink=ORI&cooper=NAVER"
     },
     {
         "title": "[속보] 코로나19 신규 확진 136명...엿새째 100명대",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202009080936001&code=940601"
     },
     {
-        "title": "[속보] 광주서 코로나19 확진자 2명 추가 발생…누적 442명",
-        "link": "https://www.news1.kr/articles/?4051383"
+        "title": "국내 코로나19 신규 확진자 119명…닷새째 100명대",
+        "link": "http://www.hani.co.kr/arti/society/society_general/961014.html"
     },
     {
-        "title": "[속보] 7일 신규 확진자 136명...사망자 5명 늘어",
-        "link": "https://www.chosun.com/national/2020/09/08/4UDPQVCX5VGYJPL5MWFZBQ7VPI/?utm_source=naver&utm_medium=original&utm_campaign=news"
+        "title": "WHO \"피할 수 없는 펜데믹...코로나19가 마지막 아니다\"",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/09/08/2020090800494.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
-        "title": "현대중공업 조선소 코로나19 확진자 3명 발생",
-        "link": "http://yna.kr/AKR20200908024300057?did=1195m"
+        "title": "日 코로나 신규 확진자 293명… 56일 만에 200명대",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/09/08/2020090800086.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
-        "title": "[전문] 수원시청 237번째 확진자 발생...영통3동 거주",
-        "link": "http://www.dailygrid.net/news/articleView.html?idxno=379402"
-    },
-    {
-        "title": "[속보] 코로나19 신규 확진 136명·사망5명…6일 연속 100명대",
-        "link": "http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202009080529&t=NN"
-    },
-    {
-        "title": "[속보] 코로나 국내 신규 확진 136명… 국내발생 120명·해외유입 16명",
-        "link": "http://health.chosun.com/site/data/html_dir/2020/09/08/2020090801041.html"
-    },
-    {
-        "title": "[속보] 코로나19 신규확진자 136명…엿새째 100명대",
-        "link": "http://www.hani.co.kr/arti/society/society_general/961193.html"
+        "title": "[속보] 문 대통령 \"추석 전 확진자 100명 아래로 전력\"",
+        "link": "http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202009070326&t=NN"
     }
 ];
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,27 +17,27 @@ var worldNewsData = [
         "link": "https://www.theguardian.com/world/2020/sep/07/spain-set-to-record-half-a-million-covid-cases"
     },
     {
-        "title": "India Moves Into 2nd Place For COVID-19 Cases",
-        "link": "https://www.npr.org/sections/coronavirus-live-updates/2020/09/07/910401174/india-moves-into-2nd-place-for-covid-19-cases"
+        "title": "The latest on the coronavirus pandemic: Live updates",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-07-20-intl/index.html"
     },
     {
-        "title": "Coronavirus: Melbourne lockdown extended by two weeks",
-        "link": "https://www.bbc.com/news/world-australia-54045102"
-    },
-    {
-        "title": "Coronavirus: fears UK government has lost control as Covid cases soar",
-        "link": "https://www.theguardian.com/world/2020/sep/06/coronavirus-fears-uk-government-lost-control-cases-soar"
+        "title": "UK records highest daily coronavirus cases since May: Live news",
+        "link": "https://www.aljazeera.com/news/2020/09/mask-rally-italy-coronavirus-deaths-inch-live-news-200905233428336.html"
     },
     {
         "title": "In Sign of Progress, Fewer Than 1% of New York’s Virus Tests Are Positive",
         "link": "https://www.nytimes.com/2020/09/06/world/covid-coronavirus.html"
     },
     {
-        "title": "Global report: India overtakes Brazil as second most Covid-infected country",
-        "link": "https://www.theguardian.com/world/2020/sep/07/global-report-india-overtakes-brazil-as-second-most-covid-infected-country-in-the-world"
+        "title": "Coronavirus: fears UK government has lost control as Covid cases soar",
+        "link": "https://www.theguardian.com/world/2020/sep/06/coronavirus-fears-uk-government-lost-control-cases-soar"
     },
     {
         "title": "Controversial Forecast Of COVID-19 Death Toll Worldwide : Goats and Soda",
         "link": "https://www.npr.org/sections/goatsandsoda/2020/09/04/909783162/new-global-coronavirus-death-forecast-is-chilling-and-controversial"
+    },
+    {
+        "title": "The latest on the coronavirus pandemic: Live updates",
+        "link": "https://edition.cnn.com/world/live-news/coronavirus-pandemic-09-07-20-intl/index.html"
     }
 ];
