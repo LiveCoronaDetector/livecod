04910f4e0c556b5fab300c4eeccda0a845d6c407
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-08 11:02:05 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,27 +17,27 @@ var worldNewsData = [
         "link": "https://www.theguardian.com/world/live/2020/sep/08/coronavirus-live-news-spain-cases-top-500000-in-european-first-as-global-deaths-pass-890000"
     },
     {
-        "title": "‘The Lockdown Killed My Father’: Farmer Suicides Add to India’s Virus Misery",
-        "link": "https://www.nytimes.com/2020/09/08/world/asia/india-coronavirus-farmer-suicides-lockdown.html"
+        "title": "Coronavirus LIVE News: Worldwide coronavirus cases cross 27.42 million, death toll at 892,454",
+        "link": "https://economictimes.indiatimes.com/news/coronavirus-cases-in-india-live-news-latest-updates-september8/liveblog/77988792.cms"
     },
     {
-        "title": "China Awards Coronavirus 'Heroes'",
-        "link": "https://www.youtube.com/watch?v=G1g59locZdk"
+        "title": "Coronavirus Infections Surge In France, Spain And UK | TODAY",
+        "link": "https://www.youtube.com/watch?v=2IPERV8DpJI"
+    },
+    {
+        "title": "The latest on the coronavirus pandemic: Live updates",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-08-20-intl/index.html"
     },
     {
         "title": "Coronavirus spikes in Spain, France and U.K. raise specter of second wave",
         "link": "https://www.nbcnews.com/news/world/coronavirus-spikes-spain-france-u-k-raise-specter-second-wave-n1239454"
     },
     {
-        "title": "Coronavirus UK map: confirmed Covid cases and deaths today",
-        "link": "https://www.theguardian.com/world/2020/sep/08/coronavirus-uk-map-confirmed-covid-cases-and-deaths-today"
-    },
-    {
-        "title": "The latest on the coronavirus pandemic: Live updates",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-08-20-intl/h_47508de310d9dc86a64ba45b4e082079"
-    },
-    {
         "title": "Controversial Forecast Of COVID-19 Death Toll Worldwide : Goats and Soda",
         "link": "https://www.npr.org/sections/goatsandsoda/2020/09/04/909783162/new-global-coronavirus-death-forecast-is-chilling-and-controversial"
+    },
+    {
+        "title": "#NZhellhole: how Kiwis are hitting back at Trump's Covid taunts",
+        "link": "https://www.theguardian.com/world/2020/sep/08/nzhellhole-how-kiwis-are-hitting-back-at-trumps-covid-taunts"
     }
 ];
