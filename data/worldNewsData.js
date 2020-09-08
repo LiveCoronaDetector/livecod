195d4cf91d2fc59bf521ceced26944ae20a0a061
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-08 11:31:23 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -13,31 +13,31 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/uk-54061938"
     },
     {
-        "title": "Coronavirus live news: India suffers most daily deaths in a month as global toll passes 890,000",
+        "title": "Coronavirus live news: school closures could hit economy 'for rest of century'; Tour de France director tests positive",
         "link": "https://www.theguardian.com/world/live/2020/sep/08/coronavirus-live-news-spain-cases-top-500000-in-european-first-as-global-deaths-pass-890000"
     },
     {
-        "title": "Coronavirus LIVE News: Worldwide coronavirus cases cross 27.42 million, death toll at 892,454",
-        "link": "https://economictimes.indiatimes.com/news/coronavirus-cases-in-india-live-news-latest-updates-september8/liveblog/77988792.cms"
+        "title": "‘The Lockdown Killed My Father’: Farmer Suicides Add to India’s Virus Misery",
+        "link": "https://www.nytimes.com/2020/09/08/world/asia/india-coronavirus-farmer-suicides-lockdown.html"
     },
     {
-        "title": "Coronavirus Infections Surge In France, Spain And UK | TODAY",
-        "link": "https://www.youtube.com/watch?v=2IPERV8DpJI"
+        "title": "Spain sets Western Europe coronavirus record: Live news",
+        "link": "https://www.aljazeera.com/news/2020/09/doctors-south-korea-suspend-strike-coronavirus-live-news-200907232556822.html"
     },
     {
-        "title": "The latest on the coronavirus pandemic: Live updates",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-08-20-intl/index.html"
-    },
-    {
-        "title": "Coronavirus spikes in Spain, France and U.K. raise specter of second wave",
-        "link": "https://www.nbcnews.com/news/world/coronavirus-spikes-spain-france-u-k-raise-specter-second-wave-n1239454"
+        "title": "Tour de France tech trends 2020 | The best tech at this year's race",
+        "link": "https://www.bikeradar.com/features/tour-de-france-2020-tech-round-up/"
     },
     {
         "title": "Controversial Forecast Of COVID-19 Death Toll Worldwide : Goats and Soda",
         "link": "https://www.npr.org/sections/goatsandsoda/2020/09/04/909783162/new-global-coronavirus-death-forecast-is-chilling-and-controversial"
     },
     {
-        "title": "#NZhellhole: how Kiwis are hitting back at Trump's Covid taunts",
-        "link": "https://www.theguardian.com/world/2020/sep/08/nzhellhole-how-kiwis-are-hitting-back-at-trumps-covid-taunts"
+        "title": "The latest on the coronavirus pandemic: Live updates",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-08-20-intl/h_47508de310d9dc86a64ba45b4e082079"
+    },
+    {
+        "title": "Xi Jinping defends China's coronavirus response amid global flak",
+        "link": "https://www.aljazeera.com/news/2020/09/xi-jinping-defends-china-coronavirus-response-global-flak-200908040117832.html"
     }
 ];
