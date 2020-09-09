817699b6bd93caa223248279fb1e372f96c92bb2
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-09 04:02:08 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,7 +17,7 @@ var worldNewsData = [
         "link": "https://www.washingtonpost.com/world/2020/09/08/bergamo-italy-covid-longterm/?arc404=true"
     },
     {
-        "title": "India coronavirus: Rumours stall testing in Punjab",
+        "title": "India coronavirus: Rumours hamper testing in Punjab",
         "link": "https://www.bbc.com/news/world-asia-india-54068007"
     },
     {
@@ -29,15 +29,15 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-54082192"
     },
     {
-        "title": "The latest on the coronavirus pandemic: Live updates",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-08-20-intl/index.html"
-    },
-    {
-        "title": "Don't blame us for UK's coronavirus spike, say young people",
-        "link": "https://www.theguardian.com/world/2020/sep/08/dont-blame-us-for-uks-coronavirus-spike-say-young-people"
+        "title": "Rebuilt after 9/11, World Trade Center threatened anew by coronavirus",
+        "link": "https://www.reuters.com/article/us-health-coronavirus-sept11/rebuilt-after-9-11-world-trade-center-threatened-anew-by-coronavirus-idUSKBN25Z1JD"
     },
     {
         "title": "Covid-19 News: Live Updates",
         "link": "https://www.nytimes.com/2020/09/08/world/covid-19-coronavirus.html"
+    },
+    {
+        "title": "EU says one in eight deaths is linked to pollution",
+        "link": "https://www.bbc.com/news/world-europe-54071380"
     }
 ];
