c27924e6ba0cf94a20548b38c21ace38c5bd5d6c
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-09 19:31:33 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,12 +5,12 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200909114500056?did=1195m"
     },
     {
-        "title": "코로나19-인플루엔자 중복 감염 확인…\"치명성은 더 확인해야\"(종합)",
-        "link": "http://www.newsis.com/view/?id=NISX20200909_0001160503&cID=10201&pID=10200"
-    },
-    {
         "title": "제주 50번째 코로나 확진 발생...게하발 10번째",
         "link": "http://www.jejusori.net/news/articleView.html?idxno=319947"
+    },
+    {
+        "title": "코로나19-인플루엔자 중복 감염 확인…\"치명성은 더 확인해야\"(종합)",
+        "link": "http://www.newsis.com/view/?id=NISX20200909_0001160503&cID=10201&pID=10200"
     },
     {
         "title": "제주 39번·45번 코로나19 확진자 퇴원…19명 치료 중",
