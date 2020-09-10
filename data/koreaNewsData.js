80bfb58ccee989b8fc3e0353bcc53d16cc38c2b8
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-10 15:02:27 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -9,28 +9,24 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20200910069600004?did=1195m"
     },
     {
-        "title": "에어컨 틀고 환기 안해서? 확진자 3분 태운 택시기사 감염",
-        "link": "https://news.joins.com/article/olink/23463755"
-    },
-    {
         "title": "국내 코로나19 신규 확진자 155명…경기 47명·서울 46명·충남 12명 등",
         "link": "http://news.kbs.co.kr/news/view.do?ncd=5001217&ref=A"
-    },
-    {
-        "title": "세브란스 확진자 17명, 서로 다른 2개 집단 동시 발생",
-        "link": "http://www.ohmynews.com/NWS_Web/View/at_pg.aspx?CNTN_CD=A0002674838&CMPT_CD=P0010&utm_source=naver&utm_medium=newsearch&utm_campaign=naver_news"
     },
     {
         "title": "포항서 코로나19 확진자 2명 발생…광주서 확진자 접촉",
         "link": "https://news.imaeil.com/Society/2020091011542894740"
     },
     {
-        "title": "[종합] 신촌세브란스병원 코로나19 확진자 총 17명 발생",
-        "link": "http://www.fnnews.com/news/202009101523048477"
+        "title": "세브란스 확진자 17명, 서로 다른 2개 집단 동시 발생",
+        "link": "http://www.ohmynews.com/NWS_Web/View/at_pg.aspx?CNTN_CD=A0002674838&CMPT_CD=P0010&utm_source=naver&utm_medium=newsearch&utm_campaign=naver_news"
     },
     {
-        "title": "울산 또 4명 코로나19 추가 감염, 누적 확진자 131명",
-        "link": "http://www.fnnews.com/news/202009101113594121"
+        "title": "확진자 5분 태운 택시기사 2명 마스크 착용했으나 확진···“이례적 감염”",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202009101428001&code=940601"
+    },
+    {
+        "title": "[종합] 신촌세브란스병원 코로나19 확진자 총 17명 발생",
+        "link": "http://www.fnnews.com/news/202009101523048477"
     },
     {
         "title": "[속보] 코로나19 신규 확진 155명·사망 2명…8일째 100명대",
@@ -39,5 +35,9 @@ var koreaNewsData = [
     {
         "title": "트럼프는, 코로나 무서운 줄 알고도 속여왔다",
         "link": "http://www.hani.co.kr/arti/international/international_general/961640.html"
+    },
+    {
+        "title": "에어컨 틀고 환기 안해서? 확진자 3분 태운 택시기사 감염",
+        "link": "https://news.joins.com/article/olink/23463755"
     }
 ];
