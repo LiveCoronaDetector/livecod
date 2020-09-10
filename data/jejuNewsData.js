8f38a80cc9f7eac3f81d774600592872b8a9797a
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-10 06:31:45 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -9,12 +9,16 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200909114500056?did=1195m"
     },
     {
-        "title": "제주 도내 49번째 확진자 역학조사 결과공개",
-        "link": "https://www.pressian.com/pages/articles/2020091012353885636?utm_source=naver&utm_medium=search"
+        "title": "제주 50번째 코로나19 확진자 발생…게스트하우스발 감염",
+        "link": "http://www.segye.com/content/html/2020/09/10/20200910515180.html?OutUrl=naver"
     },
     {
-        "title": "제주도 공공시설 10월5일까지 운영중지...22일 연장",
-        "link": "http://www.jejusori.net/news/articleView.html?idxno=319967"
+        "title": "제주도내 공공시설 운영 중지 10월 5일까지 연장",
+        "link": "https://www.pressian.com/pages/articles/2020091012571864645?utm_source=naver&utm_medium=search"
+    },
+    {
+        "title": "제주 49번 코로나19 확진자 역학조사 결과 ‘공개’",
+        "link": "https://view.asiae.co.kr/article/2020091011192722410"
     },
     {
         "title": "코로나19-인플루엔자 중복 감염 확인…\"치명성은 더 확인해야\"(종합)",
@@ -29,15 +33,11 @@ var jejuNewsData = [
         "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005972446&plink=ORI&cooper=NAVER"
     },
     {
-        "title": "국내 벤처 제약사 '제넨셀', 인도서 '제주 담팔수 추출물' 코로나19 치료제 3...",
-        "link": "http://www.viva100.com/main/view.php?key=20200909010001913"
+        "title": "제주 담팔수 추출물, 코로나19 치료제 주목",
+        "link": "http://www.jejusori.net/news/articleView.html?idxno=319893"
     },
     {
         "title": "코로나19 국내 발생현황 브리핑 (20. 09. 09. 14시)",
         "link": "http://www.ktv.go.kr/content/view?content_id=608736"
-    },
-    {
-        "title": "제주도, 도청 외부인 방문 출입제한 기간 연장",
-        "link": "http://www.jejunews.com/news/articleView.html?idxno=2171434"
     }
 ];
