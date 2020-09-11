41e5d26e68f4db3c6aab68b971186c20938c1bc3
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-11 12:03:28 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,28 +5,32 @@ var jejuNewsData = [
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202009102146001&code=620117"
     },
     {
+        "title": "온천발 코로나19 확진자 발생… 52번 확진자 가족",
+        "link": "http://www.jejunews.com/news/articleView.html?idxno=2171532"
+    },
+    {
         "title": "코로나19 제주 50번째 확진자 발생",
         "link": "https://www.nocutnews.co.kr/news/5409818"
+    },
+    {
+        "title": "제주도 53번 코로나19 환자 발생…온천발 9명째",
+        "link": "http://www.fnnews.com/news/202009112029479098"
+    },
+    {
+        "title": "제주 산방산온천發 확진자 추가 발생...'동네목욕탕'도 비상",
+        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=427141"
     },
     {
         "title": "제주 코로나19 확진자 3명 퇴원…18명 격리치료",
         "link": "https://www.news1.kr/articles/?4056091"
     },
     {
-        "title": "[2보] 제주 52번 코로나19 확진자 접촉자 3명",
-        "link": "http://www.jejunews.com/news/articleView.html?idxno=2171531"
+        "title": "제주 53번 코로나19 확진자 발생…산방산탄산온천 직원",
+        "link": "http://yna.kr/AKR20200911166100056?did=1195m"
     },
     {
-        "title": "제주 53번째 코로나19 확진자 발생... 52번 가족",
-        "link": "http://www.ihalla.com/read.php3?aid=1599822876691934073"
-    },
-    {
-        "title": "제주도 53번째 코로나19 확진자 발생...'산방산온천 직원'",
-        "link": "http://www.headlinejeju.co.kr/news/articleView.html?idxno=427140"
-    },
-    {
-        "title": "제주서 코로나19 확진자 잇따라 발생…누적 52명",
-        "link": "http://yna.kr/AKR20200910178400056?did=1195m"
+        "title": "코로나19 확진자 2명 오늘 하루 잇따라 발생",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=5001824&ref=A"
     },
     {
         "title": "제주도 49번째 코로나 확진자 발생...미국서 입국",
@@ -35,9 +39,5 @@ var jejuNewsData = [
     {
         "title": "네팔 국적 51번째 확진자 접촉자 15명",
         "link": "http://www.jejunews.com/news/articleView.html?idxno=2171527"
-    },
-    {
-        "title": "제주 50번째 코로나 확진 발생...게하발 10번째",
-        "link": "http://www.jejusori.net/news/articleView.html?idxno=319947"
     }
 ];
