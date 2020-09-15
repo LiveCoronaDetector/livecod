a65772fd0e3c0b812ee5e8988c9874fb0041315b
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-15 07:01:56 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://nypost.com/2020/09/14/chinese-virologist-posts-report-claiming-covid-19-was-made-in-wuhan-lab/"
     },
     {
-        "title": "Coronavirus: Marseille's Covid-19 hospital beds 'close to saturation'",
-        "link": "https://www.bbc.com/news/world-europe-54151281"
-    },
-    {
         "title": "'Nothing more could have been done': Trump's final phone call to Woodward",
         "link": "https://www.cnn.com/2020/09/14/politics/trump-bob-woodward-final-phone-call/index.html"
     },
     {
-        "title": "World Health Organization reports new daily record in COVID-19 cases",
-        "link": "https://www.cbsnews.com/news/coronavirus-world-health-organization-daily-record-covid-19-cases/"
-    },
-    {
         "title": "Trial finds medicine helps shorten COVID-19 hospital stay",
         "link": "https://www.youtube.com/watch?v=Y-qK3n4mz2I"
+    },
+    {
+        "title": "The latest on the coronavirus pandemic: Live updates",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-15-20-intl/index.html"
+    },
+    {
+        "title": "World Health Organization reports new daily record in COVID-19 cases",
+        "link": "https://www.cbsnews.com/news/coronavirus-world-health-organization-daily-record-covid-19-cases/"
     }
 ];
