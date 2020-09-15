b24cec202fe6248cc91b2f45a1e3869234907bca
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-15 22:31:19 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -9,8 +9,8 @@ var worldNewsData = [
         "link": "https://www.pewresearch.org/global/2020/09/15/us-image-plummets-internationally-as-most-say-country-has-handled-coronavirus-badly/"
     },
     {
-        "title": "How Israel Became the First Rich Country to Go Into a Second Nationwide Coronavirus Lockdown",
-        "link": "https://time.com/5889096/israel-second-lockdown-covid-19/"
+        "title": "Syria may only be counting 1.25% of its actual coronavirus deaths, study says",
+        "link": "https://www.cbsnews.com/news/coronavirus-in-syria-deaths-under-counted-amid-civil-war-bashar-assad-regime-blames-sanctions/"
     },
     {
         "title": "American woman's bar crawl spreads Covid in southern Germany",
@@ -21,8 +21,8 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-asia-india-54139112"
     },
     {
-        "title": "Chinese virologist posts report claiming COVID-19 was made in Wuhan lab",
-        "link": "https://nypost.com/2020/09/14/chinese-virologist-posts-report-claiming-covid-19-was-made-in-wuhan-lab/"
+        "title": "Global report: Covid cases near 30m as China expects vaccine as soon as November",
+        "link": "https://www.theguardian.com/world/2020/sep/15/global-report-covid-cases-near-30m-as-chinese-health-official-expects-vaccine-as-soon-as-november"
     },
     {
         "title": "Trump less trusted internationally than Putin and Xi after Covid-19 response",
@@ -30,14 +30,14 @@ var worldNewsData = [
     },
     {
         "title": "Carnival's Ruby Princess Cruise Ship Spread Coronavirus Around the World",
-        "link": "https://www.bloomberg.com/tosv2.html?vid=&uuid=098763b0-f79f-11ea-990c-4fb4c7b1b210&url=L25ld3MvZmVhdHVyZXMvMjAyMC0wOS0xNS9jYXJuaXZhbC1zLXJ1YnktcHJpbmNlc3MtY3J1aXNlLXNoaXAtc3ByZWFkLWNvcm9uYXZpcnVzLWFyb3VuZC10aGUtd29ybGQ="
-    },
-    {
-        "title": "No coronavirus vaccine for entire world until 2024... at best: CEO of producer",
-        "link": "https://www.foxbusiness.com/healthcare/no-coronavirus-vaccine-for-entire-world-until-2024-at-best-ceo-of-producer"
+        "link": "https://www.bloomberg.com/tosv2.html?vid=&uuid=25a3e790-f7a3-11ea-866e-71be7e3c69e7&url=L25ld3MvZmVhdHVyZXMvMjAyMC0wOS0xNS9jYXJuaXZhbC1zLXJ1YnktcHJpbmNlc3MtY3J1aXNlLXNoaXAtc3ByZWFkLWNvcm9uYXZpcnVzLWFyb3VuZC10aGUtd29ybGQ="
     },
     {
         "title": "The latest on the coronavirus pandemic: Live updates",
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-15-20-intl/index.html"
+    },
+    {
+        "title": "No coronavirus vaccine for entire world until 2024... at best: CEO of producer",
+        "link": "https://www.foxbusiness.com/healthcare/no-coronavirus-vaccine-for-entire-world-until-2024-at-best-ceo-of-producer"
     }
 ];
