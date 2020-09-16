3a05494bdd0dfd63e13aa5fcd0fbc475d4220a9e
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-16 05:02:03 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -5,20 +5,24 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200915137800056?did=1195m"
     },
     {
-        "title": "제주도, 청사 출입제한조치 당분간 유지",
-        "link": "https://www.pressian.com/pages/articles/2020091611435801796?utm_source=naver&utm_medium=search"
-    },
-    {
         "title": "‘온천 직원’ 제주 53번 확진자…해수사우나도 방문",
         "link": "http://www.fnnews.com/news/202009161235467580"
+    },
+    {
+        "title": "제주도, 청사 출입제한조치 당분간 유지",
+        "link": "https://www.pressian.com/pages/articles/2020091611435801796?utm_source=naver&utm_medium=search"
     },
     {
         "title": "코로나19 확진 일가족 농협·내과 등 방문",
         "link": "http://www.jejunews.com/news/articleView.html?idxno=2171691"
     },
     {
-        "title": "거짓말해 제주도에 코로나19 확산 시킨 목사부부 고발",
-        "link": "http://www.newsis.com/view/?id=NISX20200916_0001167436&cID=10813&pID=10800"
+        "title": "제주도, 온천방문 숨기고 역학조사 비협조 목사부부 고발",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202009161341001&code=620117"
+    },
+    {
+        "title": "서귀포시 산방산탄산온천 방문 숨긴 29·33번 확진자 고발",
+        "link": "http://www.mediajeju.com/news/articleView.html?idxno=325901"
     },
     {
         "title": "추석 연휴에 20만명 제주도 찾는다…'여름 휴가철 수준'",
@@ -35,9 +39,5 @@ var jejuNewsData = [
     {
         "title": "성남 코로나 377번 확진자, 1박2일간 제주 여행",
         "link": "http://www.jejusori.net/news/articleView.html?idxno=320111"
-    },
-    {
-        "title": "수도권 방문 제주도 일가족 3명 코로나19 확진 판정",
-        "link": "http://www.fnnews.com/news/202009150034363185"
     }
 ];
