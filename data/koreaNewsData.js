28042b4088ad93e28b79fbcb4f6efada58b2a8d1
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-18 12:47:12 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,36 +5,36 @@ var koreaNewsData = [
         "link": "https://cnbc.sbs.co.kr/article_hub/10000996810?division=NAVER"
     },
     {
-        "title": "쿠팡 송파 물류센터 재가동…추가 확진자 없어",
-        "link": "http://www.fnnews.com/news/202009181437530940"
-    },
-    {
-        "title": "부천 주민 2명 코로나19 확진…감염경로 조사 중(종합)",
-        "link": "http://yna.kr/AKR20200918088951065?did=1195m"
-    },
-    {
         "title": "코로나 신규 확진 126명…지역발생 사흘째 세자릿수",
         "link": "http://www.hani.co.kr/arti/society/health/962693.html"
     },
     {
-        "title": "서울, 신규 확진자 46명…서울국세청 관련 누적 5명",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/09/18/2020091801638.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "서울 어제 확진자 40명선 감소…서울국세청 관련 누적 5명(종합)",
+        "link": "http://yna.kr/AKR20200918061351004?did=1195m"
     },
     {
-        "title": "서울지방국세청에서 코로나 집단감염...현재까지 4명 확진",
-        "link": "https://www.ytn.co.kr/_ln/0102_202009181555052188"
+        "title": "쿠팡 송파 물류센터 재가동…추가 확진자 없어",
+        "link": "http://www.fnnews.com/news/202009181437530940"
     },
     {
-        "title": "서울시, 사랑제일교회·전광훈 목사에 46억 손배 소송…서울 확진자 46명↑",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=5007662&ref=A"
+        "title": "서울지방국세청서 코로나 집단감염…5명 확진",
+        "link": "http://news.tf.co.kr/read/life/1813663.htm"
+    },
+    {
+        "title": "쿠팡 남양주2배송캠프, 코로나19 확진자 발생으로 폐쇄",
+        "link": "http://news.kbs.co.kr/news/view.do?ncd=5007902&ref=A"
     },
     {
         "title": "'감염 경로 불명' 확진자 26.8%…나흘째 최고치 경신",
         "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1005986705&plink=ORI&cooper=NAVER"
     },
     {
-        "title": "[전문] 제주도 코로나19 58번째 확진자 동선공개",
-        "link": "http://www.dailygrid.net/news/articleView.html?idxno=380999"
+        "title": "경기도, 기아차공장 등 코로나 확진자 36명",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/09/18/2020091801233.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+    },
+    {
+        "title": "서울지방국세청에서 코로나 집단감염...현재까지 4명 확진",
+        "link": "https://www.ytn.co.kr/_ln/0102_202009181555052188"
     },
     {
         "title": "수도권 종교시설서 코로나19 확진자 지속 발생 [종합]",
