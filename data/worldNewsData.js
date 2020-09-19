fe67932d9d07842c1e38a264d3ca2712b44aaa6b
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-19 10:02:13 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,20 +5,20 @@ var worldNewsData = [
         "link": "https://www.scmp.com/news/china/science/article/3102146/coronavirus-africa-hopes-it-has-seen-worst-covid-19-case-numbers"
     },
     {
-        "title": "US extends coronavirus travel restrictions across Canadian, Mexican borders",
-        "link": "https://www.foxnews.com/politics/us-extends-travel-canadian-mexican-borders"
+        "title": "A new Canadian Covid-19 gargle test 'one of the first of its kind' in the world, doctor says",
+        "link": "https://www.cnn.com/2020/09/19/americas/canadian-gargle-test/index.html"
     },
     {
         "title": "Iran appears to be in grip of ‘third wave’ of coronavirus outbreak",
         "link": "https://www.theguardian.com/world/2020/sep/18/iran-appears-grip-of-third-wave-coronavirus-outbreak"
     },
     {
-        "title": "Lockdown measures and rising anger in Madrid as Covid takes hold again",
+        "title": "Lockdown measures and rising anger in Madrid as Covid-19 takes hold again",
         "link": "https://www.theguardian.com/world/2020/sep/18/madrid-poor-spanish-capital-covid-19"
     },
     {
-        "title": "Indonesians caught without a mask forced to dig graves for Covid-19 victims",
-        "link": "https://www.cnn.com/2020/09/17/asia/indonesia-coronavirus-grave-diggers-intl-hnk-scli/index.html"
+        "title": "Worldwide coronavirus cases top 30 million",
+        "link": "https://abcnews.go.com/Health/live-updates/coronavirus/?id=73065540"
     },
     {
         "title": "The bogus Steve Bannon-backed study claiming China created the coronavirus, explained",
@@ -29,15 +29,15 @@ var worldNewsData = [
         "link": "https://www.nytimes.com/2020/09/18/world/covid-coronavirus.html"
     },
     {
-        "title": "CDC reverses controversial coronavirus guidance that said people without symptoms may not need a test",
-        "link": "https://www.cnbc.com/2020/09/18/cdc-reverses-coronavirus-guidance-that-said-people-without-symptoms-may-not-need-a-test.html"
+        "title": "Coronavirus: US health chiefs reverse advice on Covid-19 testing",
+        "link": "https://www.bbc.com/news/world-us-canada-54214420"
     },
     {
-        "title": "China and Russia are ahead in the global coronavirus vaccine race, bending long-standing rules as they go",
-        "link": "https://www.washingtonpost.com/world/asia_pacific/china-and-russia-are-ahead-in-the-global-coronavirus-vaccine-race-bending-long-standing-rules-as-they-go/2020/09/18/9bfd4438-e2d4-11ea-82d8-5e55d47e90ca_story.html"
+        "title": "China and Russia adopt 'one team' mindset on COVID vaccines vs US",
+        "link": "https://asia.nikkei.com/Spotlight/Coronavirus/China-and-Russia-adopt-one-team-mindset-on-COVID-vaccines-vs-US"
     },
     {
-        "title": "The US is approaching 200000 Covid-19 deaths. Here's a look at where cases are rising across the country.",
-        "link": "https://edition.cnn.com/world/live-news/coronavirus-pandemic-09-18-20-intl/h_7dc4cd640e28dc0e0c3fd0a5095b07e7"
+        "title": "Watch live: The World Health Organization holds a press briefing on the coronavirus pandemic",
+        "link": "https://www.cnbc.com/2020/09/18/watch-live-the-world-health-organization-holds-a-press-briefing-on-the-coronavirus-pandemic.html"
     }
 ];
