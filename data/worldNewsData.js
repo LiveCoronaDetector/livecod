72545adb05ac8f8f54ca28f429bf8da2aa1840b1
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-19 01:03:25 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -13,31 +13,31 @@ var worldNewsData = [
         "link": "https://www.theguardian.com/world/2020/sep/18/iran-appears-grip-of-third-wave-coronavirus-outbreak"
     },
     {
-        "title": "'Pandemic fatigue' leads to resurgence of coronavirus in Europe where cases hit fresh records in France and Spain",
-        "link": "https://www.cnbc.com/2020/09/18/pandemic-fatigue-leads-to-resurgence-of-coronavirus-in-europe-where-france-and-spain-hit-records.html"
+        "title": "Coronavirus: France, Spain, and UK set to introduce new local measures amid surge in cases",
+        "link": "https://www.euronews.com/2020/09/18/covid-19-france-spain-and-uk-set-to-introduce-new-local-measures-amid-surge-in-cases"
     },
     {
         "title": "Only the US has more coronavirus cases than India. But unlike Trump, its leader seems immune from criticism",
         "link": "https://www.cnn.com/2020/09/16/asia/india-modi-coronavirus-intl-dst-hnk/index.html"
     },
     {
-        "title": "The bogus Steve Bannon-backed study claiming China created the coronavirus, explained",
-        "link": "https://www.vox.com/2020/9/18/21439865/coronavirus-china-study-bannon"
+        "title": "China and Russia are ahead in the global coronavirus vaccine race, bending long-standing rules as they go",
+        "link": "https://www.washingtonpost.com/world/asia_pacific/china-and-russia-are-ahead-in-the-global-coronavirus-vaccine-race-bending-long-standing-rules-as-they-go/2020/09/18/9bfd4438-e2d4-11ea-82d8-5e55d47e90ca_story.html"
     },
     {
         "title": "Live Covid-19 Tracker and Updates",
         "link": "https://www.nytimes.com/2020/09/18/world/covid-coronavirus.html"
     },
     {
-        "title": "CDC Head Says No Vaccine For All Until Late 2021 | The View",
-        "link": "https://www.youtube.com/watch?v=a7DGeFLkf7Q"
+        "title": "Trump clashes with top medical advisers on coronavirus vaccine timeline",
+        "link": "https://www.youtube.com/watch?v=X-psnKdGeQA"
     },
     {
         "title": "Watch live: The World Health Organization holds a press briefing on the coronavirus pandemic",
         "link": "https://www.cnbc.com/2020/09/18/watch-live-the-world-health-organization-holds-a-press-briefing-on-the-coronavirus-pandemic.html"
     },
     {
-        "title": "WHO warns of 'very serious situation' in Europe, with 'alarming rates' of virus transmission",
-        "link": "https://www.cnn.com/2020/09/17/europe/coronavirus-europe-who-second-wave-intl/index.html"
+        "title": "Fineprint: Coronavirus cases rise as European nations reopen | COVID-19 Pandemic",
+        "link": "https://news.google.com/articles/CCAiC0ZoRkdabWE1ZEdjmAEB?hl=en-US&gl=US&ceid=US%3Aen"
     }
 ];
