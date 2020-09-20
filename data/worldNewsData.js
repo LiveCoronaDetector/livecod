1bbc8777d391670f1f3e4f2d3a810af37ce83ae0
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-20 15:31:41 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,39 +5,39 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/world-africa-54207503"
     },
     {
-        "title": "Canada unveils COVID-19 'gargle, spit' test, first of its kind",
-        "link": "https://www.foxnews.com/world/canada-covid-gargle-spit-test"
+        "title": "As coronavirus skyrocketed in the US, so did stress and depression",
+        "link": "https://www.cnn.com/2020/09/20/health/us-coronavirus-sunday/index.html"
     },
     {
         "title": "Iran appears to be in grip of ‘third wave’ of coronavirus outbreak",
         "link": "https://www.theguardian.com/world/2020/sep/18/iran-appears-grip-of-third-wave-coronavirus-outbreak"
     },
     {
-        "title": "Coronavirus live news: India sees 92,000 new infections; China faces fifth wave, expert says",
-        "link": "https://www.theguardian.com/world/live/2020/sep/20/coronavirus-live-news-france-sees-another-record-daily-rise-as-victoria-records-14-new-cases"
+        "title": "How it all went wrong (again) in Europe as second wave grips continent",
+        "link": "https://www.cnn.com/2020/09/19/europe/europe-second-wave-coronavirus-intl/index.html"
     },
     {
-        "title": "2 imported COVID-19 cases reported in Shanghai - SHINE News",
-        "link": "https://www.shine.cn/news/metro/2009196368/"
+        "title": "India's coronavirus infections surge to 5.4 million",
+        "link": "https://www.reuters.com/article/us-health-coronavirus-india-cases/indias-coronavirus-infections-surge-to-54-million-idUSKCN26B06L"
     },
     {
-        "title": "China reports 14 new imported Covid-19 cases; no local case for 33 days",
-        "link": "https://www.thestar.com.my/aseanplus/aseanplus-news/2020/09/19/china-reports-14-new-imported-covid-19-cases-no-local-case-for-33-days"
-    },
-    {
-        "title": "AstraZeneca Releases Blueprints for Virus Vaccine Trial Amid Safety Scrutiny",
-        "link": "https://www.nytimes.com/2020/09/19/world/covid-coronavirus.html"
+        "title": "Belgium tops 100,000 cases; world nears 1m COVID-19 deaths: Live",
+        "link": "https://www.aljazeera.com/news/2020/09/brazil-argentina-join-coronavirus-vaccine-alliance-live-news-200919231757412.html"
     },
     {
         "title": "Global preparation: how different countries planned for the second wave of Covid-19",
         "link": "https://www.theguardian.com/world/2020/sep/20/global-preparation-how-different-countries-planned-for-the-second-wave-of-covid-19"
     },
     {
-        "title": "UK and France 'desperate' to go back into lockdown despite flatlining COVID deaths",
-        "link": "https://www.youtube.com/watch?v=5DBgWJtvhVg"
+        "title": "AstraZeneca Releases Blueprints for Virus Vaccine Trial Amid Safety Scrutiny",
+        "link": "https://www.nytimes.com/2020/09/19/world/covid-coronavirus.html"
     },
     {
-        "title": "China and Russia are ahead in the global coronavirus vaccine race, bending long-standing rules as they go",
-        "link": "https://www.washingtonpost.com/world/asia_pacific/china-and-russia-are-ahead-in-the-global-coronavirus-vaccine-race-bending-long-standing-rules-as-they-go/2020/09/18/9bfd4438-e2d4-11ea-82d8-5e55d47e90ca_story.html"
+        "title": "Hands, face, space? Johnson's Covid message has got priorities wrong, scientists warn",
+        "link": "https://www.theguardian.com/world/2020/sep/19/scientists-criticise-uks-hands-face-space-campaign-to-control-covid-19-coronavirus"
+    },
+    {
+        "title": "UK and France 'desperate' to go back into lockdown despite flatlining COVID deaths",
+        "link": "https://www.youtube.com/watch?v=5DBgWJtvhVg"
     }
 ];
