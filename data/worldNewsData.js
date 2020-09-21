28ee1d928899b1214d3c60765c280116fe16dd35
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-21 03:31:23 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.nytimes.com/2020/09/20/world/asia/china-tv-women-coronavirus.html"
     },
     {
-        "title": "Global preparation: how different countries planned for the second wave of Covid-19",
-        "link": "https://www.theguardian.com/world/2020/sep/20/global-preparation-how-different-countries-planned-for-the-second-wave-of-covid-19"
-    },
-    {
         "title": "Covid-19 Live Updates: Health Officials Tiptoe Around Trump’s Vaccine Timeline",
         "link": "https://www.nytimes.com/2020/09/20/world/covid-coronavirus.html"
+    },
+    {
+        "title": "Global preparation: how different countries planned for the second wave of Covid-19",
+        "link": "https://www.theguardian.com/world/2020/sep/20/global-preparation-how-different-countries-planned-for-the-second-wave-of-covid-19"
     },
     {
         "title": "Anti-lockdown protests demand cities reconsider resuming coronavirus restrictions",
         "link": "https://www.foxnews.com/world/coronavirus-lockdown-restrictions-london-germany-spain"
     },
     {
-        "title": "AstraZeneca Releases Blueprints for Virus Vaccine Trial Amid Safety Scrutiny",
-        "link": "https://www.nytimes.com/2020/09/19/world/covid-coronavirus.html"
+        "title": "COVID-19 in Arizona: State reports more than 400 new cases, 9 new deaths Sunday",
+        "link": "https://www.azcentral.com/story/news/local/arizona-health/2020/09/20/arizona-covid-19-update-214018-identified-cases-5476-known-deaths/5844569002/"
     }
 ];
