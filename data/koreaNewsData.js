c18034b2586e6560e8edd344b4da8f60a1856936
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-21 09:31:51 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,12 +5,12 @@ var koreaNewsData = [
         "link": "http://www.hani.co.kr/arti/society/health/962944.html"
     },
     {
-        "title": "신규 확진자 이틀 연속 두자릿수…수도권 확진자 50명 미만",
-        "link": "http://news.kbs.co.kr/news/view.do?ncd=5008855&ref=A"
-    },
-    {
         "title": "\"전국 확산 우려\"…동아대 관련 코로나19 확진자 총 12명",
         "link": "http://kormedi.com/?p=1324596"
+    },
+    {
+        "title": "'거리두기 통했나' 하루 신규확진자 70명…이틀째 '두자릿수'",
+        "link": "http://www.sisajournal.com/news/articleView.html?idxno=205445"
     },
     {
         "title": "코로나 국내 신규 확진 70명… 국내발생 55명·해외유입 15명",
@@ -21,23 +21,23 @@ var koreaNewsData = [
         "link": "http://www.ichannela.com/news/main/news_detailPage.do?publishId=000000220435"
     },
     {
-        "title": "[속보] 코로나19 신규확진 70명…이틀째 두자릿수",
-        "link": "http://news.tf.co.kr/read/life/1813956.htm"
+        "title": "경북 포항서 '코로나19' 확진자 1명 추가, 총 72명",
+        "link": "http://www.fnnews.com/news/202009211451509424"
     },
     {
-        "title": "\"미국 코로나 대응 형편없다\" 빌 게이츠의 작심 발언",
-        "link": "http://www.fnnews.com/news/202009210935241068"
-    },
-    {
-        "title": "포항 세명기독병원 코로나19 확진자 1명 추가...총 72명",
-        "link": "https://www.pressian.com/pages/articles/2020092115022625237?utm_source=naver&utm_medium=search"
+        "title": "빌 게이츠 \"美, 24시간내 코로나 진단 못해…형편없다\"",
+        "link": "http://news.mt.co.kr/mtview.php?no=2020092110192379128"
     },
     {
         "title": "미 코로나 사망자 20만명…전세계 사망자 100만명 근접(종합)",
         "link": "http://yna.kr/AKR20200921002751075?did=1195m"
     },
     {
-        "title": "[종합] 포항서 코로나19 확진자 하룻새 2명 추가 발생",
-        "link": "http://www.kukinews.com/newsView/kuk202009210304"
+        "title": "'확진자수 4위' 러시아, 한국인 입국금지 27일부터 '해제'",
+        "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=01479286625902416"
+    },
+    {
+        "title": "부산 동아대 부민캠퍼스 코로나19 환자 3명 더 늘어…총 12명",
+        "link": "https://www.donga.com/news/article/all/20200921/103030616/2"
     }
 ];
