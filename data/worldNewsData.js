7a145424078a0810383accb44c137cd03f8960a4
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-24 20:31:41 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.theguardian.com/world/2020/sep/24/wuhan-covid-journalist-missing-since-february-found-says-friend-chen-qiushi-china"
     },
     {
-        "title": "Coronavirus continuing to mutate, study finds, as US cases rise",
-        "link": "https://www.theguardian.com/world/2020/sep/24/coronavirus-mutate-contagious-study-us-cases"
-    },
-    {
         "title": "The latest on the coronavirus pandemic: Live updates",
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-24-20-intl/index.html"
     },
     {
-        "title": "'Close to 100% accuracy': Helsinki airport uses sniffer dogs to detect Covid",
-        "link": "https://www.theguardian.com/world/2020/sep/24/close-to-100-accuracy-airport-enlists-sniffer-dogs-to-test-for-covid-19"
+        "title": "Coronavirus continuing to mutate, study finds, as US cases rise",
+        "link": "https://www.theguardian.com/world/2020/sep/24/coronavirus-mutate-contagious-study-us-cases"
     },
     {
         "title": "Covid-19 Live News: Cuomo Says New York Will Vet Federally Authorized Vaccines",
         "link": "https://www.nytimes.com/2020/09/24/world/covid-19-coronavirus.html"
+    },
+    {
+        "title": "Coronavirus: Israel tightens second lockdown to avoid 'abyss'",
+        "link": "https://www.bbc.com/news/world-middle-east-54278293"
     }
 ];
