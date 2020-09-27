57d533ba9c3962965767ee0c3994fae0de735b97
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-27 15:02:01 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -29,12 +29,12 @@ var jejuNewsData = [
         "link": "http://yna.kr/AKR20200924133900530?did=1195m"
     },
     {
-        "title": "추석 '코로나19 유입 차단' 비상 방역체계 시험대",
-        "link": "http://www.jejuilbo.net/news/articleView.html?idxno=151664"
-    },
-    {
         "title": "제주 59번째 코로나19 확진자 발생…49번과 미국 방문",
         "link": "http://www.fnnews.com/news/202009232307251573"
+    },
+    {
+        "title": "추석 '코로나19 유입 차단' 비상 방역체계 시험대",
+        "link": "http://www.jejuilbo.net/news/articleView.html?idxno=151664"
     },
     {
         "title": "[속보]제주 59번째 코로나 확진자 발생…\"이동 동선 없어\"",
