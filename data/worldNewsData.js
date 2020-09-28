de160a13ba0f0540d83fc0142d102a9d44dd5572
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-28 13:31:41 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,27 +17,27 @@ var worldNewsData = [
         "link": "https://www.theguardian.com/world/2020/sep/27/madrid-standoff-government-covid-19-lockdown-coronavirus"
     },
     {
-        "title": "Why India should worry about post-Covid-19 care",
-        "link": "https://www.bbc.com/news/world-asia-india-54261125"
+        "title": "The latest on the coronavirus pandemic: Live updates",
+        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-28-20-intl/h_486efadfe0e9354392b45c26ad901760"
     },
     {
         "title": "Mother’s milk could help fight coronavirus, study finds",
         "link": "https://www.scmp.com/news/china/science/article/3103248/mothers-milk-could-help-fight-coronavirus-study-finds"
     },
     {
-        "title": "Top White House Official Denies Pressuring F.D.A. on Vaccine Guidelines",
-        "link": "https://www.nytimes.com/2020/09/27/world/covid-19-coronavirus.html"
-    },
-    {
         "title": "The latest on the coronavirus pandemic: Live updates",
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-28-20-intl/index.html"
     },
     {
-        "title": "Live updates: U.S. coronavirus cases top 7 million, as global death toll nears milestone",
-        "link": "https://www.washingtonpost.com/nation/2020/09/28/coronavirus-covid-live-updates-us/"
+        "title": "Coronavirus death toll quickly approaches 1 million",
+        "link": "https://www.washingtonpost.com/graphics/2020/world/coronavirus-deaths-1-million/"
     },
     {
-        "title": "Greece says first migrant dies of COVID-19 since the pandemic",
-        "link": "https://www.msn.com/en-us/news/world/greece-says-first-migrant-dies-of-covid-19-since-the-pandemic/ar-BB19tpzT"
+        "title": "Top White House Official Denies Pressuring F.D.A. on Vaccine Guidelines",
+        "link": "https://www.nytimes.com/2020/09/27/world/covid-19-coronavirus.html"
+    },
+    {
+        "title": "Live updates: U.S. coronavirus cases top 7 million, as global death toll nears milestone",
+        "link": "https://www.washingtonpost.com/nation/2020/09/28/coronavirus-covid-live-updates-us/"
     }
 ];
