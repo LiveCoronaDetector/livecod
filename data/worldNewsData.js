250259358a8b1621431271740f337c27a50f9eb2
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-28 06:32:27 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.scmp.com/news/china/science/article/3103248/mothers-milk-could-help-fight-coronavirus-study-finds"
     },
     {
-        "title": "Jacinda Ardern flags trans-Tasman bubble could come soon",
-        "link": "https://www.youtube.com/watch?v=70VEMQK_csg"
+        "title": "New Zealanders may be able to travel to some Australian states before Christmas, Ardern says",
+        "link": "https://www.reuters.com/article/uk-health-coronavirus-newzealand-idUKKBN26I111"
     },
     {
         "title": "CBS's Margaret Brennan Confronts Trump Aide On FDA Interference: 'You're Not A Doctor'",
         "link": "https://www.huffpost.com/entry/white-house-mark-meadows-fda-vaccine_n_5f70acb0c5b61af20e7721b4"
     },
     {
-        "title": "Coronavirus death toll quickly approaches 1 million",
-        "link": "https://www.washingtonpost.com/graphics/2020/world/coronavirus-deaths-1-million/"
+        "title": "Dr. Fauci’s latest coronavirus warning is his most crucial one yet",
+        "link": "https://bgr.com/2020/09/27/dr-faucis-latest-coronavirus-warning-is-his-most-crucial-one-yet/"
     },
     {
-        "title": "The latest on the coronavirus pandemic: Live updates",
-        "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-09-28-20-intl/index.html"
+        "title": "Coronavirus death toll quickly approaches 1 million",
+        "link": "https://www.washingtonpost.com/graphics/2020/world/coronavirus-deaths-1-million/"
     }
 ];
