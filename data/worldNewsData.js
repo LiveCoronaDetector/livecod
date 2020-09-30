e1b4c1c2abe26a27885d668f9172ba2eab51d4d7
LiveCoronaBot <commit-bot@taeuk.me>
2020-09-30 01:06:53 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -17,27 +17,27 @@ var worldNewsData = [
         "link": "https://www.dw.com/en/germany-angela-merkel-unveils-new-coronavirus-measures/a-55092025"
     },
     {
-        "title": "WHO says official COVID-19 deaths underestimates true toll: Live",
-        "link": "https://www.aljazeera.com/news/2020/9/28/india-reports-its-six-millionth-covid-19-case-live-news"
+        "title": "As Covid-19 Closes Schools, the World’s Children Go to Work",
+        "link": "https://www.nytimes.com/2020/09/27/world/asia/covid-19-india-children-school-education-labor.html"
     },
     {
         "title": "Chinese Citizens Are Already Receiving a Coronavirus Vaccine",
         "link": "https://www.newyorker.com/news/news-desk/the-november-surprise-of-chinas-coronavirus-vaccine"
     },
     {
-        "title": "Coronavirus: Florida reports 3,266 new cases Tuesday after just 738 a day earlier",
-        "link": "https://www.local10.com/news/local/2020/09/29/coronavirus-florida-reports-3266-new-cases-tuesday-after-just-738-a-day-earlier/"
-    },
-    {
         "title": "Coronavirus Deaths Pass One Million Worldwide",
         "link": "https://www.nytimes.com/2020/09/28/world/covid-1-million-deaths.html"
     },
     {
-        "title": "Coronavirus has killed 1 million people worldwide. Experts fear the toll may double before a vaccine is ready",
-        "link": "https://www.cnn.com/2020/09/28/health/coronavirus-million-global-deaths-intl/index.html"
+        "title": "New Mexico reports 2 new deaths, 178 additional COVID-19 cases",
+        "link": "https://www.kob.com/new-mexico-news/new-mexico-reports-2-new-deaths-178-additional-covid-19-cases/5878516/"
     },
     {
         "title": "Live updates: U.N. chief warns that ‘misinformation kills’ as global coronavirus death toll tops 1 million",
         "link": "https://www.washingtonpost.com/nation/2020/09/29/coronavirus-covid-live-updates-us/"
+    },
+    {
+        "title": "Coronavirus has killed 1 million people worldwide. Experts fear the toll may double before a vaccine is ready",
+        "link": "https://www.cnn.com/2020/09/28/health/coronavirus-million-global-deaths-intl/index.html"
     }
 ];
