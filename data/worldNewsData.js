1c7f4d5c59eaf7bdce07f881c386258238391a97
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-02 03:02:11 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,8 +5,8 @@ var worldNewsData = [
         "link": "https://qz.com/africa/1911951/nigerian-scientists-develop-covid-19-vaccine-need-human-trials/"
     },
     {
-        "title": "Canada cops crash covert Covid casino",
-        "link": "https://www.theguardian.com/world/2020/sep/30/canada-illegal-casino-charges"
+        "title": "Amazon says nearly 20,000 U.S. employees have contracted Covid-19",
+        "link": "https://www.nbcnews.com/tech/tech-news/amazon-says-nearly-20-000-u-s-employees-have-contracted-n1241746"
     },
     {
         "title": "Four in 10 people diagnosed with coronavirus are haredi",
@@ -21,23 +21,23 @@ var worldNewsData = [
         "link": "https://www.theguardian.com/world/live/2020/oct/02/coronavirus-live-news-paris-faces-being-placed-on-maximum-alert-as-india-nears-100000-deaths"
     },
     {
-        "title": "China promotes 'revenge travel' to boost economy after Covid lockdowns",
-        "link": "https://www.theguardian.com/world/2020/oct/01/china-promotes-revenge-travel-boost-economy-coronavirus-golden-week"
+        "title": "China encouraging 'revenge travel' following coronavirus lockdowns to help economy",
+        "link": "https://www.foxnews.com/travel/china-revenge-travel-coronavirus-economy"
     },
     {
         "title": "Trump and first lady take coronavirus tests after top aide Hope Hicks tests positive for Covid-19",
         "link": "https://www.cnn.com/2020/10/01/politics/hope-hicks-positive-coronavirus/index.html"
     },
     {
-        "title": "Coronavirus risks ‘greatest surge in child marriages in 25 years’",
-        "link": "https://www.bbc.com/news/world-54370316"
-    },
-    {
         "title": "Coronavirus update: Global case tally above 34 million — with U.S. accounting for a fifth — as Dr. Fauci hits back at Trump’s mask claim",
         "link": "https://www.marketwatch.com/story/coronavirus-update-global-case-tally-above-34-million-with-us-accounting-for-a-fifth-as-dr-fauci-hits-back-at-trumps-mask-claims-2020-10-01"
     },
     {
-        "title": "Trump's coronavirus dispute with World Health Organization is ‘tragic’: Ex-Obama official",
-        "link": "https://finance.yahoo.com/news/trumps-coronavirus-dispute-with-world-health-organization-tragic-ex-gayle-smith-131021171.html"
+        "title": "Covid-19 News: Live Updates",
+        "link": "https://www.nytimes.com/live/2020/10/01/world/covid-19-coronavirus"
+    },
+    {
+        "title": "Coronavirus: Global Covid-19 death toll passes one million",
+        "link": "https://www.bbc.com/news/world-54334496"
     }
 ];
