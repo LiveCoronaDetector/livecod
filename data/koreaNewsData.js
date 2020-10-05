65e6847fc7eac1d711b8129988089d783ac9405e
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-05 08:32:16 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,8 +5,16 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20201005015351504?did=1195m"
     },
     {
+        "title": "국내 코로나19 연관 소아 다기관염증증후군 환자 2명 첫 확인",
+        "link": "https://news.sbs.co.kr/news/endPage.do?news_id=N1006009389&plink=ORI&cooper=NAVER"
+    },
+    {
         "title": "트럼프, 코로나 입원중 깜짝외출 '돌출행동'…\"미친 짓\" 비판론(종합)",
         "link": "http://yna.kr/AKR20201005011051071?did=1195m"
+    },
+    {
+        "title": "국내 코로나 관련 소아 다기관염증증후군 2건 확인(종합)",
+        "link": "https://biz.chosun.com/site/data/html_dir/2020/10/05/2020100501827.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
     },
     {
         "title": "코로나 신규 확진자 73명…닷새째 두 자릿수",
@@ -21,23 +29,15 @@ var koreaNewsData = [
         "link": "https://imnews.imbc.com/news/2020/society/article/5930451_32633.html"
     },
     {
-        "title": "국내 코로나 관련 소아 다기관염증증후군 2건 확인(종합)",
-        "link": "https://biz.chosun.com/site/data/html_dir/2020/10/05/2020100501827.html?utm_source=naver&utm_medium=original&utm_campaign=biz"
+        "title": "코로나19 어제 73명 신규확진…사망자 누적 422명",
+        "link": "http://www.kwnews.co.kr/nview.asp?aid=220100400148"
     },
     {
-        "title": "국내서 코로나19 연관 소아 다기관염증증후군 환자 2명 첫 확인",
-        "link": "http://www.dt.co.kr/contents.html?article_no=2020100502109919607039&ref=naver"
+        "title": "교회 등 산발적 감염 발생…서울 코로나 19명 확진(종합)",
+        "link": "http://www.fnnews.com/news/202010051117047106"
     },
     {
         "title": "포천 군부대 코로나19 확진자 30여명 '비상'…감염경로 불분명",
         "link": "http://yna.kr/AKR20201005015300504?did=1195m"
-    },
-    {
-        "title": "“확진자 다녀갔다”…코로나 허위사실 유포 187명 검거",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20201005500017&wlog_tag3=naver"
-    },
-    {
-        "title": "코로나19 어제 73명 신규확진…사망자 누적 422명",
-        "link": "http://www.kwnews.co.kr/nview.asp?aid=220100400148"
     }
 ];
