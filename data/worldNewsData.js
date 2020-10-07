8dc8cfd28b5c4cc44bc017dfa3668029dfa8fd92
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-07 06:34:13 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -25,8 +25,8 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/10/06/asia/china-global-reputation-coronavirus-intl-hnk/index.html"
     },
     {
-        "title": "Biden: If Trump still has COVID-19 'we shouldn’t have a debate'",
-        "link": "https://news.yahoo.com/biden-if-trump-still-has-covid-19-we-shouldnt-have-a-debate-235938033.html"
+        "title": "Coronavirus live news: Trump aide Stephen Miller tests positive as White House outbreak spreads",
+        "link": "https://www.theguardian.com/world/live/2020/oct/07/coronavirus-live-news-six-us-states-see-record-hospital-patients-facebook-deletes-trump-post"
     },
     {
         "title": "Unfavorable Views of China Reach Historic Highs in Many Countries",
