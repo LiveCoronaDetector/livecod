e476f9528ba347e59d6588f47e48b4448b77f442
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-07 05:31:44 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -9,10 +9,6 @@ var jejuNewsData = [
         "link": "http://biz.newdaily.co.kr/site/data/html/2020/10/07/2020100700012.html"
     },
     {
-        "title": "국내 80세 이상 코로나19 확진자 치명율 21.4% 달해",
-        "link": "http://www.mdtoday.co.kr/mdtoday/index.html?no=401404"
-    },
-    {
         "title": "추캉스 28만명…제주도, 코로나19 잠복기 2주간 ‘고비’",
         "link": "http://www.fnnews.com/news/202010051128071821"
     },
@@ -39,5 +35,9 @@ var jejuNewsData = [
     {
         "title": "25만 추캉스족 제주 입도…코로나19 확진자 없어",
         "link": "http://www.newsis.com/view/?id=NISX20201003_0001185294&cID=10813&pID=10800"
+    },
+    {
+        "title": "추석 제주 찾은 관광객 20만명 훌쩍…코로나19 확진 없어",
+        "link": "http://yna.kr/AKR20201002028700056?did=1195m"
     }
 ];
--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -13,8 +13,8 @@ var worldNewsData = [
         "link": "https://www.jpost.com/health-science/covid-19-could-cause-infertility-new-israeli-study-644767"
     },
     {
-        "title": "Europe must go beyond science to survive Covid crisis, says WHO",
-        "link": "https://www.theguardian.com/world/2020/oct/06/europe-must-go-beyond-science-to-survive-covid-crisis-says-who"
+        "title": "Europe’s Second Covid-19 Wave Starts to Spill Over From Young to Old",
+        "link": "https://www.wsj.com/articles/europes-second-covid-wave-starts-to-spill-over-from-young-to-old-11601982003"
     },
     {
         "title": "Coronavirus: Is the pandemic slowing down in India?",
@@ -25,19 +25,19 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/10/06/asia/china-global-reputation-coronavirus-intl-hnk/index.html"
     },
     {
-        "title": "Flurry of coronavirus reinfections leaves scientists puzzled",
-        "link": "https://www.theguardian.com/world/2020/oct/06/flurry-of-coronavirus-reinfections-leaves-scientists-puzzled"
-    },
-    {
-        "title": "In a few days, more people in Trump’s orbit tested positive for coronavirus than in all of Taiwan",
-        "link": "https://www.washingtonpost.com/world/2020/10/06/trump-white-house-more-coronavirus-cases-than-taiwan/"
+        "title": "Biden: If Trump still has COVID-19 'we shouldn’t have a debate'",
+        "link": "https://news.yahoo.com/biden-if-trump-still-has-covid-19-we-shouldnt-have-a-debate-235938033.html"
     },
     {
         "title": "Unfavorable Views of China Reach Historic Highs in Many Countries",
         "link": "https://www.pewresearch.org/global/2020/10/06/unfavorable-views-of-china-reach-historic-highs-in-many-countries/"
     },
     {
-        "title": "Scientists call for Covid herd immunity strategy for young",
-        "link": "https://www.theguardian.com/world/2020/oct/06/scientists-call-for-herd-immunity-covid-strategy-for-young"
+        "title": "Flurry of coronavirus reinfections leaves scientists puzzled",
+        "link": "https://www.theguardian.com/world/2020/oct/06/flurry-of-coronavirus-reinfections-leaves-scientists-puzzled"
+    },
+    {
+        "title": "In a few days, more people in Trump’s orbit tested positive for coronavirus than in all of Taiwan",
+        "link": "https://www.washingtonpost.com/world/2020/10/06/trump-white-house-more-coronavirus-cases-than-taiwan/"
     }
 ];
