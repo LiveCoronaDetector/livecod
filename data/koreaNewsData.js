7dbe815e485de00d48f1e708191d29e14ba406d6
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-10 08:32:37 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,8 +5,8 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20201010034651530?did=1195m"
     },
     {
-        "title": "강릉서 코로나19 확진자 발생…도내 13일 만에",
-        "link": "http://www.newsis.com/view/?id=NISX20201010_0001192603&cID=10805&pID=10800"
+        "title": "강릉서 13일 만에 코로나19 확진자 발생",
+        "link": "http://mbn.mk.co.kr/pages/news/newsView.php?category=mbn00009&news_seq_no=4305220"
     },
     {
         "title": "코로나19 신규 확진자 72명…사흘째 두자릿수",
@@ -21,10 +21,6 @@ var koreaNewsData = [
         "link": "https://imnews.imbc.com/news/2020/world/article/5936362_32640.html"
     },
     {
-        "title": "코로나 신규확진 72명, 전날 휴일에도 확진자 증가",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20201010500016&wlog_tag3=naver"
-    },
-    {
         "title": "서울 코로나19 신규 확진 25명…사흘째 20명대",
         "link": "https://cnbc.sbs.co.kr/article_hub/10000998801?division=NAVER"
     },
@@ -33,11 +29,15 @@ var koreaNewsData = [
         "link": "http://news.kbs.co.kr/news/view.do?ncd=5022348&ref=A"
     },
     {
+        "title": "코로나 신규확진 72명, 전날 휴일에도 확진자 증가",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20201010500016&wlog_tag3=naver"
+    },
+    {
         "title": "양주 군부대서 2명 코로나19 확진…부대원 전수조사",
         "link": "http://www.dt.co.kr/contents.html?article_no=2020101002109957607002&ref=naver"
     },
     {
-        "title": "코로나 신규확진 72명…국내 발생 61명·해외유입 11명",
-        "link": "http://news.tvchosun.com/site/data/html_dir/2020/10/10/2020101090001.html"
+        "title": "“코로나19 감소세라지만...” 방역당국이 말하는 불안 요소 셋",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20201010500052&wlog_tag3=naver"
     }
 ];
