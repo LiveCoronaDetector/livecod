69240c4a0745cb877c9e184eeec52d19de3ada56
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-11 01:46:17 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -5,8 +5,8 @@ var worldNewsData = [
         "link": "https://qz.com/africa/1915794/scientists-on-how-africa-weathered-the-worst-of-covid-19/"
     },
     {
-        "title": "Wisconsin is battling America's worst coronavirus outbreak, and the state's broken politics are partly to blame",
-        "link": "https://news.yahoo.com/wisconsin-is-battling-americas-worst-coronavirus-outbreak-and-the-states-broken-politics-is-partially-to-blame-143650745.html"
+        "title": "Coronavirus cases set new single-day records in six U.S. states and worldwide",
+        "link": "https://www.nbcnews.com/news/us-news/coronavirus-cases-set-new-single-day-records-six-u-s-n1242828"
     },
     {
         "title": "Coronavirus: Israel readies for first steps toward exiting lockdown",
@@ -25,12 +25,8 @@ var worldNewsData = [
         "link": "https://www.cnn.com/2020/10/09/asia/china-covid-vaccine-global-race-intl-hnk/index.html"
     },
     {
-        "title": "Covid-19 Live Updates: World Breaks Three-Day Record for New Cases, as Virus’s Spread Accelerates",
+        "title": "Covid-19 Live Updates: White House Doctor Says Trump Is No Longer Contagious",
         "link": "https://www.nytimes.com/live/2020/10/10/world/covid-coronavirus"
-    },
-    {
-        "title": "Coronavirus cases set new single-day records in six U.S. states and worldwide",
-        "link": "https://www.nbcnews.com/news/us-news/coronavirus-cases-set-new-single-day-records-six-u-s-n1242828"
     },
     {
         "title": "A Coronavirus Vaccine Could Kill Half A Million Sharks, Conservationists Warn",
@@ -39,5 +35,9 @@ var worldNewsData = [
     {
         "title": "Watch live: The World Health Organization holds a press briefing on the coronavirus pandemic as new global cases hit record",
         "link": "https://www.cnbc.com/2020/10/09/watch-live-world-health-organization-holds-a-briefing-on-the-coronavirus.html"
+    },
+    {
+        "title": "Herd immunity letter signed by fake experts including 'Dr Johnny Bananas'",
+        "link": "https://www.theguardian.com/world/2020/oct/09/herd-immunity-letter-signed-fake-experts-dr-johnny-bananas-covid"
     }
 ];
