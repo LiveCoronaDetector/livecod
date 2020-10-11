7721dd96d31c5ba12df3b766fc809f9bf8919d18
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-11 16:02:35 +0000

    Run crawler and update current data

--- data/worldNewsData.js
+++ data/worldNewsData.js
@@ -21,23 +21,23 @@ var worldNewsData = [
         "link": "https://www.bbc.com/news/av/world-asia-54486121"
     },
     {
-        "title": "Covid: Australia in talks over quarantine-free travel",
-        "link": "https://www.bbc.com/news/world-australia-54499478"
+        "title": "Can we trust Chinese Covid-19 science?",
+        "link": "https://www.theguardian.com/science/2020/oct/11/china-coronavirus-covid-19-medical-research"
+    },
+    {
+        "title": "Would herd immunity stop the spread of coronavirus?",
+        "link": "https://www.theguardian.com/world/2020/oct/11/would-herd-immunity-stop-the-spread-of-coronavirus"
     },
     {
         "title": "The latest on the coronavirus pandemic: Live updates",
         "link": "https://www.cnn.com/world/live-news/coronavirus-pandemic-10-11-20-intl/h_7ec2c3bb5ca5302206c6cf4598af5772"
     },
     {
-        "title": "Hours after Trump's dark and divisive White House speech, his doctor still won't say if he's tested negative",
-        "link": "https://www.cnn.com/2020/10/10/politics/donald-trump-covid-white-house-event/index.html"
+        "title": "President's physician: Trump no longer a coronavirus transmission risk",
+        "link": "https://www.youtube.com/watch?v=6l2MpO0FszA"
     },
     {
-        "title": "China is doubling down in the global push for a coronavirus vaccine",
-        "link": "https://www.cnn.com/2020/10/09/asia/china-covid-vaccine-global-race-intl-hnk/index.html"
-    },
-    {
-        "title": "Watch live: The World Health Organization holds a press briefing on the coronavirus pandemic as new global cases hit record",
-        "link": "https://www.cnbc.com/2020/10/09/watch-live-world-health-organization-holds-a-briefing-on-the-coronavirus.html"
+        "title": "Coronavirus: Spain imposes state of emergency on Madrid",
+        "link": "https://www.bbc.com/news/world-europe-54478320"
     }
 ];
