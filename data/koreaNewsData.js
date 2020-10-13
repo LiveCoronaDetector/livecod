02e92b160c750fc9755510126e3bd3557f601050
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-13 15:02:19 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,28 +5,12 @@ var koreaNewsData = [
         "link": "http://news.kbs.co.kr/news/view.do?ncd=5024139&ref=A"
     },
     {
-        "title": "[속보]서울 코로나19 신규확진자 20명…전국 102명 급증 대비 완만한 안정세 ...",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202010131122001&code=940100"
-    },
-    {
-        "title": "거리두기 1단계 완화 이틀 만에 신규확진자 세자릿수",
-        "link": "https://www.nocutnews.co.kr/news/5427122"
-    },
-    {
-        "title": "인천 주민 5명 코로나19 감염…기존 확진자 접촉",
-        "link": "http://yna.kr/AKR20201013070700065?did=1195m"
-    },
-    {
         "title": "코로나19 신규 확진자 102명…엿새 만에 세 자릿수",
         "link": "http://www.hani.co.kr/arti/society/health/965533.html"
     },
     {
-        "title": "[단독] 임시완, 거주아파트 코로나19 확진자 발생으로 '또' 검사..'런온' 일정...",
-        "link": "http://www.osen.co.kr/article/G1111451528"
-    },
-    {
-        "title": "‘70명 참석’ 교회 수련회서 다수 확진자…대전시, 고발 방침(종합)",
-        "link": "https://www.seoul.co.kr/news/newsView.php?id=20201013500185&wlog_tag3=naver"
+        "title": "[속보]서울 코로나19 신규확진자 20명…전국 102명 급증 대비 완만한 안정세 ...",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202010131122001&code=940100"
     },
     {
         "title": "코로나19 신규확진 102명…6일 만에 다시 '세자릿수'",
@@ -35,6 +19,22 @@ var koreaNewsData = [
     {
         "title": "코로나 재확산 급한불 껐다…IMF, 韓성장률 -2.1%→-1.9% 상향",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=04326326625932592"
+    },
+    {
+        "title": "[단독] 임시완, 거주아파트 코로나19 확진자 발생으로 '또' 검사..'런온' 일정...",
+        "link": "http://www.osen.co.kr/article/G1111451528"
+    },
+    {
+        "title": "[종합] 코로나19 확진자 6일만에 세 자릿수...102명 증가, 서울 18명, 경기 3...",
+        "link": "http://www.fnnews.com/news/202010130935340826"
+    },
+    {
+        "title": "인천 주민 5명 코로나19 감염…기존 확진자 접촉",
+        "link": "http://yna.kr/AKR20201013070700065?did=1195m"
+    },
+    {
+        "title": "‘70명 참석’ 교회 수련회서 확진자…대전 가족모임 부부 참석",
+        "link": "https://www.seoul.co.kr/news/newsView.php?id=20201013500045&wlog_tag3=naver"
     },
     {
         "title": "코로나 국내 신규 확진 102명… 다시 100명 대로",
