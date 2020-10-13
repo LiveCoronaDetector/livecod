585ca5067ae21fe9c4151d5e6396a451568586d0
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-13 13:02:18 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -5,20 +5,20 @@ var koreaNewsData = [
         "link": "http://news.kbs.co.kr/news/view.do?ncd=5024139&ref=A"
     },
     {
-        "title": "코로나19 신규 확진자 102명…엿새 만에 세 자릿수",
-        "link": "http://www.hani.co.kr/arti/society/health/965533.html"
-    },
-    {
         "title": "[단독] 임시완, 거주아파트 코로나19 확진자 발생으로 '또' 검사..'런온' 일정...",
         "link": "http://www.osen.co.kr/article/G1111451528"
     },
     {
-        "title": "[종합] 코로나19 확진자 6일만에 세 자릿수...102명 증가, 서울 18명, 경기 3...",
-        "link": "http://www.fnnews.com/news/202010130935340826"
+        "title": "코로나19 신규 확진자 102명…엿새 만에 세 자릿수",
+        "link": "http://www.hani.co.kr/arti/society/health/965533.html"
     },
     {
         "title": "[속보]서울 코로나19 신규확진자 20명…전국 102명 급증 대비 완만한 안정세 ...",
         "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202010131122001&code=940100"
+    },
+    {
+        "title": "[종합] 코로나19 확진자 6일만에 세 자릿수...102명 증가, 서울 18명, 경기 3...",
+        "link": "http://www.fnnews.com/news/202010130935340826"
     },
     {
         "title": "코로나19 신규확진 102명…6일 만에 다시 '세자릿수'",
@@ -29,15 +29,15 @@ var koreaNewsData = [
         "link": "http://yna.kr/AKR20201013070700065?did=1195m"
     },
     {
-        "title": "부산항 러시아 선박서 16명 코로나 확진…4개월간 총 117명",
-        "link": "http://news.mk.co.kr/newsRead.php?no=1047971&year=2020"
-    },
-    {
         "title": "확진자 100명 넘어…해외 유입 33명 중 러시아만 14명(종합)",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=01958166625932592"
     },
     {
-        "title": "코로나 국내 신규 확진 102명… 다시 100명 대로",
-        "link": "http://health.chosun.com/site/data/html_dir/2020/10/13/2020101300795.html"
+        "title": "\"한국서 치료 안받겠다\" 확진자 11명 태운 러 선박 돌아간다",
+        "link": "https://news.joins.com/article/olink/23487926"
+    },
+    {
+        "title": "서울 확진자 18명으로 적어도 깜깜이 감염 40% 육박(종합2보)",
+        "link": "http://www.newsis.com/view/?id=NISX20201013_0001196213&cID=14001&pID=14000"
     }
 ];
