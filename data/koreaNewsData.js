45bbab785bcb1a783454fe8dbd20377795dd5deb
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-13 21:02:26 +0000

    Run crawler and update current data

--- data/koreaNewsData.js
+++ data/koreaNewsData.js
@@ -13,24 +13,24 @@ var koreaNewsData = [
         "link": "http://www.hani.co.kr/arti/society/health/965533.html"
     },
     {
-        "title": "[속보]서울 코로나19 신규확진자 20명…전국 102명 급증 대비 완만한 안정세 ...",
-        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202010131122001&code=940100"
-    },
-    {
         "title": "코로나 재확산 급한불 껐다…IMF, 韓성장률 -2.1%→-1.9% 상향",
         "link": "http://www.edaily.co.kr/news/newspath.asp?newsid=04326326625932592"
     },
     {
-        "title": "'확진자 늘어나더니'…영국, 코로나19 일일 사망자 100명 넘었다",
-        "link": "http://yna.kr/AKR20201014002700085?did=1195m"
+        "title": "[속보]서울 코로나19 신규확진자 20명…전국 102명 급증 대비 완만한 안정세 ...",
+        "link": "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=202010131122001&code=940100"
     },
     {
         "title": "코로나19 신규확진 102명…6일 만에 다시 '세자릿수'",
         "link": "http://news.tvchosun.com/site/data/html_dir/2020/10/13/2020101390026.html"
     },
     {
-        "title": "코로나 국내 신규 확진 102명… 다시 100명 대로",
-        "link": "http://health.chosun.com/site/data/html_dir/2020/10/13/2020101300795.html"
+        "title": "'확진자 늘어나더니'…영국, 코로나19 일일 사망자 100명 넘었다",
+        "link": "http://yna.kr/AKR20201014002700085?did=1195m"
+    },
+    {
+        "title": "신규 확진자 102명...6일 만에 다시 세 자릿수",
+        "link": "https://www.ytn.co.kr/_ln/0103_202010130944590635"
     },
     {
         "title": "한국 수출 UAE 원전서 코로나 집단발병…확진자 파악 '뒷짐'",
