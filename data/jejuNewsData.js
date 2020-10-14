3ded560172c300eafcf82970893a9e3a06f1106a
LiveCoronaBot <commit-bot@taeuk.me>
2020-10-14 04:34:26 +0000

    Run crawler and update current data

--- data/jejuNewsData.js
+++ data/jejuNewsData.js
@@ -29,15 +29,15 @@ var jejuNewsData = [
         "link": "https://www.chosun.com/economy/stock-finance/2020/10/12/TFEQKMARUFANRCQE4SOMFWTONQ/?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
+        "title": "코로나19가 가져온 ‘국가의 확장’… 개인의 자유는?",
+        "link": "http://www.segye.com/content/html/2020/10/14/20201014513521.html?OutUrl=naver"
+    },
+    {
         "title": "추석·한글날 연휴, 제주 46만명 몰렸어도 ‘코로나 0명’",
         "link": "https://www.chosun.com/national/welfare-medical/2020/10/13/63MRCZIDYRE4LI3ZXJW7XNJD7M/?utm_source=naver&utm_medium=original&utm_campaign=news"
     },
     {
         "title": "코로나19로 지친 14만명 몰린 제주…웰니스관광 15선 선정",
         "link": "https://news.joins.com/article/olink/23485689"
-    },
-    {
-        "title": "[현장르포] 연휴 맞은 김포공항은 이미 포스트코로나",
-        "link": "http://www.fnnews.com/news/202010111225076069"
     }
 ];
